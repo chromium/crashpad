chromium/crashpad

commit 7dda7b3228817cfad06e6ee523271c24ddabe416

Superficial gyp changes to not immediately error out on Windows
Just guarding the usage of $SDKROOT so that ninja doesn't error out
on invalid $ escape on parsing.

R=mark@chromium.org
BUG=crashpad:1

Review URL: https://codereview.chromium.org/620553004

util/util.gyp
@@ -25,13 +25,6 @@
         '..',
         '<(INTERMEDIATE_DIR)',
       ],
-      'link_settings': {
-        'libraries': [
-          '$(SDKROOT)/System/Library/Frameworks/CoreFoundation.framework',
-          '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
-          '$(SDKROOT)/System/Library/Frameworks/IOKit.framework',
-        ],
-      },
       'sources': [
         'file/fd_io.cc',
         'file/fd_io.h',
@@ -113,41 +106,52 @@
         'synchronization/semaphore.cc',
         'synchronization/semaphore.h',
       ],
-      'actions': [
-        {
-          'action_name': 'mig exc.defs',
-          'inputs': [
-            'mach/mig.py',
-            '$(SDKROOT)/usr/include/mach/exc.defs',
+      'conditions': [
+        ['OS=="mac"', {
+          'actions': [
+            {
+              'action_name': 'mig exc.defs',
+              'inputs': [
+                'mach/mig.py',
+                '$(SDKROOT)/usr/include/mach/exc.defs',
+              ],
+              'outputs': [
+                '<(INTERMEDIATE_DIR)/util/mach/excUser.c',
+                '<(INTERMEDIATE_DIR)/util/mach/excServer.c',
+                '<(INTERMEDIATE_DIR)/util/mach/exc.h',
+                '<(INTERMEDIATE_DIR)/util/mach/excServer.h',
+              ],
+              'action': [
+                'python', '<@(_inputs)', '<@(_outputs)'
+              ],
+              'process_outputs_as_sources': 1,
+            },
+            {
+              'action_name': 'mig mach_exc.defs',
+              'inputs': [
+                'mach/mig.py',
+                '$(SDKROOT)/usr/include/mach/mach_exc.defs',
+              ],
+              'outputs': [
+                '<(INTERMEDIATE_DIR)/util/mach/mach_excUser.c',
+                '<(INTERMEDIATE_DIR)/util/mach/mach_excServer.c',
+                '<(INTERMEDIATE_DIR)/util/mach/mach_exc.h',
+                '<(INTERMEDIATE_DIR)/util/mach/mach_excServer.h',
+              ],
+              'action': [
+                'python', '<@(_inputs)', '<@(_outputs)'
+              ],
+              'process_outputs_as_sources': 1,
+            },
           ],
-          'outputs': [
-            '<(INTERMEDIATE_DIR)/util/mach/excUser.c',
-            '<(INTERMEDIATE_DIR)/util/mach/excServer.c',
-            '<(INTERMEDIATE_DIR)/util/mach/exc.h',
-            '<(INTERMEDIATE_DIR)/util/mach/excServer.h',
-          ],
-          'action': [
-            'python', '<@(_inputs)', '<@(_outputs)'
-          ],
-          'process_outputs_as_sources': 1,
-        },
-        {
-          'action_name': 'mig mach_exc.defs',
-          'inputs': [
-            'mach/mig.py',
-            '$(SDKROOT)/usr/include/mach/mach_exc.defs',
-          ],
-          'outputs': [
-            '<(INTERMEDIATE_DIR)/util/mach/mach_excUser.c',
-            '<(INTERMEDIATE_DIR)/util/mach/mach_excServer.c',
-            '<(INTERMEDIATE_DIR)/util/mach/mach_exc.h',
-            '<(INTERMEDIATE_DIR)/util/mach/mach_excServer.h',
-          ],
-          'action': [
-            'python', '<@(_inputs)', '<@(_outputs)'
-          ],
-          'process_outputs_as_sources': 1,
-        },
+          'link_settings': {
+            'libraries': [
+              '$(SDKROOT)/System/Library/Frameworks/CoreFoundation.framework',
+              '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
+              '$(SDKROOT)/System/Library/Frameworks/IOKit.framework',
+            ],
+          },
+        }],
       ],
     },
     {
@@ -162,11 +166,6 @@
       'include_dirs': [
         '..',
       ],
-      'link_settings': {
-        'libraries': [
-          '$(SDKROOT)/usr/lib/libbsm.dylib',
-        ],
-      },
       'sources': [
         'test/errors.cc',
         'test/errors.h',
@@ -237,6 +236,15 @@
         'test/mac/mach_multiprocess_test.cc',
         'test/multiprocess_exec_test.cc',
         'test/multiprocess_test.cc',
+      ],
+      'conditions': [
+        ['OS=="mac"', {
+          'link_settings': {
+            'libraries': [
+              '$(SDKROOT)/usr/lib/libbsm.dylib',
+            ],
+          },
+        }],
       ],
     },
     {

tools/tools.gyp
@@ -13,74 +13,80 @@
 # limitations under the License.
 
 {
-  'targets': [
-    {
-      'target_name': 'tool_support',
-      'type': 'static_library',
-      'dependencies': [
-        '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
+  'conditions': [
+    ['OS=="mac"', {
+      'targets': [
+        {
+          'target_name': 'tool_support',
+          'type': 'static_library',
+          'dependencies': [
+            '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
+          ],
+          'include_dirs': [
+            '..',
+          ],
+          'sources': [
+            'tool_support.cc',
+            'tool_support.h',
+          ],
+        },
+        {
+          'target_name': 'catch_exception_tool',
+          'type': 'executable',
+          'dependencies': [
+            'tool_support',
+            '../compat/compat.gyp:compat',
+            '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
+            '../util/util.gyp:util',
+          ],
+          'include_dirs': [
+            '..',
+          ],
+          'sources': [
+            'catch_exception_tool.cc',
+          ],
+        },
+        {
+          'target_name': 'exception_port_tool',
+          'type': 'executable',
+          'dependencies': [
+            'tool_support',
+            '../compat/compat.gyp:compat',
+            '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
+            '../util/util.gyp:util',
+          ],
+          'include_dirs': [
+            '..',
+          ],
+          'sources': [
+            'exception_port_tool.cc',
+          ],
+        },
+        {
+          'target_name': 'on_demand_service_tool',
+          'type': 'executable',
+          'dependencies': [
+            'tool_support',
+            '../compat/compat.gyp:compat',
+            '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
+            '../util/util.gyp:util',
+          ],
+          'include_dirs': [
+            '..',
+          ],
+          'link_settings': {
+            'libraries': [
+              '$(SDKROOT)/System/Library/Frameworks/CoreFoundation.framework',
+              '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
+            ],
+          },
+          'sources': [
+            'on_demand_service_tool.mm',
+          ],
+        },
       ],
-      'include_dirs': [
-        '..',
-      ],
-      'sources': [
-        'tool_support.cc',
-        'tool_support.h',
-      ],
-    },
-    {
-      'target_name': 'catch_exception_tool',
-      'type': 'executable',
-      'dependencies': [
-        'tool_support',
-        '../compat/compat.gyp:compat',
-        '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
-        '../util/util.gyp:util',
-      ],
-      'include_dirs': [
-        '..',
-      ],
-      'sources': [
-        'catch_exception_tool.cc',
-      ],
-    },
-    {
-      'target_name': 'exception_port_tool',
-      'type': 'executable',
-      'dependencies': [
-        'tool_support',
-        '../compat/compat.gyp:compat',
-        '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
-        '../util/util.gyp:util',
-      ],
-      'include_dirs': [
-        '..',
-      ],
-      'sources': [
-        'exception_port_tool.cc',
-      ],
-    },
-    {
-      'target_name': 'on_demand_service_tool',
-      'type': 'executable',
-      'dependencies': [
-        'tool_support',
-        '../compat/compat.gyp:compat',
-        '../third_party/mini_chromium/mini_chromium/base/base.gyp:base',
-        '../util/util.gyp:util',
-      ],
-      'include_dirs': [
-        '..',
-      ],
-      'link_settings': {
-        'libraries': [
-          '$(SDKROOT)/System/Library/Frameworks/CoreFoundation.framework',
-          '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
-        ],
-      },
-      'sources': [
-        'on_demand_service_tool.mm',
-      ],
-    },
+    }, {
+      'targets': [],
+    }],
   ],
 }

commit 47f17926e4f3f1425a39732ae3ca60cc6fc5365d

util_test: specify libbsm link dependency properly.
7dda7b322881 accidentally moved the libbsm.dylib dependency from
util_test_lib to util_test. It is only used by
test/mac/mach_multiprocess.cc in util_test_lib.

Meanwhile, util_test should link against Foundation.framework, which is
used by three tests in util/mac: launchd_test.mm, mac_util_test.mm, and
service_management_test.mm.

R=scottmg@chromium.org

Review URL: https://codereview.chromium.org/612923004

util/util.gyp
@@ -183,6 +183,15 @@
         'test/posix/close_multiple.cc',
         'test/posix/close_multiple.h',
       ],
+      'conditions': [
+        ['OS=="mac"', {
+          'link_settings': {
+            'libraries': [
+              '$(SDKROOT)/usr/lib/libbsm.dylib',
+            ],
+          },
+        }],
+      ],
     },
     {
       'target_name': 'util_test',
@@ -241,7 +250,7 @@
         ['OS=="mac"', {
           'link_settings': {
             'libraries': [
-              '$(SDKROOT)/usr/lib/libbsm.dylib',
+              '$(SDKROOT)/System/Library/Frameworks/Foundation.framework',
             ],
           },
         }],