9b9e49d0d9836d91420e1d4a3e5648432f10318d
Mark Mentovai <mark@chromium.org>
2015-09-17 13:11:23 -0400

    Remove unnecessary crashpad:: qualification from implicit_cast<>
    This undoes 595803e1be18, a workaround that should no longer be
    necessary now that base no longer provides implicit_cast<> in the global
    namespace.
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/1356673003 .

--- util/mac/launchd.mm
+++ util/mac/launchd.mm
@@ -44,8 +44,8 @@ launch_data_t CFPropertyToLaunchData(CFPropertyListRef property_cf) {
           return nullptr;
         }
 
-        CFPropertyListRef value_cf = crashpad::implicit_cast<CFPropertyListRef>(
-            [dictionary_ns objectForKey:key]);
+        CFPropertyListRef value_cf =
+            implicit_cast<CFPropertyListRef>([dictionary_ns objectForKey:key]);
         launch_data_t value_launch = CFPropertyToLaunchData(value_cf);
         if (!value_launch) {
           return nullptr;
@@ -65,7 +65,7 @@ launch_data_t CFPropertyToLaunchData(CFPropertyListRef property_cf) {
 
       for (id element_ns in array_ns) {
         CFPropertyListRef element_cf =
-            crashpad::implicit_cast<CFPropertyListRef>(element_ns);
+            implicit_cast<CFPropertyListRef>(element_ns);
         launch_data_t element_launch = CFPropertyToLaunchData(element_cf);
         if (!element_launch) {
           return nullptr;
