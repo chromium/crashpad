27bf3f290c6c7e31901bd147ad73d175012d5082
Mark Mentovai <mark@chromium.org>
2015-10-30 18:48:03 -0400

    Update documentation to master e86d9bdc55e4

--- doc/generated/doxygen/search/classes_3.js
+++ doc/generated/doxygen/search/classes_3.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['data',['Data',['../structcrashpad_1_1Settings_1_1Data.html',1,'crashpad::Settings']]],
   ['databasesizeprunecondition',['DatabaseSizePruneCondition',['../classcrashpad_1_1DatabaseSizePruneCondition.html',1,'crashpad']]],
+  ['defaultinterface',['DefaultInterface',['../classcrashpad_1_1NotifyServer_1_1DefaultInterface.html',1,'crashpad::NotifyServer']]],
   ['delegate',['Delegate',['../classcrashpad_1_1MemorySnapshot_1_1Delegate.html',1,'crashpad::MemorySnapshot']]],
   ['delegate',['Delegate',['../classcrashpad_1_1ExceptionHandlerServer_1_1Delegate.html',1,'crashpad::ExceptionHandlerServer']]]
 ];
