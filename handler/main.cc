6c1bd97df0a2f3a483ed4f36b63569485ca8b07e
Scott Graham <scottmg@chromium.org>
2015-11-06 10:59:33 -0800

    Fix compile after 20869d9
    TBR=mark@chromium.org
    BUG=chromium:546288
    
    Review URL: https://codereview.chromium.org/1431813003 .

--- handler/main.cc
+++ handler/main.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "handler/main.h"
+#include "handler/handler_main.h"
 
 #include "build/build_config.h"
 #include "tools/tool_support.h"
