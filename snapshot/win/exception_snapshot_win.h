17c6b181da5ab5118ae89b23bbd07ab3fa9f87cf
Scott Graham <scottmg@chromium.org>
2016-05-02 11:36:41 -0700

    win: Adjust thread suspend count for DumpAndCrashTargetProcess() case
    Because DumpAndCrashTargetProcess() suspends the process, the thread
    suspend count is one too high for all threads other than the injection
    one in the thread snapshots. Compensate for this when we detect this
    type of exception.
    
    BUG=crashpad:103
    
    Change-Id: Ib77112fddf5324fc0e43f598604e56f77d67ff54

--- snapshot/win/exception_snapshot_win.h
+++ snapshot/win/exception_snapshot_win.h
@@ -57,12 +57,15 @@ class ExceptionSnapshotWin final : public ExceptionSnapshot {
   //!     `EXCEPTION_POINTERS` record in the target process, passed through from
   //!     the exception handler.
   //!
+  //! \note If the exception was triggered by
+  //!     CrashpadClient::DumpAndCrashTargetProcess(), this has the side-effect
+  //!     of correcting the thread suspend counts for \a process_reader.
+  //!
   //! \return `true` if the snapshot could be created, `false` otherwise with
   //!     an appropriate message logged.
   bool Initialize(ProcessReaderWin* process_reader,
                   DWORD thread_id,
-                  WinVMAddress exception_pointers,
-                  const PointerVector<internal::ThreadSnapshotWin>& threads);
+                  WinVMAddress exception_pointers);
 
   // ExceptionSnapshot:
 
@@ -79,9 +82,9 @@ class ExceptionSnapshotWin final : public ExceptionSnapshot {
             class ExceptionPointersType,
             class ContextType>
   bool InitializeFromExceptionPointers(
-      const ProcessReaderWin& process_reader,
+      ProcessReaderWin* process_reader,
       WinVMAddress exception_pointers_address,
-      const PointerVector<internal::ThreadSnapshotWin>& threads,
+      DWORD thread_id,
       void (*native_to_cpu_context)(const ContextType& context_record,
                                     CPUContext* context,
                                     CPUContextUnion* context_union));
