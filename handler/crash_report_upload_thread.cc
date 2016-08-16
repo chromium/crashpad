03ebd66d458a1485cf4eaf119add21b1dd6792c8
Gayane Petrosyan <gayane@chromium.org>
2016-08-16 16:08:44 -0400

    Adding support for on-demand uploads.
    In order to allow on-demand uploads for crash reports, adding a
    upload_explicitly_requested bit on 'pending' state and necessary support
    for it.
    
    BUG=chromium:620762
    
    Change-Id: Ida38e483fe8d0e48eb5cbe95e8b8bfd96a2f8f00

--- handler/crash_report_upload_thread.cc
+++ handler/crash_report_upload_thread.cc
@@ -190,11 +190,12 @@ void CrashReportUploadThread::ProcessPendingReport(
   Settings* const settings = database_->GetSettings();
 
   bool uploads_enabled;
-  if (!settings->GetUploadsEnabled(&uploads_enabled) ||
-      !uploads_enabled ||
-      url_.empty()) {
-    // If the upload-enabled state can’t be determined, uploads are disabled, or
-    // there’s no URL to upload to, don’t attempt to upload the new report.
+  if (url_.empty() ||
+      (!report.upload_explicitly_requested &&
+       (!settings->GetUploadsEnabled(&uploads_enabled) || !uploads_enabled))) {
+    // Don’t attempt an upload if there’s no URL to upload to. Allow upload if
+    // it has been explicitly requested by the user, otherwise, respect the
+    // upload-enabled state stored in the database’s settings.
     database_->SkipReportUpload(report.uuid);
     return;
   }
@@ -250,6 +251,10 @@ void CrashReportUploadThread::ProcessPendingReport(
       // In these cases, SkipReportUpload() might not work either, but it’s best
       // to at least try to get the report out of the way.
       database_->SkipReportUpload(report.uuid);
+      return;
+
+    case CrashReportDatabase::kCannotRequestUpload:
+      NOTREACHED();
       return;
   }
 
