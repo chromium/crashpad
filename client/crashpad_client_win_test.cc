719c6defcf74e187f957404016af25d3f2c01877
Scott Graham <scottmg@chromium.org>
2016-08-16 16:30:31 -0700

    win: switch crashpad_handler.exe to /subsystem:windows and add .com
    This switches the default behaviour of crashpad_handler.exe to be a
    /subsystem:windows app, so that normal usage won't cause a console to be
    popped up. At the same time, creates a copy of crashpad_handler.exe in
    the output dir named crashpad_handler.com. The .com doesn't affect
    normal operation, as the way StartHandler() uses CreateProcess()
    requires a real path to a file. However, when run from a command prompt,
    .com are found before .exe, so editbin the .com to be to a console app,
    which will be run in preference to the exe when run as just
    "crashpad_handler", as one tends to do from a command prompt when
    debugging. That is:
    
      d:\src\crashpad\crashpad\out\Debug>where crashpad_handler
      d:\src\crashpad\crashpad\out\Debug\crashpad_handler.com
      d:\src\crashpad\crashpad\out\Debug\crashpad_handler.exe
    
      d:\src\crashpad\crashpad\out\Debug>crashpad_handler --help
      Usage: crashpad_handler [OPTION]...
      ...
    
      d:\src\crashpad\crashpad\out\Debug>crashpad_handler.exe --help
      <no output>
    
      d:\src\crashpad\crashpad\out\Debug>crashpad_handler.com --help
      Usage: crashpad_handler.com [OPTION]...
      ...
    
    We also use the .com file in test invocations so that output streams
    will be visible.
    
    R=mark@chromium.org
    
    Change-Id: I1a27f88472d491b2a1d76e63c45e6415d9f679c0

--- client/crashpad_client_win_test.cc
+++ client/crashpad_client_win_test.cc
@@ -27,7 +27,7 @@ namespace {
 void StartAndUseHandler() {
   ScopedTempDir temp_dir;
   base::FilePath handler_path = Paths::Executable().DirName().Append(
-      FILE_PATH_LITERAL("crashpad_handler.exe"));
+      FILE_PATH_LITERAL("crashpad_handler.com"));
   CrashpadClient client;
   ASSERT_TRUE(client.StartHandler(handler_path,
                                   temp_dir.path(),
