d25c39790f15803851c960d92d651a83e6e66f4e
Scott Graham <scottmg@chromium.org>
2016-09-19 10:44:02 -0700

    Add UMA to exception handler exception catching
    Includes mini_chromium DEPS roll for:
    88e0a3e Add stub of sparse_histogram.h
    
    R=mark@chromium.org
    BUG=crashpad:100
    
    Change-Id: I4c541a33be0f7f47e972af638d4765bd06682acf

--- util/misc/metrics.h
+++ util/misc/metrics.h
@@ -15,6 +15,8 @@
 #ifndef CRASHPAD_UTIL_MISC_METRICS_H_
 #define CRASHPAD_UTIL_MISC_METRICS_H_
 
+#include <inttypes.h>
+
 #include "base/macros.h"
 #include "util/file/file_io.h"
 
@@ -31,6 +33,55 @@ class Metrics {
   //! \brief Reports the size of a crash report file in bytes. Should be called
   //!     when a new report is written to disk.
   static void CrashReportSize(FileHandle file);
+
+  //! \brief The result of capturing an exception. These are used as metrics
+  //!     enumeration values so new values should always be added at the end.
+  enum class CaptureResult : int {
+    //! \brief The exception capture succeeded normally.
+    kSuccess = 0,
+
+    //! \brief Unexpected exception behavior.
+    //!
+    //! This value is only used on Mac OS X.
+    kUnexpectedExceptionBehavior = 1,
+
+    //! \brief Failed due to attempt to suspend self.
+    //!
+    //! This value is only used on Mac OS X.
+    kFailedDueToSuspendSelf = 2,
+
+    //! \brief The process snapshot could not be captured.
+    kSnapshotFailed = 3,
+
+    //! \brief The exception could not be initialized.
+    kExceptionInitializationFailed = 4,
+
+    //! \brief The attempt to prepare a new crash report in the crash database
+    //!     failed.
+    kPrepareNewCrashReportFailed = 5,
+
+    //! \brief Writing the minidump to disk failed.
+    kMinidumpWriteFailed = 6,
+
+    //! \brief There was a database error in attempt to complete the report.
+    kFinishedWritingCrashReportFailed = 7,
+
+    //! \brief The number of values in this enumeration; not a valid value.
+    kMaxValue
+  };
+
+  //! \brief Reports on the outcome of capturing a report in the exception
+  //!     handler.
+  static void ExceptionCaptureResult(CaptureResult result);
+
+  //! \brief The exception code for an exception was retrieved.
+  //!
+  //! These values are OS-specific, and correspond to
+  //! MINIDUMP_EXCEPTION::ExceptionCode.
+  static void ExceptionCode(uint32_t exception_code);
+
+  //! \brief The exception handler server started capturing an exception.
+  static void ExceptionEncountered();
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(Metrics);
