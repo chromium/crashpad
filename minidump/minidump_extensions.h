6c7f8a6f097ffc0137c4d77c827e11bf33504234
Mark Mentovai <mark@chromium.org>
2016-11-08 14:23:09 -0500

    doc: Fix all Doxygen warnings, cleaning up some generated documentation
    This makes Doxygen’s output more actionable by setting QUIET = YES to
    suppress verbose progress spew, and WARN_IF_UNDOCUMENTED = NO to prevent
    warnings for undocumented classes and members from being generated. The
    latter is too noisy, producing 721 warnings in the current codebase.
    
    The remaining warnings produced by Doxygen were useful and actionable.
    They fell into two categories: abuses of Doxygen’s markup syntax, and
    missing (or misspelled) parameter documentation. In a small number of
    cases, pass-through parameters had intentionally been left undocumented.
    In these cases, they are now given blank \param descriptions. This is
    not optimal, but there doesn’t appear to be any other way to tell
    Doxygen to allow a single parameter to be undocumented.
    
    Some tricky Doxygen errors were resolved by asking it to not enter
    directiores that we do not provide documentation in (such as the
    “on-platform” compat directories, compat/mac and compat/win, as well as
    compat/non_cxx11_lib) while allowing it to enter the
    “off-platform” directories that we do document (compat/non_mac and
    compat/non_win).
    
    A Doxygen run (doc/support/generate_doxygen.sh) now produces no output
    at all. It would produce warnings if any were triggered.
    
    Not directly related, but still relevant to documentation,
    doc/support/generate.sh is updated to remove temporary removals of
    now-extinct files and directories. doc/appengine/README is updated so
    that a consistent path to “goapp” is used throughout the file.
    
    Change-Id: I300730c04de4d3340551ea3086ca70cc5ff862d1

--- minidump/minidump_extensions.h
+++ minidump/minidump_extensions.h
@@ -276,7 +276,7 @@ struct ALIGNAS(4) PACKED MinidumpSimpleStringDictionary {
 //! #version, so that newer parsers will be able to determine whether the added
 //! fields are valid or not.
 //!
-//! \sa #MinidumpModuleCrashpadInfoList
+//! \sa MinidumpModuleCrashpadInfoList
 struct ALIGNAS(4) PACKED MinidumpModuleCrashpadInfo {
   //! \brief The structure’s currently-defined version number.
   //!
@@ -423,7 +423,7 @@ struct ALIGNAS(4) PACKED MinidumpCrashpadInfo {
   //! This field is present when #version is at least `1`.
   MINIDUMP_LOCATION_DESCRIPTOR simple_annotations;
 
-  //! \brief A pointer to a #MinidumpModuleCrashpadInfoList structure.
+  //! \brief A pointer to a MinidumpModuleCrashpadInfoList structure.
   //!
   //! This field is present when #version is at least `1`.
   MINIDUMP_LOCATION_DESCRIPTOR module_list;
