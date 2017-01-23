a216e81f0c7bf30192e36be0f78cb3700e0f4553
Mark Mentovai <mark@chromium.org>
2017-01-23 17:26:43 -0500

    mac: Report richer exception codes via metrics
    Previously, only the top-level exception code was reported via the
    Crashpad.ExceptionCode.Mac histogram. Making this histogram work
    (https://crbug.com/678720) has revealed that Chrome is triggering
    EXC_RESOURCE exceptions at a rate in excess of 4x that of ordinary
    crashes. These exceptions were not previously visible because they are
    not uploaded unless the system treats them as fatal, which it does not
    normally do absent an explicit request.
    
    In order to learn more about the problem, this change augments the data
    reported via the Crashpad.ExceptionCode.Mac histogram to report (at
    least) second-level exception data. This means that we will no longer
    see just EXC_RESOURCE, but potentially more useful information such as
    EXC_RESOURCE / RESOURCE_TYPE_IO / FLAVOR_IO_PHYSICAL_WRITES. This also
    applies to other exception types, so that the majority of crashes
    currently falling into the EXC_CRASH bucket will now have additional
    information decoded and will be reported as, for example, EXC_BAD_ACCESS
    / KERN_INVALID_ADDRESS, EXC_BAD_INSTRUCTION / EXC_I386_INVOP, and
    EXC_CRASH / SIGABRT.
    
    Because the old mechanism was only live (in an “it works” sense) for
    several days, and the new mechanism does not overlap with histogram
    values used by the old one, there’s no need to invent a new histogram
    name.
    
    BUG=chromium:684051
    
    Change-Id: Ia0a372b4127f7b3b2e7dbbaac9304cce3b5aadfe

--- util/mach/exception_types_test.cc
+++ util/mach/exception_types_test.cc
@@ -41,9 +41,21 @@ TEST(ExceptionTypes, ExcCrashRecoverOriginalException) {
       {0xb100001, EXC_BAD_ACCESS, KERN_INVALID_ADDRESS, SIGSEGV},
       {0xb100002, EXC_BAD_ACCESS, KERN_PROTECTION_FAILURE, SIGSEGV},
       {0xa100002, EXC_BAD_ACCESS, KERN_PROTECTION_FAILURE, SIGBUS},
-      {0x4200001, EXC_BAD_INSTRUCTION, 1, SIGILL},
-      {0x8300001, EXC_ARITHMETIC, 1, SIGFPE},
-      {0x5600002, EXC_BREAKPOINT, 2, SIGTRAP},
+      {0xa100005, EXC_BAD_ACCESS, VM_PROT_READ | VM_PROT_EXECUTE, SIGBUS},
+      {0xa10000d, EXC_BAD_ACCESS, EXC_I386_GPFLT, SIGBUS},
+      {0x9100032, EXC_BAD_ACCESS, KERN_CODESIGN_ERROR, SIGKILL},
+      {0x4200001, EXC_BAD_INSTRUCTION, EXC_I386_INVOP, SIGILL},
+      {0x420000b, EXC_BAD_INSTRUCTION, EXC_I386_SEGNPFLT, SIGILL},
+      {0x420000c, EXC_BAD_INSTRUCTION, EXC_I386_STKFLT, SIGILL},
+      {0x8300001, EXC_ARITHMETIC, EXC_I386_DIV, SIGFPE},
+      {0x8300002, EXC_ARITHMETIC, EXC_I386_INTO, SIGFPE},
+      {0x8300005, EXC_ARITHMETIC, EXC_I386_EXTERR, SIGFPE},
+      {0x8300008, EXC_ARITHMETIC, EXC_I386_SSEEXTERR, SIGFPE},
+      {0x5500007, EXC_SOFTWARE, EXC_I386_BOUND, SIGTRAP},
+      {0x5600001, EXC_BREAKPOINT, EXC_I386_SGL, SIGTRAP},
+      {0x5600002, EXC_BREAKPOINT, EXC_I386_BPT, SIGTRAP},
+      {0x0700080, EXC_SYSCALL, 128, 0},
+      {0x0706000, EXC_SYSCALL, 0x6000, 0},
       {0x3000000, 0, 0, SIGQUIT},
       {0x6000000, 0, 0, SIGABRT},
       {0xc000000, 0, 0, SIGSYS},
@@ -86,40 +98,173 @@ TEST(ExceptionTypes, ExcCrashRecoverOriginalException) {
   EXPECT_EQ(test_data.signal, signal);
 }
 
-// These macros come from the #ifdef KERNEL section of <kern/exc_resource.h>:
-// 10.10 xnu-2782.1.97/osfmk/kern/exc_resource.h.
-#define EXC_RESOURCE_ENCODE_TYPE(code, type) \
-  ((code) |= ((static_cast<uint64_t>(type) & 0x7ull) << 61))
-#define EXC_RESOURCE_ENCODE_FLAVOR(code, flavor) \
-  ((code) |= ((static_cast<uint64_t>(flavor) & 0x7ull) << 58))
+TEST(ExceptionTypes, ExcCrashCouldContainException) {
+  // This seems wrong, but it happens when EXC_CRASH carries an exception not
+  // originally caused by a hardware fault, such as SIGABRT.
+  EXPECT_TRUE(ExcCrashCouldContainException(0));
+
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_BAD_ACCESS));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_BAD_INSTRUCTION));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_ARITHMETIC));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_EMULATION));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_SOFTWARE));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_BREAKPOINT));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_SYSCALL));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_MACH_SYSCALL));
+  EXPECT_TRUE(ExcCrashCouldContainException(EXC_RPC_ALERT));
+  EXPECT_FALSE(ExcCrashCouldContainException(EXC_CRASH));
+  EXPECT_FALSE(ExcCrashCouldContainException(EXC_RESOURCE));
+  EXPECT_FALSE(ExcCrashCouldContainException(EXC_GUARD));
+  EXPECT_FALSE(ExcCrashCouldContainException(EXC_CORPSE_NOTIFY));
+  EXPECT_FALSE(ExcCrashCouldContainException(kMachExceptionSimulated));
+}
+
+// This macro is adapted from those in the #ifdef KERNEL section of
+// <kern/exc_resource.h>: 10.12.2 xnu-3789.31.2/osfmk/kern/exc_resource.h.
+#define EXC_RESOURCE_ENCODE_TYPE_FLAVOR(type, flavor)   \
+  (static_cast<mach_exception_code_t>(                  \
+      (((static_cast<uint64_t>(type) & 0x7ull) << 61) | \
+       (static_cast<uint64_t>(flavor) & 0x7ull) << 58)))
+
+TEST(ExceptionTypes, ExceptionCodeForMetrics) {
+  struct TestData {
+    exception_type_t exception;
+    mach_exception_code_t code_0;
+    int32_t metrics_code;
+  };
+  const TestData kTestData[] = {
+#define ENCODE_EXC(type, code_0) \
+  { (type), (code_0), ((type) << 16) | (code_0) }
+      ENCODE_EXC(EXC_BAD_ACCESS, KERN_INVALID_ADDRESS),
+      ENCODE_EXC(EXC_BAD_ACCESS, KERN_PROTECTION_FAILURE),
+      ENCODE_EXC(EXC_BAD_ACCESS, VM_PROT_READ | VM_PROT_EXECUTE),
+      ENCODE_EXC(EXC_BAD_ACCESS, EXC_I386_GPFLT),
+      ENCODE_EXC(EXC_BAD_ACCESS, KERN_CODESIGN_ERROR),
+      ENCODE_EXC(EXC_BAD_INSTRUCTION, EXC_I386_INVOP),
+      ENCODE_EXC(EXC_BAD_INSTRUCTION, EXC_I386_SEGNPFLT),
+      ENCODE_EXC(EXC_BAD_INSTRUCTION, EXC_I386_STKFLT),
+      ENCODE_EXC(EXC_ARITHMETIC, EXC_I386_DIV),
+      ENCODE_EXC(EXC_ARITHMETIC, EXC_I386_INTO),
+      ENCODE_EXC(EXC_ARITHMETIC, EXC_I386_EXTERR),
+      ENCODE_EXC(EXC_ARITHMETIC, EXC_I386_SSEEXTERR),
+      ENCODE_EXC(EXC_SOFTWARE, EXC_I386_BOUND),
+      ENCODE_EXC(EXC_BREAKPOINT, EXC_I386_SGL),
+      ENCODE_EXC(EXC_BREAKPOINT, EXC_I386_BPT),
+      ENCODE_EXC(EXC_SYSCALL, 128),
+      ENCODE_EXC(EXC_SYSCALL, 0x6000),
+#undef ENCODE_EXC
+
+#define ENCODE_EXC_CRASH(type, code_0)                        \
+  {                                                           \
+    EXC_CRASH, (((type) & 0xf) << 20) | ((code_0) & 0xfffff), \
+        ((type) << 16) | (code_0)                             \
+  }
+      ENCODE_EXC_CRASH(EXC_BAD_ACCESS, KERN_INVALID_ADDRESS),
+      ENCODE_EXC_CRASH(EXC_BAD_ACCESS, KERN_PROTECTION_FAILURE),
+      ENCODE_EXC_CRASH(EXC_BAD_ACCESS, VM_PROT_READ | VM_PROT_EXECUTE),
+      ENCODE_EXC_CRASH(EXC_BAD_ACCESS, EXC_I386_GPFLT),
+      ENCODE_EXC_CRASH(EXC_BAD_ACCESS, KERN_CODESIGN_ERROR),
+      ENCODE_EXC_CRASH(EXC_BAD_INSTRUCTION, EXC_I386_INVOP),
+      ENCODE_EXC_CRASH(EXC_BAD_INSTRUCTION, EXC_I386_SEGNPFLT),
+      ENCODE_EXC_CRASH(EXC_BAD_INSTRUCTION, EXC_I386_STKFLT),
+      ENCODE_EXC_CRASH(EXC_ARITHMETIC, EXC_I386_DIV),
+      ENCODE_EXC_CRASH(EXC_ARITHMETIC, EXC_I386_INTO),
+      ENCODE_EXC_CRASH(EXC_ARITHMETIC, EXC_I386_EXTERR),
+      ENCODE_EXC_CRASH(EXC_ARITHMETIC, EXC_I386_SSEEXTERR),
+      ENCODE_EXC_CRASH(EXC_SOFTWARE, EXC_I386_BOUND),
+      ENCODE_EXC_CRASH(EXC_BREAKPOINT, EXC_I386_SGL),
+      ENCODE_EXC_CRASH(EXC_BREAKPOINT, EXC_I386_BPT),
+      ENCODE_EXC_CRASH(EXC_SYSCALL, 128),
+      ENCODE_EXC_CRASH(EXC_SYSCALL, 0x6000),
+#undef ENCODE_EXC_CRASH
+
+#define ENCODE_EXC_CRASH_SIGNAL(signal) \
+  { EXC_CRASH, (((signal) & 0xff) << 24), (EXC_CRASH << 16) | (signal) }
+      ENCODE_EXC_CRASH_SIGNAL(SIGQUIT),
+      ENCODE_EXC_CRASH_SIGNAL(SIGABRT),
+      ENCODE_EXC_CRASH_SIGNAL(SIGSYS),
+#undef ENCODE_EXC_CRASH_SIGNAL
+
+#define ENCODE_EXC_RESOURCE(type, flavor)                            \
+  {                                                                  \
+    EXC_RESOURCE, EXC_RESOURCE_ENCODE_TYPE_FLAVOR((type), (flavor)), \
+        (EXC_RESOURCE << 16) | ((type) << 8) | (flavor)              \
+  }
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_CPU, FLAVOR_CPU_MONITOR),
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_CPU, FLAVOR_CPU_MONITOR_FATAL),
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_WAKEUPS, FLAVOR_WAKEUPS_MONITOR),
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_MEMORY, FLAVOR_HIGH_WATERMARK),
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_IO, FLAVOR_IO_PHYSICAL_WRITES),
+      ENCODE_EXC_RESOURCE(RESOURCE_TYPE_IO, FLAVOR_IO_LOGICAL_WRITES),
+#undef ENCODE_EXC_RESOURCE
+
+#define ENCODE_EXC_GUARD(type, flavor)                                         \
+  {                                                                            \
+    EXC_GUARD,                                                                 \
+        static_cast<mach_exception_code_t>(static_cast<uint64_t>((type) & 0x7) \
+                                           << 61) |                            \
+            (static_cast<uint64_t>((1 << (flavor)) & 0x1ffffffff) << 32),      \
+        (EXC_GUARD << 16) | ((type) << 8) | (flavor)                           \
+  }
+      ENCODE_EXC_GUARD(GUARD_TYPE_MACH_PORT, 0),  // kGUARD_EXC_DESTROY
+      ENCODE_EXC_GUARD(GUARD_TYPE_MACH_PORT, 1),  // kGUARD_EXC_MOD_REFS
+      ENCODE_EXC_GUARD(GUARD_TYPE_MACH_PORT, 2),  // kGUARD_EXC_SET_CONTEXT
+      ENCODE_EXC_GUARD(GUARD_TYPE_MACH_PORT, 3),  // kGUARD_EXC_UNGUARDED
+      ENCODE_EXC_GUARD(GUARD_TYPE_MACH_PORT, 4),  // kGUARD_EXC_INCORRECT_GUARD
+
+      // 2 is GUARD_TYPE_FD from 10.12.2 xnu-3789.31.2/bsd/sys/guarded.h.
+      ENCODE_EXC_GUARD(2, 0),  // kGUARD_EXC_CLOSE
+      ENCODE_EXC_GUARD(2, 1),  // kGUARD_EXC_DUP
+      ENCODE_EXC_GUARD(2, 2),  // kGUARD_EXC_NOCLOEXEC
+      ENCODE_EXC_GUARD(2, 3),  // kGUARD_EXC_SOCKET_IPC
+      ENCODE_EXC_GUARD(2, 4),  // kGUARD_EXC_FILEPORT
+      ENCODE_EXC_GUARD(2, 5),  // kGUARD_EXC_MISMATCH
+      ENCODE_EXC_GUARD(2, 6),  // kGUARD_EXC_WRITE
+#undef ENCODE_EXC_GUARD
+
+      // Test that overflow saturates.
+      {0x00010000, 0x00001000, static_cast<int32_t>(0xffff1000)},
+      {0x00001000, 0x00010000, 0x1000ffff},
+      {0x00010000, 0x00010000, static_cast<int32_t>(0xffffffff)},
+  };
+
+  for (size_t index = 0; index < arraysize(kTestData); ++index) {
+    const TestData& test_data = kTestData[index];
+    SCOPED_TRACE(base::StringPrintf("index %zu, exception 0x%x, code_0 0x%llx",
+                                    index,
+                                    test_data.exception,
+                                    test_data.code_0));
+
+    int32_t metrics_code =
+        ExceptionCodeForMetrics(test_data.exception, test_data.code_0);
+
+    EXPECT_EQ(test_data.metrics_code, metrics_code);
+  }
+}
 
 TEST(ExceptionTypes, IsExceptionNonfatalResource) {
   const pid_t pid = getpid();
 
-  mach_exception_code_t code = 0;
-  EXC_RESOURCE_ENCODE_TYPE(code, RESOURCE_TYPE_CPU);
-  EXC_RESOURCE_ENCODE_FLAVOR(code, FLAVOR_CPU_MONITOR);
+  mach_exception_code_t code =
+      EXC_RESOURCE_ENCODE_TYPE_FLAVOR(RESOURCE_TYPE_CPU, FLAVOR_CPU_MONITOR);
   EXPECT_TRUE(IsExceptionNonfatalResource(EXC_RESOURCE, code, pid));
 
   if (MacOSXMinorVersion() >= 10) {
     // FLAVOR_CPU_MONITOR_FATAL was introduced in OS X 10.10.
-    code = 0;
-    EXC_RESOURCE_ENCODE_TYPE(code, RESOURCE_TYPE_CPU);
-    EXC_RESOURCE_ENCODE_FLAVOR(code, FLAVOR_CPU_MONITOR_FATAL);
+    code = EXC_RESOURCE_ENCODE_TYPE_FLAVOR(RESOURCE_TYPE_CPU,
+                                           FLAVOR_CPU_MONITOR_FATAL);
     EXPECT_FALSE(IsExceptionNonfatalResource(EXC_RESOURCE, code, pid));
   }
 
   // This assumes that WAKEMON_MAKE_FATAL is not set for this process. The
   // default is for WAKEMON_MAKE_FATAL to not be set, there’s no public API to
   // enable it, and nothing in this process should have enabled it.
-  code = 0;
-  EXC_RESOURCE_ENCODE_TYPE(code, RESOURCE_TYPE_WAKEUPS);
-  EXC_RESOURCE_ENCODE_FLAVOR(code, FLAVOR_WAKEUPS_MONITOR);
+  code = EXC_RESOURCE_ENCODE_TYPE_FLAVOR(RESOURCE_TYPE_WAKEUPS,
+                                         FLAVOR_WAKEUPS_MONITOR);
   EXPECT_TRUE(IsExceptionNonfatalResource(EXC_RESOURCE, code, pid));
 
-  code = 0;
-  EXC_RESOURCE_ENCODE_TYPE(code, RESOURCE_TYPE_MEMORY);
-  EXC_RESOURCE_ENCODE_FLAVOR(code, FLAVOR_HIGH_WATERMARK);
+  code = EXC_RESOURCE_ENCODE_TYPE_FLAVOR(RESOURCE_TYPE_MEMORY,
+                                         FLAVOR_HIGH_WATERMARK);
   EXPECT_TRUE(IsExceptionNonfatalResource(EXC_RESOURCE, code, pid));
 
   // Non-EXC_RESOURCE exceptions should never be considered nonfatal resource
