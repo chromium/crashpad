7f90922bda75a748794096fb4205c84e37525faf
Sigurdur Asgeirsson <siggi@chromium.org>
2017-02-07 14:05:59 -0500

    Use best-effort allocation in ProcessInfo::BuildHandleVector.
    Use best-effort allocation in ProcessInfo::BuildHandleVector.
    
    BUG=158
    
    Change-Id: If8666140a7fc5315eeb791d0998226de89a22cc3

--- util/win/process_info.cc
+++ util/win/process_info.cc
@@ -22,6 +22,8 @@
 #include <type_traits>
 
 #include "base/logging.h"
+#include "base/memory/free_deleter.h"
+#include "base/process/memory.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "util/numeric/safe_assignment.h"
@@ -35,6 +37,19 @@
 namespace crashpad {
 
 namespace {
+
+using UniqueMallocPtr=std::unique_ptr<uint8_t[], base::FreeDeleter>;
+
+bool UncheckedAllocate(size_t size, UniqueMallocPtr *allocated_ptr) {
+  DCHECK(allocated_ptr);
+  allocated_ptr->reset();
+  void* raw_ptr = nullptr;
+  if (!base::UncheckedMalloc(size, &raw_ptr))
+    return false;
+
+  allocated_ptr->reset(static_cast<uint8_t*>(raw_ptr));
+  return true;
+}
 
 NTSTATUS NtQueryInformationProcess(HANDLE process_handle,
                                    PROCESSINFOCLASS process_information_class,
@@ -347,14 +362,16 @@ bool ReadMemoryInfo(HANDLE process, bool is_64_bit, ProcessInfo* process_info) {
 std::vector<ProcessInfo::Handle> ProcessInfo::BuildHandleVector(
     HANDLE process) const {
   ULONG buffer_size = 2 * 1024 * 1024;
-  std::unique_ptr<uint8_t[]> buffer(new uint8_t[buffer_size]);
-
   // Typically if the buffer were too small, STATUS_INFO_LENGTH_MISMATCH would
   // return the correct size in the final argument, but it does not for
   // SystemExtendedHandleInformation, so we loop and attempt larger sizes.
   NTSTATUS status;
   ULONG returned_length;
+  UniqueMallocPtr buffer;
   for (int tries = 0; tries < 5; ++tries) {
+    if (!UncheckedAllocate(buffer_size, &buffer))
+      return std::vector<Handle>();
+
     status = crashpad::NtQuerySystemInformation(
         static_cast<SYSTEM_INFORMATION_CLASS>(SystemExtendedHandleInformation),
         buffer.get(),
@@ -364,8 +381,6 @@ std::vector<ProcessInfo::Handle> ProcessInfo::BuildHandleVector(
       break;
 
     buffer_size *= 2;
-    buffer.reset();
-    buffer.reset(new uint8_t[buffer_size]);
   }
 
   if (!NT_SUCCESS(status)) {
