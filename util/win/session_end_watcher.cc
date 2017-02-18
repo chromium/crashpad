ae64d5a8647e4b296be69206a5cbf176599164ed
Mark Mentovai <mark@chromium.org>
2017-02-17 21:33:37 -0500

    metrics: Record handler lifetime milestone events
    It could be useful to put our existing Crashpad.HandlerCrashed metrics
    into context by getting a sense of handler starts, clean exits, and
    other types of exits.
    
    BUG=crashpad:100
    
    Change-Id: I8982075158ea6d210eb2ddad678302e339a42192

---
+++ util/win/session_end_watcher.cc
@@ -0,0 +1,217 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/win/session_end_watcher.h"
+
+#include "base/logging.h"
+
+extern "C" {
+extern IMAGE_DOS_HEADER __ImageBase;
+}  // extern "C"
+
+namespace crashpad {
+
+namespace {
+
+// Calls a lambda when going out of scope. This is “defer” for the poor.
+template <typename T>
+class ScopedCallLambda {
+ public:
+  ScopedCallLambda(const T& lambda) : lambda_(lambda) {}
+  ~ScopedCallLambda() { lambda_(); }
+
+ private:
+  const T& lambda_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedCallLambda);
+};
+
+// GetWindowLongPtr()’s return value doesn’t unambiguously indicate whether it
+// was successful, because 0 could either represent successful retrieval of the
+// value 0, or failure. This wrapper is more convenient to use.
+bool GetWindowLongPtrAndSuccess(HWND window, int index, LONG_PTR* value) {
+  SetLastError(ERROR_SUCCESS);
+  *value = GetWindowLongPtr(window, index);
+  return *value || GetLastError() == ERROR_SUCCESS;
+}
+
+// SetWindowLongPtr() has the same problem as GetWindowLongPtr(). Use this
+// wrapper instead.
+bool SetWindowLongPtrAndGetSuccess(HWND window, int index, LONG_PTR value) {
+  SetLastError(ERROR_SUCCESS);
+  LONG_PTR previous = SetWindowLongPtr(window, index, value);
+  return previous || GetLastError() == ERROR_SUCCESS;
+}
+
+}  // namespace
+
+SessionEndWatcher::SessionEndWatcher()
+    : Thread(),
+      window_(),
+      started_(CreateEvent(nullptr, true, false, nullptr)),
+      stopped_(CreateEvent(nullptr, true, false, nullptr)) {
+  PLOG_IF(ERROR, !started_.get()) << "CreateEvent";
+  PLOG_IF(ERROR, !stopped_.get()) << "CreateEvent";
+  Start();
+}
+
+SessionEndWatcher::~SessionEndWatcher() {
+  // Tear everything down by posting a WM_CLOSE to the window. This obviously
+  // can’t work until the window has been created, and that happens on a
+  // different thread, so wait for the start event to be signaled first.
+  WaitForStart();
+  if (window_) {
+    if (!PostMessage(window_, WM_CLOSE, 0, 0)) {
+      PLOG(ERROR) << "PostMessage";
+    }
+  }
+
+  Join();
+  DCHECK(!window_);
+}
+
+void SessionEndWatcher::WaitForStart() {
+  if (WaitForSingleObject(started_.get(), INFINITE) != WAIT_OBJECT_0) {
+    PLOG(ERROR) << "WaitForSingleObject";
+  }
+}
+
+void SessionEndWatcher::WaitForStop() {
+  if (WaitForSingleObject(stopped_.get(), INFINITE) != WAIT_OBJECT_0) {
+    PLOG(ERROR) << "WaitForSingleObject";
+  }
+}
+
+void SessionEndWatcher::ThreadMain() {
+  auto signal_stop_lambda = [this]() {
+    PLOG_IF(ERROR, !SetEvent(stopped_.get())) << "SetEvent";
+  };
+  ScopedCallLambda<decltype(signal_stop_lambda)> call_signal_stop(
+      signal_stop_lambda);
+
+  WNDCLASS wndclass = {};
+  ATOM atom;
+
+  {
+    auto signal_start_lambda = [this]() {
+      PLOG_IF(ERROR, !SetEvent(started_.get())) << "SetEvent";
+    };
+    ScopedCallLambda<decltype(signal_start_lambda)> call_signal_start(
+        signal_start_lambda);
+
+    wndclass.lpfnWndProc = WindowProc;
+    wndclass.hInstance = reinterpret_cast<HMODULE>(&__ImageBase);
+    wndclass.lpszClassName = L"crashpad_SessionEndWatcher";
+    atom = RegisterClass(&wndclass);
+    if (!atom) {
+      PLOG(ERROR) << "RegisterClass";
+      return;
+    }
+
+    window_ =
+        CreateWindow(MAKEINTATOM(atom), nullptr, 0, 0, 0, 0, 0, 0, 0, 0, this);
+    if (!window_) {
+      PLOG(ERROR) << "CreateWindow";
+      return;
+    }
+  }
+
+  MSG message;
+  BOOL rv = 0;
+  while (window_ && (rv = GetMessage(&message, window_, 0, 0)) > 0) {
+    TranslateMessage(&message);
+    DispatchMessage(&message);
+  }
+
+  if (window_) {
+    if (rv == -1) {
+      PLOG(ERROR) << "GetMessage";
+      return;
+    }
+
+    // If the message loop terminated because a WM_DESTROY message was
+    // processed, DestroyWindow() will have already been called and window_ will
+    // be nullptr. But if the message loop terminated because it processed a
+    // WM_QUIT message, the window still exists and needs to be cleaned up.
+    rv = DestroyWindow(window_);
+    if (!rv) {
+      PLOG(ERROR) << "DestroyWindow";
+      return;
+    }
+    window_ = 0;
+  }
+
+  rv = UnregisterClass(MAKEINTATOM(atom), 0);
+  if (!rv) {
+    PLOG(ERROR) << "UnregisterClass";
+    return;
+  }
+}
+
+// static
+LRESULT CALLBACK SessionEndWatcher::WindowProc(HWND window,
+                                               UINT message,
+                                               WPARAM w_param,
+                                               LPARAM l_param) {
+  // Figure out which object this is. A pointer to it is stuffed into the last
+  // parameter of CreateWindow(), which shows up as CREATESTRUCT::lpCreateParams
+  // in a WM_CREATE message. That should be processed before any of the other
+  // messages of interest to this function. Once the object is known, save a
+  // pointer to it in the GWLP_USERDATA slot for later retrieval when processing
+  // other messages.
+  SessionEndWatcher* self;
+  if (!GetWindowLongPtrAndSuccess(
+          window, GWLP_USERDATA, reinterpret_cast<LONG_PTR*>(&self))) {
+    PLOG(ERROR) << "GetWindowLongPtr";
+  }
+  if (!self && message == WM_CREATE) {
+    CREATESTRUCT* create = reinterpret_cast<CREATESTRUCT*>(l_param);
+    self = reinterpret_cast<SessionEndWatcher*>(create->lpCreateParams);
+    if (!SetWindowLongPtrAndGetSuccess(
+            window, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(self))) {
+      PLOG(ERROR) << "SetWindowLongPtr";
+    }
+  }
+
+  if (self) {
+    if (message == WM_ENDSESSION) {
+      // If w_param is false, this WM_ENDSESSION cancels a previous
+      // WM_QUERYENDSESSION.
+      if (w_param) {
+        self->SessionEnding();
+
+        // If the session is ending, post a close message which will kick off
+        // window destruction and cause the message loop thread to terminate.
+        if (!PostMessage(self->window_, WM_CLOSE, 0, 0)) {
+          PLOG(ERROR) << "PostMessage";
+        }
+      }
+    } else if (message == WM_DESTROY) {
+      // The window is being destroyed. Clear GWLP_USERDATA so that |self| won’t
+      // be found during a subsequent call into this function for this window.
+      // Clear self->window_ too, because it refers to an object that soon won’t
+      // exist. That signals the message loop to stop processing messages.
+      if (!SetWindowLongPtrAndGetSuccess(window, GWLP_USERDATA, 0)) {
+        PLOG(ERROR) << "SetWindowLongPtr";
+      }
+      self->window_ = 0;
+    }
+  }
+
+  // If |message| was WM_CLOSE, DefWindowProc() will call DestroyWindow(), and
+  // this function will be called again with WM_DESTROY.
+  return DefWindowProc(window, message, w_param, l_param);
+}
+
+}  // namespace crashpad
