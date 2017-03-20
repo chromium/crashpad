c5ee8f81088e15795c3980137520382a3ab60478
Sigurdur Asgeirsson <siggi@chromium.org>
2017-03-20 14:47:45 -0400

    Add user extensibility interface to add new streams computed at crash time.
    Bug: crashpad:167
    Change-Id: I94c143353482f100a31d6afb0685b38757a662d6

---
+++ minidump/minidump_user_extension_stream.h
@@ -0,0 +1,44 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_MINIDUMP_MINIDUMP_USER_EXTENSION_STREAM_H_
+#define CRASHPAD_MINIDUMP_MINIDUMP_USER_EXTENSION_STREAM_H_
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "base/macros.h"
+
+namespace crashpad {
+
+//! \brief TODO(siggi): document me. DO NOT SUBMIT.
+class MinidumpUserExtensionStream final {
+ public:
+  MinidumpUserExtensionStream();
+  ~MinidumpUserExtensionStream();
+
+  uint32_t stream_type() const { return stream_type_; }
+
+ private:
+  uint32_t stream_type_;
+  // TODO(siggi): Should this reference the user's buffer, or copy?
+  std::vector<uint8_t> data_;
+
+  DISALLOW_COPY_AND_ASSIGN(MinidumpUserExtensionStream);
+};
+
+}  // namespace crashpad
+
+#endif  // CRASHPAD_MINIDUMP_MINIDUMP_USER_EXTENSION_STREAM_H_
