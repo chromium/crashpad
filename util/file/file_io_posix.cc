10173722d7ee2d190a63e8e5c10ca1591bfcec66
Mark Mentovai <mark@chromium.org>
2017-03-21 14:49:45 -0400

    util/file: Handle oversized reads and writes gracefully
    file_io and the FileReader family had a few loose ends regarding big
    reads and writes. It’s not likely that we’ve experienced these
    conditions yet, but they’d be likely to appear in a potential future
    involving full memory dumps. This specifies the behavior with large
    reads and writes, consolidates some logic, and improves some interfaces.
    
    ReadFile() should always return without retrying after a short read, and
    in fact does return after short reads since 00b64427523b. It is
    straightforward to limit the maximum read size based on a parameter
    limitation of the underlying operation, or a limitation of the type used
    for FileOperationResult.
    
    In contrast, WriteFile() should always retry after a short write,
    including a write shortened because of a parameter limitation of the
    underlying operation. This allows its return value to be simplified to a
    “bool”.
    
    The platform-specific WriteFile() code has been moved to
    internal::WriteFileInternal(), and WriteFile() is now a
    platform-independent loop that retries following a short write.
    
    The platform-agnostic ReadFileExactlyInternal() implementation has been
    refactored into internal::ReadExactlyInternal so that it can be used by
    FileReaderInterface::ReadExactly(), which had a nearly identical
    implementation.
    
    Test: *
    Change-Id: I487450322ab049c6f2acd4061ea814037cc9a864

--- util/file/file_io_posix.cc
+++ util/file/file_io_posix.cc
@@ -19,10 +19,19 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <algorithm>
+#include <limits>
+
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/numerics/safe_conversions.h"
 #include "base/posix/eintr_wrapper.h"
+
+namespace {
+
+constexpr size_t kMaxReadWriteSize =
+    static_cast<size_t>(std::numeric_limits<ssize_t>::max());
+
+}  // namespace
 
 namespace crashpad {
 
@@ -60,44 +69,38 @@ FileHandle OpenFileForOutput(int rdwr_or_wronly,
 
 }  // namespace
 
+namespace internal {
+
 const char kNativeReadFunctionName[] = "read";
 const char kNativeWriteFunctionName[] = "write";
 
-// TODO(mark): Handle > ssize_t-sized reads and writes if necessary. The
-// standard leaves this implementation-defined. Some systems return EINVAL in
-// this case. ReadFile() and WriteFile() could enforce this behavior.
+FileOperationResult NativeWriteFile(FileHandle file,
+                                    const void* buffer,
+                                    size_t size) {
+  const size_t write_size = std::min(size, kMaxReadWriteSize);
 
-FileOperationResult ReadFile(FileHandle file, void* buffer, size_t size) {
-  FileOperationResult bytes = HANDLE_EINTR(read(file, buffer, size));
-  if (bytes < 0) {
+  FileOperationResult bytes_written =
+      HANDLE_EINTR(write(file, buffer, write_size));
+  if (bytes_written < 0) {
     return -1;
   }
 
-  DCHECK_LE(static_cast<size_t>(bytes), size);
-  return bytes;
+  DCHECK_LE(static_cast<size_t>(bytes_written), write_size);
+  return bytes_written;
 }
 
-FileOperationResult WriteFile(FileHandle file,
-                              const void* buffer,
-                              size_t size) {
-  const char* buffer_c = static_cast<const char*>(buffer);
+}  // namespace internal
 
-  FileOperationResult total_bytes = 0;
-  while (size > 0) {
-    FileOperationResult bytes = HANDLE_EINTR(write(file, buffer_c, size));
-    if (bytes < 0) {
-      return -1;
-    }
+FileOperationResult ReadFile(FileHandle file, void* buffer, size_t size) {
+  const size_t read_size = std::min(size, kMaxReadWriteSize);
 
-    DCHECK_NE(bytes, 0);
-    DCHECK_LE(static_cast<size_t>(bytes), size);
-
-    buffer_c += bytes;
-    size -= bytes;
-    total_bytes += bytes;
+  FileOperationResult bytes_read = HANDLE_EINTR(read(file, buffer, read_size));
+  if (bytes_read < 0) {
+    return -1;
   }
 
-  return total_bytes;
+  DCHECK_LE(static_cast<size_t>(bytes_read), read_size);
+  return bytes_read;
 }
 
 FileHandle OpenFileForRead(const base::FilePath& path) {
