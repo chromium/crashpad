c42064963ad72447d6f0a9362ba505529b67737a
Mark Mentovai <mark@chromium.org>
2017-03-30 21:16:19 -0400

    posix: Allow ScopedMmap::ResetAddrLen() to deal with overlap
    It should be possible to shrink a region already supervised by
    ScopedMmap, or in rare cases when ScopedMmap is supervising only a
    smaller portion of an overall larger region, increase the size of the
    region it supervises. This is now equivalent to the operation of
    base::mac::ScopedMachVM::reset().
    
    The Reset() and ResetAddrLen() methods are upgraded from a void return
    to a bool return to indicate their success.
    
    Bug: crashpad:30
    Test: crashpad_util_test ScopedMmap*.ResetAddrLen_*
    Change-Id: I564e154cd2387e8df3f83b416ecc1c83c9bcf71d

--- util/posix/scoped_mmap.cc
+++ util/posix/scoped_mmap.cc
@@ -14,7 +14,22 @@
 
 #include "util/posix/scoped_mmap.h"
 
+#include <unistd.h>
+
 #include "base/logging.h"
+
+namespace {
+
+bool Munmap(uintptr_t addr, size_t len) {
+  if (munmap(reinterpret_cast<void*>(addr), len) != 0) {
+    LOG(ERROR) << "munmap";
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace
 
 namespace crashpad {
 
@@ -24,17 +39,33 @@ ScopedMmap::~ScopedMmap() {
   Reset();
 }
 
-void ScopedMmap::Reset() {
-  ResetAddrLen(MAP_FAILED, 0);
+bool ScopedMmap::Reset() {
+  return ResetAddrLen(MAP_FAILED, 0);
 }
 
-void ScopedMmap::ResetAddrLen(void* addr, size_t len) {
-  if (is_valid() && munmap(addr_, len_) != 0) {
-    LOG(ERROR) << "munmap";
+bool ScopedMmap::ResetAddrLen(void* addr, size_t len) {
+  const uintptr_t new_addr = reinterpret_cast<uintptr_t>(addr);
+
+  DCHECK(addr == MAP_FAILED || new_addr % getpagesize() == 0);
+  DCHECK_EQ(len % getpagesize(), 0u);
+
+  bool rv = true;
+
+  if (addr_ != MAP_FAILED) {
+    const uintptr_t old_addr = reinterpret_cast<uintptr_t>(addr_);
+    if (old_addr < new_addr) {
+      rv &= Munmap(old_addr, std::min(len_, new_addr - old_addr));
+    }
+    if (old_addr + len_ > new_addr + len) {
+      uintptr_t deallocate_start = std::max(old_addr, new_addr + len);
+      rv &= Munmap(deallocate_start, old_addr + len_ - deallocate_start);
+    }
   }
 
   addr_ = addr;
   len_ = len;
+
+  return rv;
 }
 
 bool ScopedMmap::ResetMmap(void* addr,
