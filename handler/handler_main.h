c4b33e39fb494e6d2f8c16fe8b3e8b27442f523a
Sigurdur Asgeirsson <siggi@chromium.org>
2017-04-03 14:17:12 -0400

    handler: Add user extensibility stream call-out.
    Bug: crashpad:167
    Change-Id: I79b0b71dc4f61e6dce6bc10083e2f924dc83c940

--- handler/handler_main.h
+++ handler/handler_main.h
@@ -15,13 +15,85 @@
 #ifndef CRASHPAD_HANDLER_HANDLER_MAIN_H_
 #define CRASHPAD_HANDLER_HANDLER_MAIN_H_
 
+#include <stdint.h>
+#include <sys/types.h>
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 namespace crashpad {
+
+class ProcessSnapshot;
+
+class UserStreamDataSource {
+ public:
+  class Delegate {
+   public:
+    virtual ~Delegate() {}
+
+    //! \brief Called by  UserStreamDataSource::Read() to provide data requested
+    //!     by a call to that method.
+    //!
+    //! \param[in] data A pointer to the data that was read. The callee does not
+    //!     take ownership of this data. This data is only valid for the
+    //!     duration of the call to this method. This parameter may be `nullptr`
+    //!     if \a size is `0`.
+    //! \param[in] size The size of the data that was read.
+    //!
+    //! \return `true` on success, `false` on failure.
+    //!     UserStreamDataSource::ReadStreamData() will use this as its own
+    //!     return value.
+    virtual bool UserStreamDataSourceRead(const void* data, size_t size) = 0;
+  };
+
+  virtual ~UserStreamDataSource() {}
+
+  // TODO(siggi): document me.
+  virtual bool Initialize(ProcessSnapshot* process_snapshot) = 0;
+
+  // TODO(siggi): document me.
+  virtual size_t StreamDataSize() = 0;
+
+  //! \brief Calls Delegate::UserStreamDataSourceRead(), providing it with
+  //!     the stream data.
+  //!
+  //! Implementations do not necessarily comute the stream data prior to
+  //! this method being called. The stream data may be computed or loaded lazily
+  //! and may be discarded after being passed to the delegate.
+  //!
+  //! \return `false` on failure, otherwise, the return value of
+  //!     Delegate::UserStreamDataSourceRead(), which should be `true` on
+  //!     success and `false` on failure.
+  virtual bool ReadStreamData(Delegate* delegate) = 0;
+};
+
+using UserStreamSources =
+    std::map<uint32_t, std::unique_ptr<UserStreamDataSource>>;
 
 //! \brief The `main()` of the `crashpad_handler` binary.
 //!
 //! This is exposed so that `crashpad_handler` can be embedded into another
 //! binary, but called and used as if it were a standalone executable.
 int HandlerMain(int argc, char* argv[]);
+
+//! \brief The `main()` of the `crashpad_handler` binary with extensibility.
+//!
+//! This is allows running the Crashpad handler embedded into another binary,
+//! with user extensibility stream sources.
+//!
+//! \param[in] user_stream_sources A map containing the extensibility data
+//!     sources to call on crash. The resulting data streams will be added to
+//!     the captured minidump.
+int HandlerMainWithExtensibility(int argc,
+                                 char* argv[],
+                                 const UserStreamSources* user_stream_sources);
 
 }  // namespace crashpad
 
