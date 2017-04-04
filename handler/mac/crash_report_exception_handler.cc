136e96ba4d85206d4c23a2cfa5739aacb9d90a94
Sigurdur Asgeirsson <siggi@chromium.org>
2017-04-04 14:40:41 -0400

    handler: Add user extensibility stream call-out.
    Bug: crashpad:167
    Change-Id: I79b0b71dc4f61e6dce6bc10083e2f924dc83c940

--- handler/mac/crash_report_exception_handler.cc
+++ handler/mac/crash_report_exception_handler.cc
@@ -22,6 +22,7 @@
 #include "base/strings/stringprintf.h"
 #include "client/settings.h"
 #include "minidump/minidump_file_writer.h"
+#include "minidump/minidump_user_extension_stream_data_source.h"
 #include "snapshot/crashpad_info_client_options.h"
 #include "snapshot/mac/process_snapshot_mac.h"
 #include "util/file/file_writer.h"
@@ -41,11 +42,12 @@ namespace crashpad {
 CrashReportExceptionHandler::CrashReportExceptionHandler(
     CrashReportDatabase* database,
     CrashReportUploadThread* upload_thread,
-    const std::map<std::string, std::string>* process_annotations)
+    const std::map<std::string, std::string>* process_annotations,
+    const UserStreamSources* user_stream_sources)
     : database_(database),
       upload_thread_(upload_thread),
-      process_annotations_(process_annotations) {
-}
+      process_annotations_(process_annotations),
+      user_stream_sources_(user_stream_sources) {}
 
 CrashReportExceptionHandler::~CrashReportExceptionHandler() {
 }
@@ -169,6 +171,21 @@ kern_return_t CrashReportExceptionHandler::CatchMachException(
 
     MinidumpFileWriter minidump;
     minidump.InitializeFromSnapshot(&process_snapshot);
+
+    if (user_stream_sources_) {
+      for (const auto& source : *user_stream_sources_) {
+        std::unique_ptr<MinidumpUserExtensionStreamDataSource> data_source(
+            source->ProduceStreamData(&process_snapshot));
+        if (data_source &&
+            !minidump.AddUserExtensionStream(std::move(data_source))) {
+          // This should only happen if multiple user stream sources yield the
+          // same stream type. It's the users responsibility to make sure
+          // sources don't collide on the same stream type.
+          LOG(ERROR) << "AddUserExtensionStream failed";
+        }
+      }
+    }
+
     if (!minidump.WriteEverything(&file_writer)) {
       Metrics::ExceptionCaptureResult(
           Metrics::CaptureResult::kMinidumpWriteFailed);
