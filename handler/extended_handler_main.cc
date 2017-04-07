0fff0fce56c9db92041f3a40efc9d4f848ddad63
Sigurdur Asgeirsson <siggi@chromium.org>
2017-04-07 16:51:31 -0400

    handler: Add user extensibility stream call-out.
    Bug: crashpad:167
    Change-Id: I79b0b71dc4f61e6dce6bc10083e2f924dc83c940

---
+++ handler/extended_handler_main.cc
@@ -0,0 +1,62 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "handler/handler_main.h"
+
+#include "base/memory/ptr_util.h"
+#include "build/build_config.h"
+#include "minidump/test/minidump_user_extension_stream_util.h"
+#include "tools/tool_support.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
+namespace {
+
+class TestUserStreamDataSource : public crashpad::UserStreamDataSource {
+ public:
+  std::unique_ptr<crashpad::MinidumpUserExtensionStreamDataSource>
+  ProduceStreamData(crashpad::ProcessSnapshot* process_snapshot) override;
+};
+
+std::unique_ptr<crashpad::MinidumpUserExtensionStreamDataSource>
+TestUserStreamDataSource::ProduceStreamData(
+    crashpad::ProcessSnapshot* process_snapshot) {
+  static const char kTestData[] = "Injected extension stream!";
+
+  return base::WrapUnique(new crashpad::test::BufferExtensionStreamDataSource(
+      0xCAFEBABE, kTestData, sizeof(kTestData)));
+}
+
+int ExtendedHandlerMain(int argc, char* argv[]) {
+  crashpad::UserStreamDataSources user_stream_data_sources;
+  user_stream_data_sources.push_back(
+      base::WrapUnique(new TestUserStreamDataSource()));
+
+  return crashpad::HandlerMain(argc, argv, &user_stream_data_sources);
+}
+
+}  // namespace
+
+#if defined(OS_MACOSX)
+int main(int argc, char* argv[]) {
+  return crashpad::HandlerMain(argc, argv, &test_source);
+}
+#elif defined(OS_WIN)
+
+int APIENTRY wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) {
+  return crashpad::ToolSupport::Wmain(__argc, __wargv, &ExtendedHandlerMain);
+}
+#endif  // OS_MACOSX
