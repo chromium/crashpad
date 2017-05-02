chromium/crashpad

commit 8af3203d811c27c0e7d2f3fd96bbb41692211404

Add LoggingReadEntireFile for reading a file into a string
Change-Id: Ie07ef12131ef1d995aa78749091f3adacde75160
Reviewed-on: https://chromium-review.googlesource.com/492446
Commit-Queue: Joshua Peraza <jperaza@chromium.org>
Reviewed-by: Mark Mentovai <mark@chromium.org>

util/file/file_io.cc
@@ -156,6 +156,26 @@ void CheckedReadFileAtEOF(FileHandle file) {
   }
 }
 
+bool LoggingReadEntireFile(const base::FilePath& path, std::string* contents) {
+  FileHandle handle = LoggingOpenFileForRead(path);
+  if (handle == kInvalidFileHandle) {
+    return false;
+  }
+
+  char buffer[4096];
+  FileOperationResult rv;
+  std::string local_contents;
+  while ((rv = ReadFile(handle, buffer, sizeof(buffer))) > 0) {
+    local_contents.append(buffer, rv);
+  }
+  if (rv < 0) {
+    PLOG(ERROR) << internal::kNativeReadFunctionName;
+    return false;
+  }
+  contents->swap(local_contents);
+  return true;
+}
+
 void CheckedCloseFile(FileHandle file) {
   CHECK(LoggingCloseFile(file));
 }

util/file/file_io.h
@@ -17,6 +17,8 @@
 
 #include <sys/types.h>
 
+#include <string>
+
 #include "build/build_config.h"
 
 #if defined(OS_POSIX)
@@ -305,6 +307,12 @@ void CheckedWriteFile(FileHandle file, const void* buffer, size_t size);
 //! \sa CheckedReadFileExactly
 //! \sa ReadFile
 void CheckedReadFileAtEOF(FileHandle file);
+
+//! brief Wraps LoggingOpenFileForRead() and ReadFile() reading the entire file
+//!     into \a contents.
+//!
+//! \return `true` on success, or `false` with a message logged.
+bool LoggingReadEntireFile(const base::FilePath& path, std::string* contents);
 
 //! \brief Wraps `open()` or `CreateFile()`, opening an existing file for
 //!     reading.

util/posix/process_info_linux.cc
@@ -31,6 +31,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
 #include "util/file/delimited_file_reader.h"
+#include "util/file/file_io.h"
 #include "util/file/file_reader.h"
 #include "util/linux/scoped_ptrace_attach.h"
 
@@ -76,20 +77,6 @@ bool AdvancePastNumber(const char** input, T* value) {
     return true;
   }
   return false;
-}
-
-bool ReadEntireFile(const char* path, std::string* contents) {
-  FileReader file;
-  if (!file.Open(base::FilePath(path))) {
-    return false;
-  }
-
-  char buffer[4096];
-  FileOperationResult length;
-  while ((length = file.Read(buffer, sizeof(buffer))) > 0) {
-    contents->append(buffer, length);
-  }
-  return length >= 0;
 }
 
 void SubtractTimespec(const timespec& t1, const timespec& t2,
@@ -391,7 +378,7 @@ bool ProcessInfo::StartTime(timeval* start_time) const {
     char path[32];
     snprintf(path, sizeof(path), "/proc/%d/stat", pid_);
     std::string stat_contents;
-    if (!ReadEntireFile(path, &stat_contents)) {
+    if (!LoggingReadEntireFile(base::FilePath(path), &stat_contents)) {
       return false;
     }
 

commit 605424e58599ff0381a12ea14f42cce499aedc1c

Fix file descriptor/handle leak in LoggingReadEntireFile()
8af3203d811c introduced a resource leak.

Change-Id: Ia909eef39b6b772d8808dd6f5770c06add6467bc

util/file/file_io.cc
@@ -157,15 +157,15 @@ void CheckedReadFileAtEOF(FileHandle file) {
 }
 
 bool LoggingReadEntireFile(const base::FilePath& path, std::string* contents) {
-  FileHandle handle = LoggingOpenFileForRead(path);
-  if (handle == kInvalidFileHandle) {
+  ScopedFileHandle handle(LoggingOpenFileForRead(path));
+  if (!handle.is_valid()) {
     return false;
   }
 
   char buffer[4096];
   FileOperationResult rv;
   std::string local_contents;
-  while ((rv = ReadFile(handle, buffer, sizeof(buffer))) > 0) {
+  while ((rv = ReadFile(handle.get(), buffer, sizeof(buffer))) > 0) {
     local_contents.append(buffer, rv);
   }
   if (rv < 0) {