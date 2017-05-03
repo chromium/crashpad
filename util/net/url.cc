5e6985c7ea3a3551dca9dec8cb088b00cafcede2
Roman Margold <rmargold@chromium.org>
2017-05-02 18:42:19 -0700

    Client identifies itself via URL parameters
    During crash report upload, the client provides the product name,
    version, and client id via URL parameters to the crash reporting
    service.
    Also added percent-encoding function and a test.
    
    More details in the following document: https://docs.google.com/a/google.com/document/d/1i4Se4EOxNZD9IzrJxHi0pr6q3QSqqC3eL5oMGy_RP3w/edit?usp=sharing
    
    Bug: crashpad:
    Change-Id: I62f3a646d4ab6029543bd80938b79de28b1f20e4

---
+++ util/net/url.cc
@@ -0,0 +1,45 @@
+// Copyright 2014 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/net/url.h"
+
+#include <cctype>
+
+namespace crashpad {
+
+std::string URLEncode(const std::string& url) {
+  const char kDecToHex[16 + 1] = "0123456789ABCDEF";
+  std::string encoded;
+  encoded.reserve(url.length());
+
+  for (size_t i = 0; i < url.length(); ++i) {
+    unsigned char character = static_cast<unsigned char>(url[i]);
+    if (isalnum(character) || character == '-' || character == '_' ||
+        character == '.' || character == '~') {
+      // Copy unreserved character
+      encoded += character;
+    }
+    else
+    {
+      // Escape character
+      encoded += '%';
+      encoded += kDecToHex[character >> 4];
+      encoded += kDecToHex[character & 0xF];
+    }
+  }
+
+  return encoded;
+}
+
+}  // namespace crashpad
