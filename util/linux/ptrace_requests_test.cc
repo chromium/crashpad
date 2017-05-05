c7d5e019388170ca78d65e0042c33c1c4fd43aee
Joshua Peraza <jperaza@chromium.org>
2017-05-04 19:28:40 -0700

    linux: Provide alternative to sys/user.h structs
    sys/user.h only defines register structs for the target archiecture.
    This change provides ptrace_requests.h which defines structs equivalent
    to those in sys/user.h for both 32 and 64-bit variants of the target
    architecture. This change also provides functions for collecting general
    purpose and floating point registers.
    
    Bug: crashpad:30
    Change-Id: I91d0d788927bdac5fb630a6ad3c6ea6d3645ef8a

---
+++ util/linux/ptrace_requests_test.cc
@@ -0,0 +1,90 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/linux/ptrace_requests.h"
+
+#include "gtest/gtest.h"
+#include "test/multiprocess.h"
+#include "util/file/file_io.h"
+#include "util/linux/scoped_ptrace_attach.h"
+
+namespace crashpad {
+namespace test {
+namespace {
+
+class SameBitnessTest : public Multiprocess {
+ public:
+  SameBitnessTest() : Multiprocess() {}
+  ~SameBitnessTest() {}
+
+ private:
+  void MultiprocessParent() override {
+    ScopedPtraceAttach attachment;
+    ASSERT_TRUE(attachment.ResetAttach(ChildPID()));
+
+#if defined(ARCH_CPU_64_BITS)
+    auto expected = GetRegistersResult::k64Bit;
+#else
+    auto expected = GetRegistersResult::k32Bit;
+#endif
+
+    ThreadContext thread_context;
+    EXPECT_EQ(GetGeneralPurposeRegisters(ChildPID(), &thread_context),
+              expected);
+
+    FloatContext float_context;
+    EXPECT_EQ(GetFloatingPointRegisters(ChildPID(), &float_context), expected);
+  }
+
+  void MultiprocessChild() override { CheckedReadFileAtEOF(ReadPipeHandle()); }
+
+  DISALLOW_COPY_AND_ASSIGN(SameBitnessTest);
+};
+
+TEST(PtraceRequests, SameBitness) {
+  SameBitnessTest test;
+  test.Run();
+}
+
+class UnattachedTest : public Multiprocess {
+ public:
+  UnattachedTest() : Multiprocess() {}
+  ~UnattachedTest() {}
+
+ private:
+  void MultiprocessParent() override {
+    ThreadContext thread_context;
+    EXPECT_EQ(GetGeneralPurposeRegisters(ChildPID(), &thread_context),
+              GetRegistersResult::kError);
+
+    FloatContext float_context;
+    EXPECT_EQ(GetFloatingPointRegisters(ChildPID(), &float_context),
+              GetRegistersResult::kError);
+  }
+
+  void MultiprocessChild() override { CheckedReadFileAtEOF(ReadPipeHandle()); }
+
+  DISALLOW_COPY_AND_ASSIGN(UnattachedTest);
+};
+
+TEST(PtraceRequests, UnattachedRequestsReturnErrors) {
+  UnattachedTest test;
+  test.Run();
+}
+
+// TODO(jperaza): Test against a process with different bitness.
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
