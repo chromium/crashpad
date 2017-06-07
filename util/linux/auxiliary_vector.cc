c229cb33a0f77d02f56faa972cf21f2d8849aac8
Joshua Peraza <jperaza@chromium.org>
2017-06-07 11:04:12 -0700

    linux: Add AuxiliaryVector for reading other process' aux vectors
    Bug: crashpad:30
    Change-Id: Ief19be7d60decb17f159b3d740ac9d15a034b807

---
+++ util/linux/auxiliary_vector.cc
@@ -0,0 +1,54 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/linux/auxiliary_vector.h"
+
+#include <stdio.h>
+
+#include "base/files/file_path.h"
+#include "util/file/file_reader.h"
+
+namespace crashpad {
+
+AuxiliaryVector::AuxiliaryVector() : values_() {}
+
+AuxiliaryVector::~AuxiliaryVector() {}
+
+bool AuxiliaryVector::Initialize(pid_t tid, bool is_64_bit) {
+  return is_64_bit ? Read<uint64_t>(tid)
+                   : Read<uint32_t>(tid);
+}
+
+template <typename ULong>
+bool AuxiliaryVector::Read(pid_t tid) {
+  char path[32];
+  snprintf(path, sizeof(path), "/proc/%d/auxv", tid);
+  FileReader aux_file;
+  if (!aux_file.Open(base::FilePath(path))) {
+    return false;
+  }
+
+  ULong type;
+  ULong value;
+  while (aux_file.ReadExactly(&type, sizeof(type)) &&
+         aux_file.ReadExactly(&value, sizeof(value))) {
+    if (type == 0 && value == 0) {
+      return true;
+    }
+    values_[type] = value;
+  }
+  return false;
+}
+
+}  // namespace crashpad
---
+++ util/linux/auxiliary_vector_test.cc
@@ -0,0 +1,75 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/linux/auxiliary_vector.h"
+
+#include <linux/auxvec.h>
+#include <unistd.h>
+
+#include "base/macros.h"
+#include "gtest/gtest.h"
+#include "test/multiprocess.h"
+
+namespace crashpad {
+namespace test {
+namespace {
+
+void TestAgainstCloneOrSelf(pid_t pid) {
+#if defined(ARCH_CPU_64_BITS)
+  constexpr bool am_64_bit = true;
+#else
+  constexpr bool am_64_bit = false;
+#endif
+  AuxiliaryVector aux;
+  ASSERT_TRUE(aux.Initialize(pid, am_64_bit));
+
+  gid_t gid;
+  ASSERT_TRUE(aux.GetValue(AT_GID, &gid));
+  EXPECT_EQ(gid, getgid());
+
+  int pagesize;
+  ASSERT_TRUE(aux.GetValue(AT_PAGESZ, &pagesize));
+  EXPECT_EQ(pagesize, getpagesize());
+}
+
+TEST(AuxiliaryVector, ReadSelf) {
+  TestAgainstCloneOrSelf(getpid());
+}
+
+class ReadChildTest : public Multiprocess {
+ public:
+  ReadChildTest() : Multiprocess() {}
+  ~ReadChildTest() {}
+
+ private:
+  void MultiprocessParent() override {
+    TestAgainstCloneOrSelf(ChildPID());
+  }
+
+  void MultiprocessChild() override {
+    CheckedReadFileAtEOF(ReadPipeHandle());
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(ReadChildTest);
+};
+
+TEST(AuxiliaryVector, ReadChild) {
+  ReadChildTest test;
+  test.Run();
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
+
