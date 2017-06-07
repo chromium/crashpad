e636e6ebfe9197f9bb3b94277d7f6d3006f0090d
Joshua Peraza <jperaza@chromium.org>
2017-06-06 18:01:54 -0700

    linux: Add AuxiliaryVector for reading other process' aux vectors
    Bug: crashpad:30
    Change-Id: Ief19be7d60decb17f159b3d740ac9d15a034b807

---
+++ util/linux/auxiliary_vector.h
@@ -0,0 +1,69 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_UTIL_LINUX_AUXILIARY_VECTOR_H_
+#define CRASHPAD_UTIL_LINUX_AUXILIARY_VECTOR_H_
+
+#include <sys/types.h>
+
+#include <unordered_map>
+
+#include "base/macros.h"
+#include "util/linux/address_types.h"
+
+namespace crashpad {
+
+//! \brief Read the auxiliary vector for a target thread.
+template <typename ULong>
+class AuxiliaryVector {
+ public:
+  AuxiliaryVector();
+  ~AuxiliaryVector();
+
+  //! \brief Initializes this object with the auxiliary vector for thread with
+  //!     thread ID \a tid.
+  //!
+  //! This method must be called successfully prior to calling any other method
+  //! in this class.
+  //!
+  //! \param[in] tid The thread ID of a target thread
+  //!
+  //! \return `true` on success, `false` on failure with a message logged.
+  bool Initialize(pid_t tid);
+
+  //! \brief Retrieve a value from the vector.
+  //!
+  //! \param[in] type Specifies which value should be retrieved. The possible
+  //!   values for the parameter are defined by linux/auxvec.h.
+  //! \param[out] value The value, casted to an appropriate type, if found.
+  //! \return `true` if the value is found.
+  template <typename V>
+  bool GetValue(ULong type, V* value) const {
+    auto iter = values_.find(type);
+    if (iter != values_.end()) {
+      *value = static_cast<V>(iter->second);
+      return true;
+    }
+    return false;
+  }
+
+ private:
+  std::unordered_map<ULong, ULong> values_;
+
+  DISALLOW_COPY_AND_ASSIGN(AuxiliaryVector<ULong>);
+};
+
+}  // namespace crashpad
+
+#endif  // CRASHPAD_UTIL_LINUX_AUXILIARY_VECTOR_H_
