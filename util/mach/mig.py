099a7bcda1e3c25258fb8870c86bb72f939f60f5
Mark Mentovai <mark@chromium.org>
2017-06-20 09:50:37 -0400

    mac: Provide a larger thread state buffer for AVX-512 on 10.13
    Crashpad doesn’t use AVX-512, but when receiving replies to exceptions
    forwarded to ReportCrash, may see buffers large enough to contain
    AVX-512 thread state. This can result in messages like
    “UniversalExceptionRaise: (ipc/rcv) msg too large (0x10004004)”.
    
    I386_THREAD_STATE_MAX has increased from 224 to 614 in the 10.13 SDK,
    meaning that the maximum supported size for old_state and new_state in
    [mach_]exception_raise_state[_identity]() has increased from 896 to
    2,456 bytes. This constant defines the size of the buffer that these
    MIG-generated routines will work with. By providing this definition in
    compat, the buffer size is increased when building with older SDKs.
    
    Note that on the “send” side, the size of the message given to
    mach_msg() will be trimmed to include only the valid part of the state
    area based on the stateCnt field, so increasing the value to 614 here
    won’t result Crashpad sending messages this large. That would be a
    potential interoperability concern with older OS versions.
    
    Bug: crashpad:185, crashpad:190
    Change-Id: Ia46091ae46fd6227a17f59eb4bc00914be471aa7

--- util/mach/mig.py
+++ util/mach/mig.py
@@ -111,6 +111,10 @@ def main(args):
   parser = argparse.ArgumentParser()
   parser.add_argument('--developer-dir', help='Path to Xcode')
   parser.add_argument('--sdk', help='Path to SDK')
+  parser.add_argument('--include',
+                      default=[],
+                      action='append',
+                      help='Additional include directory')
   parser.add_argument('defs')
   parser.add_argument('user_c')
   parser.add_argument('server_c')
@@ -124,10 +128,12 @@ def main(args):
              '-header', parsed.user_h,
              '-sheader', parsed.server_h,
             ]
-  if parsed.sdk is not None:
-    command.extend(['-isysroot', parsed.sdk])
   if parsed.developer_dir is not None:
     os.environ['DEVELOPER_DIR'] = parsed.developer_dir
+  if parsed.sdk is not None:
+    command.extend(['-isysroot', parsed.sdk])
+  for include in parsed.include:
+    command.extend(['-I' + include])
   command.append(parsed.defs)
   subprocess.check_call(command)
   FixUserImplementation(parsed.user_c)
