ee640a25c4283102d836c1be79eb90db72786eaa
Dave Bort <dbort@google.com>
2017-09-13 11:14:41 -0700

    RFC: Make ProcessMemory an abstract interface
    Bug: crashpad:196
    Change-Id: I05966db1599a9cac3146d2a3d964e7ad8629d616

--- snapshot/linux/debug_rendezvous_test.cc
+++ snapshot/linux/debug_rendezvous_test.cc
@@ -32,6 +32,7 @@
 #include "util/linux/auxiliary_vector.h"
 #include "util/linux/memory_map.h"
 #include "util/process/process_memory.h"
+#include "util/process/process_memory_linux.h"
 #include "util/process/process_memory_range.h"
 
 #if defined(OS_ANDROID)
@@ -76,10 +77,16 @@ void TestAgainstTarget(pid_t pid, bool is_64_bit) {
       mappings.FindFileMmapStart(*phdr_mapping);
   LinuxVMAddress elf_address = exe_mapping->range.Base();
 
-  ProcessMemory memory;
-  ASSERT_TRUE(memory.Initialize(pid));
+  std::unique_ptr<ProcessMemory> memory;
+  {
+    // Hide the concrete type.
+    ProcessMemoryLinux* memory_linux = new ProcessMemoryLinux();
+    ASSERT_TRUE(memory_linux->Initialize(pid));
+    memory.reset(memory_linux);
+  }
+
   ProcessMemoryRange range;
-  ASSERT_TRUE(range.Initialize(&memory, is_64_bit));
+  ASSERT_TRUE(range.Initialize(memory.get(), is_64_bit));
 
   ElfImageReader exe_reader;
   ASSERT_TRUE(exe_reader.Initialize(range, elf_address));
