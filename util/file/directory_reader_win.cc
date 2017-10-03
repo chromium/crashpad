9c11b4ea96413474a9f46dc6ad7aaef89e4b70a1
Joshua Peraza <jperaza@chromium.org>
2017-10-02 17:53:22 -0700

    Add DirectoryReader to enumerate files in a directory
    Change-Id: I62b78219ae2b277d6976d2d90ec86fcabd0ef073

---
+++ util/file/directory_reader_win.cc
@@ -0,0 +1,63 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/file/directory_reader.h"
+
+#include <errno.h>
+
+#include "base/logging.h"
+
+namespace crashpad {
+
+DirectoryReader::DirectoryReader()
+    : find_data_(), handle_(), first_entry_(false) {}
+
+DirectoryReader::~DirectoryReader() {}
+
+bool DirectoryReader::Open(const base::FilePath& path) {
+  if (path.empty()) {
+    LOG(ERROR) << "Empty directory path";
+    return false;
+  }
+
+  handle_.reset(FindFirstFile(
+      path.Append(FILE_PATH_LITERAL("*")).value().c_str(), &find_data_));
+  if (!handle_.is_valid()) {
+    PLOG(ERROR) << "FindFirstFile";
+    return false;
+  }
+  first_entry_ = true;
+  return true;
+}
+
+DirectoryReader::Result DirectoryReader::NextFile(base::FilePath* filename) {
+  CHECK(handle_.is_valid());
+
+  if (!first_entry_) {
+    if (!FindNextFile(handle_.get(), &find_data_)) {
+      if (GetLastError() != ERROR_NO_MORE_FILES) {
+        PLOG(ERROR) << "FindNextFile";
+        return Result::kError;
+      }
+      return Result::kFileNotFound;
+    }
+  } else {
+    first_entry_ = false;
+  }
+
+  *filename = base::FilePath(find_data_.cFileName);
+  return Result::kFileFound;
+}
+
+}  // namespace crashpad
--- util/util.gyp
+++ util/util.gyp
@@ -32,6 +32,9 @@
       'sources': [
         'file/delimited_file_reader.cc',
         'file/delimited_file_reader.h',
+        'file/directory_reader_posix.cc',
+        'file/directory_reader_win.cc',
+        'file/directory_reader.h',
         'file/file_io.cc',
         'file/file_io.h',
         'file/file_io_posix.cc',
