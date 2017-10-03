3d1d428cf9f78cef1ca4cca9b770d52f4bc632e0
Joshua Peraza <jperaza@chromium.org>
2017-10-02 17:42:29 -0700

    Add DirectoryReader to enumerate files in a directory
    Change-Id: I62b78219ae2b277d6976d2d90ec86fcabd0ef073

---
+++ util/file/directory_reader_test.cc
@@ -0,0 +1,48 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/file/directory_reader.h"
+
+#include <vector>
+
+#include "base/files/file_path.h"
+#include "gtest/gtest.h"
+#include "test/scoped_temp_dir.h"
+
+namespace crashpad {
+namespace test {
+namespace {
+
+TEST(DirectoryReader, Basic) {
+  DirectoryReader reader;
+  EXPECT_FALSE(reader.Open(base::FilePath()));
+  EXPECT_FALSE(reader.Open(base::FilePath(FILE_PATH_LITERAL("notadirectory"))));
+
+  ScopedTempDir temp_dir;
+  ASSERT_TRUE(reader.Open(temp_dir.path()));
+
+  std::vector<base::FilePath> files;
+  base::FilePath path;
+  DirectoryReader::Result result;
+  while ((result = reader.NextFile(&path)) ==
+         DirectoryReader::Result::kFileFound) {
+    files.push_back(path);
+  }
+  EXPECT_EQ(result, DirectoryReader::Result::kFileNotFound);
+  EXPECT_EQ(files.size(), 2u);
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
