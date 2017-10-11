022a1b8e0c84247e22b0764fb35a35d35d22ce70
Joshua Peraza <jperaza@chromium.org>
2017-10-11 14:33:27 -0700

    Add DirectoryReader to iterate over files in a directory
    This change also adds functions to create directories, remove files and
    directories, and check for the existence of files and directories.
    
    Change-Id: I62b78219ae2b277d6976d2d90ec86fcabd0ef073

---
+++ util/file/filesystem.h
@@ -0,0 +1,65 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_UTIL_FILE_FILESYSTEM_H_
+#define CRASHPAD_UTIL_FILE_FILESYSTEM_H_
+
+#include "base/files/file_path.h"
+
+namespace crashpad {
+
+//! \brief Creates a directory, logging a message on failure.
+//!
+//! \param[in] path The path to the directory to create.
+//! \param[in] may_reuse Whether to fail if the directory exists.
+//! \return `true` if the directory is successfully created or it already
+//!     existed and \a may_reuse is `true`. Otherwise, `false`.
+bool LoggingCreateDirectory(const base::FilePath& path, bool may_reuse);
+
+//! \brief Determines if a path refers to a regular file, logging a message on
+//!     failure.
+//!
+//! Returns `false` if the final component in the path is a directory or
+//! symbolic link.
+//!
+//! \param[in] path The path to the file to check.
+//! \return `true` if the file exists and is a regular file. Otherwise `false`.
+bool IsRegularFile(const base::FilePath& path);
+
+//! \brief Determines if a path refers to a directory, logging a message on
+//!     failure.
+//!
+//! \param[in] path The path to check.
+//! \param[in] allow_symlinks Whether to allow the final component in the path
+//!     to be a symbolic link to a directory.
+//! \return `true` if the path exists and is a directory. Otherwise `false`.
+bool IsDirectory(const base::FilePath& path, bool allow_symlinks);
+
+//! \brief Deletes a file, logging a message on failure.
+//!
+//! This function deletes files or symbolic links to files or directories.
+//!
+//! \param[in] path The path to the file to delete.
+//! \return `true` on success. `false` on failure with a message logged.
+bool LoggingDeleteFile(const base::FilePath& path);
+
+//! \brief Removes an empty directory, logging a message on failure.
+//!
+//! \param[in] path The to the directory to remove.
+//! \return `true` if the directory was removed. Otherwise, `false`.
+bool LoggingRemoveDirectory(const base::FilePath& path);
+
+}  // namespace crashpad
+
+#endif  // CRASHPAD_UTIL_FILE_FILESYSTEM_H_
