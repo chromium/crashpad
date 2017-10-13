762dc79fef366c4bc0533bf0322cf6d09db51890
Joshua Peraza <jperaza@chromium.org>
2017-10-13 14:47:20 -0700

    Add DirectoryReader to iterate over files in a directory
    This change also adds functions to create directories, remove files and
    directories, and check for the existence of files and directories.
    
    Change-Id: I62b78219ae2b277d6976d2d90ec86fcabd0ef073

---
+++ util/file/filesystem_win.cc
@@ -0,0 +1,87 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/file/filesystem.h"
+
+#include <windows.h>
+
+#include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
+
+namespace crashpad {
+
+bool LoggingCreateDirectory(const base::FilePath& path, bool may_reuse) {
+  if (CreateDirectory(path.value().c_str(), nullptr)) {
+    return true;
+  }
+  if (may_reuse && GetLastError() == ERROR_ALREADY_EXISTS) {
+    if (!IsDirectory(path, true)) {
+      LOG(ERROR) << base::UTF16ToUTF8(path.value()) << " not a directory";
+      return false;
+    }
+    return true;
+  }
+  PLOG(ERROR) << "CreateDirectory " << base::UTF16ToUTF8(path.value());
+  return false;
+}
+
+bool IsRegularFile(const base::FilePath& path) {
+  DWORD fileattr = GetFileAttributes(path.value().c_str());
+  if (fileattr == INVALID_FILE_ATTRIBUTES) {
+    PLOG_IF(ERROR, GetLastError() != ERROR_FILE_NOT_FOUND)
+        << "GetFileAttributes " << base::UTF16ToUTF8(path.value());
+    return false;
+  }
+  if ((fileattr & FILE_ATTRIBUTE_DIRECTORY) != 0 ||
+      (fileattr & FILE_ATTRIBUTE_REPARSE_POINT) != 0) {
+    return false;
+  }
+  return true;
+}
+
+bool IsDirectory(const base::FilePath& path, bool allow_symlinks) {
+  DWORD fileattr = GetFileAttributes(path.value().c_str());
+  if (fileattr == INVALID_FILE_ATTRIBUTES) {
+    PLOG(ERROR) << "GetFileAttributes " << base::UTF16ToUTF8(path.value());
+    return false;
+  }
+  if (!allow_symlinks && (fileattr & FILE_ATTRIBUTE_REPARSE_POINT) != 0) {
+    return false;
+  }
+  return (fileattr & FILE_ATTRIBUTE_DIRECTORY) != 0;
+}
+
+bool LoggingRemoveFile(const base::FilePath& path) {
+  DWORD fileattr = GetFileAttributes(path.value().c_str());
+  if ((fileattr & FILE_ATTRIBUTE_DIRECTORY) != 0 &&
+      (fileattr & FILE_ATTRIBUTE_REPARSE_POINT) != 0) {
+    return LoggingRemoveDirectory(path);
+  }
+
+  if (!DeleteFile(path.value().c_str())) {
+    PLOG(ERROR) << "DeleteFile " << base::UTF16ToUTF8(path.value());
+    return false;
+  }
+  return true;
+}
+
+bool LoggingRemoveDirectory(const base::FilePath& path) {
+  if (!RemoveDirectory(path.value().c_str())) {
+    PLOG(ERROR) << "RemoveDirectory " << base::UTF16ToUTF8(path.value());
+    return false;
+  }
+  return true;
+}
+
+}  // namespace crashpad
