709860d07078b1ca33ce54d98b192f9d2ec01384
Joshua Peraza <jperaza@chromium.org>
2017-10-17 09:50:14 -0700

    Crash Report Database
    Bug: crashpad:
    Change-Id: I317cf7cb43e5176236107aaa4a007339a445d890

--- handler/crash_report_upload_thread.cc
+++ handler/crash_report_upload_thread.cc
@@ -108,40 +108,6 @@ std::map<std::string, std::string> BreakpadHTTPFormParametersFromMinidump(
   return parameters;
 }
 
-// Calls CrashReportDatabase::RecordUploadAttempt() with |successful| set to
-// false upon destruction unless disarmed by calling Fire() or Disarm(). Fire()
-// triggers an immediate call. Armed upon construction.
-class CallRecordUploadAttempt {
- public:
-  CallRecordUploadAttempt(CrashReportDatabase* database,
-                          const CrashReportDatabase::Report* report)
-      : database_(database),
-        report_(report) {
-  }
-
-  ~CallRecordUploadAttempt() {
-    Fire();
-  }
-
-  void Fire() {
-    if (report_) {
-      database_->RecordUploadAttempt(report_, false, std::string());
-    }
-
-    Disarm();
-  }
-
-  void Disarm() {
-    report_ = nullptr;
-  }
-
- private:
-  CrashReportDatabase* database_;  // weak
-  const CrashReportDatabase::Report* report_;  // weak
-
-  DISALLOW_COPY_AND_ASSIGN(CallRecordUploadAttempt);
-};
-
 }  // namespace
 
 CrashReportUploadThread::CrashReportUploadThread(CrashReportDatabase* database,
@@ -290,7 +256,7 @@ void CrashReportUploadThread::ProcessPendingReport(
     }
   }
 
-  const CrashReportDatabase::Report* upload_report;
+  std::unique_ptr<const CrashReportDatabase::UploadReport> upload_report;
   CrashReportDatabase::OperationStatus status =
       database_->GetReportForUploading(report.uuid, &upload_report);
   switch (status) {
@@ -317,18 +283,16 @@ void CrashReportUploadThread::ProcessPendingReport(
       return;
   }
 
-  CallRecordUploadAttempt call_record_upload_attempt(database_, upload_report);
-
   std::string response_body;
-  UploadResult upload_result = UploadReport(upload_report, &response_body);
+  UploadResult upload_result =
+      UploadReport(upload_report.get(), &response_body);
   switch (upload_result) {
     case UploadResult::kSuccess:
-      call_record_upload_attempt.Disarm();
-      database_->RecordUploadAttempt(upload_report, true, response_body);
+      database_->RecordUploadComplete(&upload_report, response_body);
       break;
     case UploadResult::kPermanentFailure:
     case UploadResult::kRetry:
-      call_record_upload_attempt.Fire();
+      upload_report.reset();
 
       // TODO(mark): Deal with retries properly: don’t call SkipReportUplaod()
       // if the result was kRetry and the report hasn’t already been retried
@@ -340,7 +304,7 @@ void CrashReportUploadThread::ProcessPendingReport(
 }
 
 CrashReportUploadThread::UploadResult CrashReportUploadThread::UploadReport(
-    const CrashReportDatabase::Report* report,
+    const CrashReportDatabase::UploadReport* report,
     std::string* response_body) {
   std::map<std::string, std::string> parameters;
 
