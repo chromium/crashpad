5e4794abe123f2fa9e05d2ec2a99b463f76b44a1
Joshua Peraza <jperaza@chromium.org>
2017-10-26 10:16:42 -0700

    Crash Report Database
    Change-Id: I317cf7cb43e5176236107aaa4a007339a445d890

--- handler/mac/crash_report_exception_handler.cc
+++ handler/mac/crash_report_exception_handler.cc
@@ -14,6 +14,7 @@
 
 #include "handler/mac/crash_report_exception_handler.h"
 
+#include <utility>
 #include <vector>
 
 #include "base/logging.h"
@@ -155,7 +156,7 @@ kern_return_t CrashReportExceptionHandler::CatchMachException(
     process_snapshot.SetClientID(client_id);
     process_snapshot.SetAnnotationsSimpleMap(*process_annotations_);
 
-    CrashReportDatabase::NewReport* new_report;
+    std::unique_ptr<CrashReportDatabase::NewReport> new_report;
     CrashReportDatabase::OperationStatus database_status =
         database_->PrepareNewCrashReport(&new_report);
     if (database_status != CrashReportDatabase::kNoError) {
@@ -164,28 +165,22 @@ kern_return_t CrashReportExceptionHandler::CatchMachException(
       return KERN_FAILURE;
     }
 
-    process_snapshot.SetReportID(new_report->uuid);
-
-    CrashReportDatabase::CallErrorWritingCrashReport
-        call_error_writing_crash_report(database_, new_report);
-
-    WeakFileHandleFileWriter file_writer(new_report->handle);
+    process_snapshot.SetReportID(new_report->ReportID());
 
     MinidumpFileWriter minidump;
     minidump.InitializeFromSnapshot(&process_snapshot);
     AddUserExtensionStreams(
         user_stream_data_sources_, &process_snapshot, &minidump);
 
-    if (!minidump.WriteEverything(&file_writer)) {
+    if (!minidump.WriteEverything(new_report->Writer())) {
       Metrics::ExceptionCaptureResult(
           Metrics::CaptureResult::kMinidumpWriteFailed);
       return KERN_FAILURE;
     }
 
-    call_error_writing_crash_report.Disarm();
-
     UUID uuid;
-    database_status = database_->FinishedWritingCrashReport(new_report, &uuid);
+    database_status =
+        database_->FinishedWritingCrashReport(std::move(new_report), &uuid);
     if (database_status != CrashReportDatabase::kNoError) {
       Metrics::ExceptionCaptureResult(
           Metrics::CaptureResult::kFinishedWritingCrashReportFailed);
