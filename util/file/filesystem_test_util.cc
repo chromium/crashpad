a3386ca78e88899cfc76f450e8324cd08dc25ee6
Joshua Peraza <jperaza@chromium.org>
2017-10-31 13:46:03 -0700

    Add FileModificationTime
    FileModificationTime gets the last write time for files and symbolic
    links, but not directories. Symbolic links may point to files,
    directories, or be dangling. Modifications of pointed to files or
    directories do not affect the modification time of the link.
    
    Change-Id: Ic83b5a7d318502ad5db5c01731d06c8624925e15

--- util/file/filesystem_test_util.cc
+++ util/file/filesystem_test_util.cc
@@ -16,6 +16,7 @@
 
 #include <errno.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/types.h>
 
 #include "base/logging.h"
@@ -113,6 +114,26 @@ bool PathExists(const base::FilePath& path) {
     return false;
   }
   return true;
+#endif
+}
+
+void TimespecToTimeval(const timespec& ts, timeval* tv) {
+  tv->tv_sec = ts.tv_sec;
+  tv->tv_usec = ts.tv_nsec / 1000;
+}
+
+bool SetFileModificationTime(const base::FilePath& path, const timespec& mtime) {
+#if defined(OS_POSIX)
+  timeval tv[2];
+  TimespecToTimeval(mtime, tv);
+  tv[1] = tv[0];
+  if (utimes(path.value().c_str(), tv) != 0) {
+    PLOG(ERROR) << "utimes " << path.value();
+    return false;
+  }
+  return true;
+#elif defined(OS_WIN)
+  return false;
 #endif
 }
 
