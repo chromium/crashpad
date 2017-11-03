9c0c99fbd83891ed70b9974a1e5f3c9ca692d220
Joshua Peraza <jperaza@chromium.org>
2017-11-02 18:01:30 -0700

    Move win/time to misc/time and add more conversion functions
    Bug: crashpad:206
    Change-Id: I1d9b1560884ffde2364af0092114f82e1534ad1c

---
+++ util/misc/time.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/misc/time.h"
+#include "util/numeric/safe_assignment.h"
+
+namespace crashpad {
+
+void AddTimespec(const timespec& ts1, const timespec& ts2, timespec* result) {
+  result->tv_sec = ts1.tv_sec + ts2.tv_sec;
+  result->tv_nsec = ts1.tv_nsec + ts2.tv_nsec;
+  if (result->tv_nsec > static_cast<long>(1E9)) {
+    ++result->tv_sec;
+    result->tv_nsec -= static_cast<long>(1E9);
+  }
+}
+
+void SubtractTimespec(const timespec& t1,
+                      const timespec& t2,
+                      timespec* result) {
+  result->tv_sec = t1.tv_sec - t2.tv_sec;
+  result->tv_nsec = t1.tv_nsec - t2.tv_nsec;
+  if (result->tv_nsec < 0) {
+    result->tv_sec -= 1;
+    result->tv_nsec += static_cast<long>(1E9);
+  }
+}
+
+bool TimespecToTimeval(const timespec& ts, timeval* tv) {
+  tv->tv_usec = ts.tv_nsec / 1000;
+  return AssignIfInRange(&tv->tv_sec, ts.tv_sec);
+}
+
+}  // namespace crashpad
