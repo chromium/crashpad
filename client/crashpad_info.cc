69f015c9ed37b81e46f9184d794504c1dc565dae
Mark Mentovai <mark@chromium.org>
2017-11-14 15:08:29 -0500

    Tolerate safe size mismatches in the CrashpadInfo struct
    The handler will now be less strict about checking CrashpadInfo struct
    sizes. Assuming the signature and version fields match:
    
     - If the handler sees a struct smaller than it’s expecting, the module
       was likely built with an earlier version of the client library, and
       it’s safe to treat the unknown fields as though they were zero or
       other suitable default values.
     - If the handler sees a struct larger than it’s expecting, the module
       was likely built with a later version of the client library. In that
       case, actions desired by the client will not be performed, but this
       is not otherwise an error condition.
    
    The CrashpadInfo struct must always be at least large enough to contain
    at least the size field. The signature and version fields are always
    checked.
    
    The section size must be at least as large as the size carried within
    the struct. To account for possible section padding, strict equality is
    not required.
    
    Bug: chromium:784427
    Test: crashpad_snapshot_test CrashpadInfoSizes_ClientOptions/*.*
    Change-Id: Ibb0690ca6ed5e7619d1278a68ba7e893d55f19fb

--- client/crashpad_info.cc
+++ client/crashpad_info.cc
@@ -25,6 +25,11 @@
 
 namespace {
 
+// Don’t change this when simply adding fields. Readers will size-check the
+// structure and ignore fields they’re aware of when not present, as well as
+// fields they’re not aware of. Only change this when introducing an
+// incompatible layout, with the understanding that existing readers will not
+// understand new versions.
 constexpr uint32_t kCrashpadInfoVersion = 1;
 
 }  // namespace
@@ -106,13 +111,7 @@ CrashpadInfo::CrashpadInfo()
       extra_memory_ranges_(nullptr),
       simple_annotations_(nullptr),
       user_data_minidump_stream_head_(nullptr),
-      annotations_list_(nullptr)
-#if !defined(NDEBUG) && defined(OS_WIN)
-      ,
-      invalid_read_detection_(0xbadc0de)
-#endif
-{
-}
+      annotations_list_(nullptr) {}
 
 void CrashpadInfo::AddUserDataMinidumpStream(uint32_t stream_type,
                                              const void* data,
