86eb576e1f0c29dfdb4c26452f4faa48ba476615
Joshua Peraza <jperaza@chromium.org>
2017-11-20 13:18:34 -0800

    Add exception handler server
    Bug: crashpad:30
    Change-Id: I60874a26ccb281144f870df2b4d16c6970a39f6b

---
+++ handler/linux/exception_handler_server.cc
@@ -0,0 +1,300 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "handler/linux/exception_handler_server.h"
+
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <utility>
+
+#include "base/logging.h"
+#include "base/posix/eintr_wrapper.h"
+#include "util/file/file_io.h"
+#include "util/misc/as_underlying_type.h"
+
+namespace crashpad {
+
+namespace {
+
+// Log an error for a socket after an EPOLLERR.
+void LogSocketError(int sock) {
+  int err;
+  socklen_t err_len = sizeof(err);
+  if (getsockopt(sock, SOL_SOCKET, SO_ERROR, &err, &err_len) !=
+      0) {
+    PLOG(ERROR) << "getsockopt";
+  } else {
+    errno = err;
+    PLOG(ERROR) << "EPOLLERR";
+  }
+}
+
+}  // namespace
+
+struct ExceptionHandlerServer::Event {
+  enum class Type { kShutdown, kClientMessage } type;
+
+  ScopedFileHandle fd;
+
+  union {
+    // Valid for type == kClientMessage.
+    Registration registration;
+  };
+};
+
+ExceptionHandlerServer::ExceptionHandlerServer()
+    : clients_(),
+      shutdown_event_(),
+      delegate_(nullptr),
+      pollfd_(),
+      keep_running_(true) {}
+
+ExceptionHandlerServer::~ExceptionHandlerServer() = default;
+
+bool ExceptionHandlerServer::InitializeWithClient(
+    const Registration& registration,
+    ScopedFileHandle sock) {
+  pollfd_.reset(epoll_create1(EPOLL_CLOEXEC));
+  if (!pollfd_.is_valid()) {
+    PLOG(ERROR) << "epoll_create1";
+    return false;
+  }
+
+  shutdown_event_ = std::make_unique<Event>();
+  shutdown_event_->type = Event::Type::kShutdown;
+  shutdown_event_->fd.reset(eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK));
+  if (!shutdown_event_->fd.is_valid()) {
+    PLOG(ERROR) << "eventfd";
+    return false;
+  }
+
+  epoll_event poll_event;
+  poll_event.events = EPOLLIN;
+  poll_event.data.ptr = shutdown_event_.get();
+  if (epoll_ctl(pollfd_.get(),
+                EPOLL_CTL_ADD,
+                shutdown_event_->fd.get(),
+                &poll_event) != 0) {
+    PLOG(ERROR) << "epoll_ctl";
+    return false;
+  }
+
+  return InstallClientRegistration(registration, std::move(sock));
+}
+
+void ExceptionHandlerServer::Run(Delegate* delegate) {
+  delegate_ = delegate;
+
+  while (keep_running_ && clients_.size() > 0) {
+    epoll_event poll_event;
+    int res = HANDLE_EINTR(epoll_wait(pollfd_.get(), &poll_event, 1, -1));
+    if (res < 0) {
+      PLOG(ERROR) << "epoll_wait";
+      return;
+    }
+
+    Event* eventp = reinterpret_cast<Event*>(poll_event.data.ptr);
+    if (poll_event.events & EPOLLERR) {
+
+    }
+
+    if (eventp->type == Event::Type::kShutdown) {
+      if (poll_event.events & EPOLLERR) {
+        LogSocketError(eventp->fd.get());
+      }
+      keep_running_ = false;
+    } else {
+      HandleEvent(eventp, poll_event.events);
+    }
+  }
+}
+
+void ExceptionHandlerServer::Stop() {
+  keep_running_ = false;
+  if (shutdown_event_ && shutdown_event_->fd.is_valid()) {
+    uint64_t value = 1;
+    LoggingWriteFile(shutdown_event_->fd.get(), &value, sizeof(value));
+  }
+}
+
+void ExceptionHandlerServer::HandleEvent(Event* event, uint32_t event_type) {
+  DCHECK_EQ(AsUnderlyingType(event->type),
+            AsUnderlyingType(Event::Type::kClientMessage));
+
+  if (event_type & EPOLLERR) {
+    LogSocketError(event->fd.get());
+    UninstallClientRegistration(event);
+    return;
+  }
+
+  if (event_type & EPOLLIN) {
+    if (!ReceiveClientMessage(event)) {
+      UninstallClientRegistration(event);
+    }
+    return;
+  }
+
+  if (event_type & EPOLLHUP || event_type & EPOLLRDHUP) {
+    UninstallClientRegistration(event);
+    return;
+  }
+
+  LOG(ERROR) << "Unexpected event " << event_type;
+  return;
+}
+
+
+bool ExceptionHandlerServer::InstallClientRegistration(
+    const Registration& registration,
+    ScopedFileHandle socket) {
+  auto event = std::make_unique<Event>();
+  event->type = Event::Type::kClientMessage;
+  event->fd.reset(socket.release());
+  event->registration = registration;
+
+  Event* eventp = event.get();
+
+  if (!clients_.insert(std::make_pair(event->fd.get(), std::move(event)))
+           .second) {
+    LOG(ERROR) << "duplicate descriptor";
+    return false;
+  }
+
+  epoll_event poll_event;
+  poll_event.events = EPOLLIN | EPOLLRDHUP;
+  poll_event.data.ptr = eventp;
+
+  if (epoll_ctl(pollfd_.get(), EPOLL_CTL_ADD, eventp->fd.get(), &poll_event) !=
+      0) {
+    PLOG(ERROR) << "epoll_ctl";
+    clients_.erase(eventp->fd.get());
+    return false;
+  }
+
+  return true;
+}
+
+bool ExceptionHandlerServer::UninstallClientRegistration(Event* event) {
+  if (epoll_ctl(pollfd_.get(), EPOLL_CTL_DEL, event->fd.get(), nullptr) != 0) {
+    PLOG(ERROR) << "epoll_ctl";
+    return false;
+  }
+
+  if (clients_.erase(event->fd.get()) != 1) {
+    LOG(ERROR) << "event not found";
+    return false;
+  }
+
+  return true;
+}
+
+bool ExceptionHandlerServer::ReceiveClientMessage(Event* event) {
+  ClientToServerMessage message;
+  memset(&message, 0, sizeof(message));
+
+  iovec iov;
+  iov.iov_base = &message;
+  iov.iov_len = sizeof(message);
+
+  msghdr msg;
+  msg.msg_name = nullptr;
+  msg.msg_namelen = 0;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+
+  char cmsg_buf[CMSG_SPACE(sizeof(int))];
+  msg.msg_control = cmsg_buf;
+  msg.msg_controllen = sizeof(cmsg_buf);
+  msg.msg_flags = 0;
+
+  int res = recvmsg(event->fd.get(), &msg, 0);
+  if (res < 0) {
+    PLOG(ERROR) << "recvmsg";
+    return false;
+  }
+  if (res == 0) {
+    LOG(ERROR) << "client shutdown";
+    return false;
+  }
+
+  if (msg.msg_name != nullptr || msg.msg_namelen != 0) {
+    LOG(ERROR) << "unexpected msg name";
+    return false;
+  }
+
+  if (msg.msg_iovlen != 1) {
+    LOG(ERROR) << "unexpected iovlen";
+    return false;
+  }
+
+  if (msg.msg_iov[0].iov_len != sizeof(ClientToServerMessage)) {
+    LOG(ERROR) << "unexpected message size " << msg.msg_iov[0].iov_len;
+    return false;
+  }
+  auto client_msg =
+      reinterpret_cast<ClientToServerMessage*>(msg.msg_iov[0].iov_base);
+
+  if (client_msg->type == ClientToServerMessage::kRegistration) {
+    cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+    if (cmsg == nullptr) {
+      LOG(ERROR) << "missing registration descriptor";
+      return false;
+    }
+
+    if (cmsg->cmsg_level != SOL_SOCKET) {
+      LOG(ERROR) << "unexpected cmsg_level " << cmsg->cmsg_level;
+      return false;
+    }
+
+    if (cmsg->cmsg_type != SCM_RIGHTS) {
+      LOG(ERROR) << "unexpected cmsg_type " << cmsg->cmsg_type;
+      return false;
+    }
+
+    if (cmsg->cmsg_len != CMSG_LEN(sizeof(int))) {
+      LOG(ERROR) << "unexpected cmsg_len " << cmsg->cmsg_len;
+      return false;
+    }
+
+    ScopedFileHandle reg_socket(*reinterpret_cast<int*>(CMSG_DATA(cmsg)));
+    bool success = InstallClientRegistration(client_msg->registration,
+                                             std::move(reg_socket));
+
+    LoggingWriteFile(event->fd.get(), &success, sizeof(success));
+
+    return true;
+  }
+
+  if (client_msg->type == ClientToServerMessage::kCrashDumpRequest) {
+    if (event->registration.use_broker) {
+      delegate_->HandleExceptionWithBroker(
+          event->registration.client_process_id,
+          event->registration.exception_information_address,
+          event->fd.get());
+    } else {
+      delegate_->HandleException(
+          event->registration.client_process_id,
+          event->registration.exception_information_address);
+    }
+    return true;
+  }
+
+  LOG(ERROR) << "Unknown message type";
+  return false;
+}
+
+}  // namespace crashpad
---
+++ util/linux/registration_protocol.cc
@@ -0,0 +1,85 @@
+// Copyright 2017 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/linux/registration_protocol.h"
+
+#include <sys/socket.h>
+
+#include "base/logging.h"
+#include "util/file/file_io.h"
+
+namespace crashpad {
+
+bool RegisterWithHandler(int server_sock,
+                         const Registration& registration,
+                         int registration_socket) {
+  ClientToServerMessage message;
+  message.type = ClientToServerMessage::kRegistration;
+  message.registration = registration;
+
+  iovec iov;
+  iov.iov_base = &message;
+  iov.iov_len = sizeof(message);
+
+  msghdr msg;
+  msg.msg_name = nullptr;
+  msg.msg_namelen = 0;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+
+  char cmsg_buf[CMSG_SPACE(sizeof(int))];
+  msg.msg_control = cmsg_buf;
+  msg.msg_controllen = sizeof(cmsg_buf);
+
+  cmsghdr* cmsg = CMSG_FIRSTHDR(&msg);
+  cmsg->cmsg_level = SOL_SOCKET;
+  cmsg->cmsg_type = SCM_RIGHTS;
+  cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+  *reinterpret_cast<int*>(CMSG_DATA(cmsg)) = registration_socket;
+
+  if (sendmsg(server_sock, &msg, MSG_NOSIGNAL) < 0) {
+    PLOG(ERROR) << "sendmsg";
+    return false;
+  }
+
+  bool success;
+  LoggingReadFileExactly(server_sock, &success, sizeof(success));
+  return success;
+}
+
+bool RequestCrashDump(int registration_socket) {
+  ClientToServerMessage message;
+  message.type = ClientToServerMessage::kCrashDumpRequest;
+
+  iovec iov;
+  iov.iov_base = &message;
+  iov.iov_len = sizeof(message);
+
+  msghdr msg;
+  msg.msg_name = nullptr;
+  msg.msg_namelen = 0;
+  msg.msg_iov = &iov;
+  msg.msg_iovlen = 1;
+  msg.msg_control = nullptr;
+  msg.msg_controllen = 0;
+
+  if (sendmsg(registration_socket, &msg, MSG_NOSIGNAL) < 0) {
+    PLOG(ERROR) << "sendmsg";
+    return false;
+  }
+
+  return true;
+}
+
+}  // namespace crashpad
