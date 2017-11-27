f520fd1217b92cbf840603c29ccfbb7a0281bd90
Scott Graham <scottmg@chromium.org>
2017-11-27 13:09:41 -0800

    fuchsia: Use crashpad_info section matching Linux/Android for now
    I have no idea if this will work as not much is building yet, but it
    seems plausible for the time being.
    
    Bug: crashpad:196
    Change-Id: Ie3a358512a968e9e777ed03c0bffc5e273a0f12e

--- client/crashpad_info.cc
+++ client/crashpad_info.cc
@@ -58,11 +58,11 @@ __attribute__((
     // found without having to consult the symbol table.
 #if defined(OS_MACOSX)
     section(SEG_DATA ",crashpad_info"),
-#elif defined(OS_LINUX) || defined(OS_ANDROID)
+#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_FUCHSIA)
     section("crashpad_info"),
-#else  // !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_ANDROID)
+#else
 #error Port
-#endif  // !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_ANDROID)
+#endif
 
 #if defined(ADDRESS_SANITIZER)
     // AddressSanitizer would add a trailing red zone of at least 32 bytes,
