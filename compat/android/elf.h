a1b01126ce2159cf16fb44a7d7c59ccb070069b6
Joshua Peraza <jperaza@chromium.org>
2017-11-30 13:16:56 -0800

    Enable reading notes from ELF images
    Bug: crashpad:30
    Change-Id: Ie6c594b05c6d39a869ed30b7a7b49e6a6301cc65

--- compat/android/elf.h
+++ compat/android/elf.h
@@ -17,6 +17,8 @@
 
 #include_next <elf.h>
 
+#include <android/api-level.h>
+
 #if !defined(ELF32_ST_VISIBILITY)
 #define ELF32_ST_VISIBILITY(other) ((other) & 0x3)
 #endif
@@ -34,5 +36,19 @@
 #if !defined(STT_TLS)
 #define STT_TLS 6
 #endif
+
+#if __ANDROID_API__ < 21
+typedef struct elf32_note {
+  Elf32_Word n_namesz;
+  Elf32_Word n_descsz;
+  Elf32_Word n_type;
+} Elf32_Nhdr;
+
+typedef struct elf64_note {
+  Elf64_Word n_namesz;
+  Elf64_Word n_descsz;
+  Elf64_Word n_type;
+} Elf64_Nhdr;
+#endif  // __ANDROID_API__ < 21
 
 #endif  // CRASHPAD_COMPAT_ANDROID_ELF_H_
