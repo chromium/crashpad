a052abaeb1d1acf16084d27f65a6912646f0d136
Scott Graham <scottmg@chromium.org>
2017-11-30 13:45:31 -0800

    fuchsia: Get snapshot compiling with stubs
    Bug: crashpad:196
    Change-Id: I7ab0014219c9b309648509b497a66b14e711a087

--- snapshot/crashpad_info_size_test_module.cc
+++ snapshot/crashpad_info_size_test_module.cc
@@ -70,11 +70,11 @@ struct TestCrashpadInfo {
 __attribute__((
 #if defined(OS_MACOSX)
     section(SEG_DATA ",crashpad_info"),
-#elif defined(OS_LINUX) || defined(OS_ANDROID)
+#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_FUCHSIA)
     section("crashpad_info"),
-#else  // !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_ANDROID)
+#else
 #error Port
-#endif  // !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_ANDROID)
+#endif
 #if defined(ADDRESS_SANITIZER)
     aligned(64),
 #endif  // defined(ADDRESS_SANITIZER)
