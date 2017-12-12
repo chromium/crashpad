e487caf61ce4b40ea6229091050b99498222a3e1
Scott Graham <scottmg@chromium.org>
2017-12-12 13:17:53 -0800

    fuchsia: Add qemu and helper script to start/stop instance
    This is mostly intended to be used for waterfall/trybots.
    Fuchsia-on-metal isn't available as a Swarming dimension, so in order to
    run tests, use QEMU-with-KVM on the host. It might also be useful for
    local development for those without a Fuchsia hardware device.
    
    Bug: crashpad:196, crashpad:212
    Change-Id: I88170bc95bd532676b787b50a94f7fa3c69b1ac7

---
+++ build/run_fuchsia_qemu.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+# Copyright 2017 The Crashpad Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Helper script to [re]start or stop a helper Fuchsia QEMU instance to be used
+for running tests without a device.
+"""
+
+from __future__ import print_function
+
+import os
+import signal
+import subprocess
+import sys
+
+
+CRASHPAD_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             os.pardir)
+
+
+def _Stop(pid_file):
+  if os.path.isfile(pid_file):
+    with open(pid_file, 'rb') as f:
+      pid = int(f.read())
+      try:
+        os.kill(pid, signal.SIGTERM)
+      except:
+        print('Unable to kill pid %d, continuing' % pid, file=sys.stderr)
+    os.unlink(pid_file)
+
+
+def _Start(pid_file):
+  arch = 'mac-amd64' if sys.platform == 'darwin' else 'linux-amd64'
+  qemu_path = os.path.join(CRASHPAD_ROOT, 'third_party', 'fuchsia', 'qemu',
+                           arch, 'bin', 'qemu-system-x86_64')
+  kernel_path = os.path.join(CRASHPAD_ROOT, 'third_party', 'fuchsia', 'sdk',
+                             arch, 'target', 'x86_64', 'zircon.bin')
+  initrd_path = os.path.join(CRASHPAD_ROOT, 'third_party', 'fuchsia', 'sdk',
+                             arch, 'target', 'x86_64', 'bootdata.bin')
+
+  # These arguments are from the Fuchsia repo in zircon/scripts/run-zircon.
+  popen = subprocess.Popen([
+    qemu_path,
+    '-m', '2048',
+    '-nographic',
+    '-kernel', kernel_path,
+    '-initrd', initrd_path,
+    '-smp', '4',
+    '-serial', 'stdio',
+    '-monitor', 'none',
+    '-machine', 'q35',
+    '-cpu', 'host,migratable=no',
+    '-enable-kvm',
+    '-netdev', 'type=tap,ifname=qemu,script=no,downscript=no,id=net0',
+    '-device', 'e1000,netdev=net0,mac=52:54:00:63:5e:7b',
+    '-append', 'TERM=dumb',
+  ])
+
+  with open(pid_file, 'wb') as f:
+    f.write('%d' % popen.pid)
+
+
+def main(args):
+  if len(args) != 1:
+    print('usage: run_fuchsia_qemu.py start|stop', file=sys.stderr)
+    return 1
+
+  command = args[0]
+
+  pid_file = os.path.join(CRASHPAD_ROOT, '.fuchsia_qemu_pid')
+  if command == 'start':
+    if os.path.exists(pid_file):
+      _Stop(pid_file)
+    _Start(pid_file)
+  elif command == 'stop':
+    _Stop(pid_file)
+
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
