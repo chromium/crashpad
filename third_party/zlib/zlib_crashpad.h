a0bd3fee55ea0538ac66bd9d20b06df766941f53
Scott Graham <scottmg@chromium.org>
2017-12-18 16:41:20 -0800

    fuchsia: Support referencing the Fuchsia tree's third_party/zlib
    This renames the "chromium" configuration of zlib to "external". What it
    really means is that zlib lives in //third_party/zlib, which happens to
    be where both Chromium and Fuchsia put it, with moderately similar build
    files.
    
    Bug: crashpad:79, crashpad:196
    Change-Id: I380c106ec1f97471b2354166f5cf92885196e1b8
    Reviewed-on: https://chromium-review.googlesource.com/833095
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>

--- third_party/zlib/zlib_crashpad.h
+++ third_party/zlib/zlib_crashpad.h
@@ -19,7 +19,7 @@
 // available at any other location in the source tree. It will #include the
 // proper <zlib.h> depending on how the build has been configured.
 
-#if defined(CRASHPAD_ZLIB_SOURCE_CHROMIUM)
+#if defined(CRASHPAD_ZLIB_SOURCE_EXTERNAL)
 #include "third_party/zlib/zlib.h"
 #elif defined(CRASHPAD_ZLIB_SOURCE_SYSTEM)
 #include <zlib.h>
