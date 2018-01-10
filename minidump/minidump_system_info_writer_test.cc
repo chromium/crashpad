6e553d5d7ec7de13e3da26f53d4b27e9f24a0c8b
Scott Graham <scottmg@chromium.org>
2018-01-10 13:58:07 -0800

    fuchsia: Don't use {0} for initialization
    Fuchsia builds with -Wmissing-field-initializers. Remove these {0}s. It
    all seems a bit awful, but as far as I can tell from reading
    http://en.cppreference.com/w/cpp/language/aggregate_initialization, the
    0 is unnecessary.
    
    ../../third_party/crashpad/minidump/minidump_system_info_writer_test.cc:42:27: error: missing field 'Buffer' initializer [-Werror,-Wmissing-field-initializers]
      MINIDUMP_STRING tmp = {0};
                                ^
    			    1 error generated.
    
    
    Bug: crashpad:196
    Change-Id: I21f48eb24238a607475b0e92ffe5fd88386b40b6

--- minidump/minidump_system_info_writer_test.cc
+++ minidump/minidump_system_info_writer_test.cc
@@ -39,11 +39,11 @@ void GetSystemInfoStream(const std::string& file_contents,
                          const MINIDUMP_SYSTEM_INFO** system_info,
                          const MINIDUMP_STRING** csd_version) {
   // The expected number of bytes for the CSD version’s MINIDUMP_STRING::Buffer.
-  MINIDUMP_STRING tmp = {0};
+  MINIDUMP_STRING* tmp;
   ALLOW_UNUSED_LOCAL(tmp);
-  const size_t kCSDVersionBytes = csd_version_length * sizeof(tmp.Buffer[0]);
+  const size_t kCSDVersionBytes = csd_version_length * sizeof(tmp->Buffer[0]);
   const size_t kCSDVersionBytesWithNUL =
-      kCSDVersionBytes + sizeof(tmp.Buffer[0]);
+      kCSDVersionBytes + sizeof(tmp->Buffer[0]);
 
   constexpr size_t kDirectoryOffset = sizeof(MINIDUMP_HEADER);
   constexpr size_t kSystemInfoStreamOffset =
