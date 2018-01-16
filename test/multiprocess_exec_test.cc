bb7e3b540e9d140a110a98b333598a22c91781f6
Scott Graham <scottmg@chromium.org>
2018-01-16 14:22:16 -0800

    wip on fuchsia multiprocess exec
    Bug: crashpad:196
    Change-Id: Id01dee43f2d04e682e70c12777aff41f8dd848d6

--- test/multiprocess_exec_test.cc
+++ test/multiprocess_exec_test.cc
@@ -52,6 +52,8 @@ TEST(MultiprocessExec, MultiprocessExec) {
       FILE_PATH_LITERAL("test"),
       FILE_PATH_LITERAL("multiprocess_exec_test_child"),
       TestPaths::FileType::kExecutable);
+  child_test_executable = base::FilePath(
+      "/pkg/bin/crashpad_test_test_multiprocess_exec_test_child");
   multiprocess_exec.SetChildCommand(child_test_executable, nullptr);
   multiprocess_exec.Run();
 }
