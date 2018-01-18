fde3780abf003dad8e1663f1b8c794f814a16245
Peter Collingbourne <pcc@google.com>
2018-01-17 16:21:36 -0800

    Add support for libc++ on Windows.
    Use the "POSIX" implementation of ThrowBadAlloc() on Windows when libc++
    is being used.
    
    Bug: chromium:801780
    Change-Id: I230a8df9040aa73e290bb0d002996e822958a94b

--- util/stdlib/aligned_allocator.cc
+++ util/stdlib/aligned_allocator.cc
@@ -18,7 +18,7 @@
 
 #include "build/build_config.h"
 
-#if defined(OS_POSIX)
+#if defined(OS_POSIX) || defined(_LIBCPP_STD_VER)
 #include <stdlib.h>
 #elif defined(OS_WIN)
 #include <malloc.h>
@@ -31,7 +31,7 @@ namespace {
 // library to do so. This works even if C++ exceptions are disabled, causing
 // program termination if uncaught.
 void ThrowBadAlloc() {
-#if defined(OS_POSIX)
+#if defined(OS_POSIX) || defined(_LIBCPP_STD_VER)
   // This works with both libc++ and libstdc++.
   std::__throw_bad_alloc();
 #elif defined(OS_WIN)
