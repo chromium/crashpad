09364351150ed035385475bb5a6ac2ff40ce4268
Scott Graham <scottmg@chromium.org>
2018-01-25 11:25:08 -0800

    Add CRASHPAD_CHILD_TEST_MAIN() helper for multiprocess tests
    Extends MultiprocessExec to support running functions registered via
    CRASHPAD_CHILD_TEST_MAIN() as the main of a new child process.
    
    Additionally, implements Fuchsia exit code checking, and adds a
    CRASHPAD_CHILD_TEST_MAIN()-based test for that.
    
    Bug: crashpad:196, crashpad:215
    Change-Id: I49ce3f4d95a3b9823813e6df5a602cee2583bcf8

---
+++ test/multiprocess_exec.cc
@@ -0,0 +1,69 @@
+// Copyright 2018 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test/multiprocess_exec.h"
+
+#include <map>
+
+#include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
+#include "test/main_arguments.h"
+#include "util/stdlib/map_insert.h"
+
+namespace crashpad {
+namespace test {
+
+namespace internal {
+
+namespace {
+
+std::map<std::string, int(*)()>* GetMultiprocessFunctionMap() {
+  static auto* map = new std::map<std::string, int(*)()>();
+  return map;
+}
+
+}  // namespace
+
+AppendMultiprocessTest::AppendMultiprocessTest(const std::string& test_name,
+                                               int (*main_function_pointer)()) {
+  CHECK(MapInsertOrReplace(
+      GetMultiprocessFunctionMap(), test_name, main_function_pointer, nullptr))
+      << test_name << " already registered";
+}
+
+int CheckedInvokeMultiprocessChild(const std::string& test_name) {
+  auto* functions = internal::GetMultiprocessFunctionMap();
+  auto it = functions->find(test_name);
+  CHECK(it != functions->end())
+      << "child main " << test_name << " not registered";
+  return (*it->second)();
+}
+
+}  // namespace internal
+
+void MultiprocessExec::SetChildTestMainFunction(
+    const std::string& function_name) {
+  std::string argv0 = GetMainArguments()[0];
+  std::vector<std::string> rest(GetMainArguments().begin() + 1,
+                                GetMainArguments().end());
+  rest.push_back(internal::kChildTestFunction + function_name);
+#if defined(OS_WIN)
+  SetChildCommand(base::FilePath(base::UTF8ToUTF16(argv0)), &rest);
+#else
+  SetChildCommand(base::FilePath(argv0), &rest);
+#endif
+}
+
+}  // namespace test
+}  // namespace crashpad
