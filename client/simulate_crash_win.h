461c1e39174d29b35c2e8ab8ffb6420a09b266cd
Scott Graham <scottmg@chromium.org>
2018-02-06 13:21:45 -0800

    win: Improve child crash location test
    In setting up the gn build, slightly different optimization settings
    were applied for release builds. This caused a couple things to happen,
    1) the sketchy noinline declspec was ignored, and 2) the distance
    between reading the IP and the actual crash exceeded the tolerance of 64
    bytes in the parent.
    
    To make the test more robust to this, use CaptureContext() (I think our
    improved version didn't exist at the time the tests was originally
    written). Also, switch from crashpad::CheckedWriteFile to Windows'
    WriteFile(), which avoids inlining a whole lot of code at that point.
    The return value is not checked, but the next thing that happens is that
    the function crashes unconditionally, so this does not seem like a huge
    problem.
    
    Bug: crashpad:79
    Change-Id: I8193d8ce8b01e1533c16b207813c36d6d6113d89

--- client/simulate_crash_win.h
+++ client/simulate_crash_win.h
@@ -23,11 +23,12 @@
 //! \file
 
 //! \brief Captures the CPU context and captures a dump without an exception.
-#define CRASHPAD_SIMULATE_CRASH()                        \
-  do {                                                   \
-    CONTEXT context;                                     \
-    crashpad::CaptureContext(&context);                  \
-    crashpad::CrashpadClient::DumpWithoutCrash(context); \
+#define CRASHPAD_SIMULATE_CRASH()                                           \
+  do {                                                                      \
+    /* Not "context" to avoid variable shadowing warnings. */               \
+    CONTEXT simulate_crash_cpu_context;                                     \
+    crashpad::CaptureContext(&simulate_crash_cpu_context);                  \
+    crashpad::CrashpadClient::DumpWithoutCrash(simulate_crash_cpu_context); \
   } while (false)
 
 #endif  // CRASHPAD_CLIENT_SIMULATE_CRASH_WIN_H_
