1a266a1461cc8dc1af2b03f3be8c61b8f5227500
Joshua Peraza <jperaza@chromium.org>
2018-02-09 11:44:48 -0800

    linux: fix incorrect fallthrough
    Bug: crashpad:30
    Change-Id: I9445c022550ad14497186c6878863fbf72d8cd59

--- util/linux/ptracer.cc
+++ util/linux/ptracer.cc
@@ -285,9 +285,9 @@ size_t GetGeneralPurposeRegistersAndLength(pid_t tid,
     switch (errno) {
 #if defined(ARCH_CPU_ARMEL)
       case EIO:
-        if (GetGeneralPurposeRegistersLegacy(tid, context, can_log)) {
-          return sizeof(context->t32);
-        }
+        return GetGeneralPurposeRegistersLegacy(tid, context, can_log)
+                   ? sizeof(context->t32)
+                   : 0;
 #endif  // ARCH_CPU_ARMEL
       default:
         PLOG_IF(ERROR, can_log) << "ptrace";
