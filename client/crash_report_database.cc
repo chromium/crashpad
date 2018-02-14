e97ad5ff8f32533862caac278c983f14c94242f4
Joshua Peraza <jperaza@chromium.org>
2018-02-13 19:02:28 -0800

    Add  UploadReport
    Bug: crashpad:206
    Change-Id: I8385d08d52185ad30b06a3ed054de9812ae006a2

--- client/crash_report_database.cc
+++ client/crash_report_database.cc
@@ -55,4 +55,25 @@ bool CrashReportDatabase::NewReport::Initialize(
   return true;
 }
 
+CrashReportDatabase::UploadReport::UploadReport()
+    : Report(), reader_(std::make_unique<FileReader>()), database_(nullptr) {}
+
+CrashReportDatabase::UploadReport::~UploadReport() {
+  if (database_) {
+    database_->RecordUploadAttempt(this, false, std::string());
+  }
+}
+
+bool CrashReportDatabase::UploadReport::Initialize(const base::FilePath path,
+                                                   CrashReportDatabase* db) {
+  database_ = db;
+  return reader_->Open(path);
+}
+
+OperationStatus CrashReportDatabase::RecordUploadComplete(
+    std::unique_ptr<const UploadReport> report,
+    const std::string& id) {
+  return RecordUploadAttempt(const_cast<UploadReport*>(report.get()), true, id);
+}
+
 }  // namespace crashpad
