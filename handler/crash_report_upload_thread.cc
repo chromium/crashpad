bc8e046c4498702738fbdf3de69216658df9e810
Joshua Peraza <jperaza@chromium.org>
2018-02-15 13:05:42 -0800

    linux: Add CrashReportExceptionHandler
    Bug: crashpad:30
    Change-Id: I48dc18681aa088cc5e11cb0896ee5d1d95741819

--- handler/crash_report_upload_thread.cc
+++ handler/crash_report_upload_thread.cc
@@ -241,6 +241,12 @@ void CrashReportUploadThread::ProcessPendingReport(
 CrashReportUploadThread::UploadResult CrashReportUploadThread::UploadReport(
     const CrashReportDatabase::UploadReport* report,
     std::string* response_body) {
+#if defined(OS_ANDROID)
+  // TODO(jperaza): This method can be enabled on Android after HTTPTransport is
+  // implemented and Crashpad takes over upload responsibilty on Android.
+  NOTREACHED();
+  return UploadResult::kPermanentFailure;
+#else
   std::map<std::string, std::string> parameters;
 
   FileReader* reader = report->Reader();
@@ -322,6 +328,7 @@ CrashReportUploadThread::UploadResult CrashReportUploadThread::UploadReport(
   }
 
   return UploadResult::kSuccess;
+#endif  // OS_ANDROID
 }
 
 void CrashReportUploadThread::DoWork(const WorkerThread* thread) {
