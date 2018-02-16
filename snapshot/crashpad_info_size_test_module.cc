f1b31d4ea33c4939802a6f90bdc9a71a5b4cbe82
Scott Graham <scottmg@chromium.org>
2018-02-16 09:55:34 -0800

    Fix CrashpadInfoSizes_ClientOptions/CrashpadInfoSizes_ClientOptions
    These tests needed to be updated to expose CrashpadInfo in the same way
    as the main CrashpadInfo g_crashpad_info is found on
    Linux/Android/Fuchsia.
    
    Unfortunately, while the tests pass on Fuchsia when run in isolation,
    the implementation of dlclose() on Fuchsia currently does nothing. So,
    if the full test suite is run, there's interference between the test
    modules (i.e. the values in _small vs. the values in _large), so the
    tests fail.
    
    I filed ZX-1728 upstream about this to see if it might be implemented,
    or if the test will need to spawn a clean child to do the module load
    tests in.
    
    Bug: crashpad:196
    Change-Id: I9ee01b142a29c508c6967dc83da824afa254d379

--- snapshot/crashpad_info_size_test_module.cc
+++ snapshot/crashpad_info_size_test_module.cc
@@ -23,7 +23,6 @@
 #endif  // OS_MACOSX
 
 namespace crashpad {
-namespace {
 
 #if defined(CRASHPAD_INFO_SIZE_TEST_MODULE_SMALL) == \
     defined(CRASHPAD_INFO_SIZE_TEST_MODULE_LARGE)
@@ -70,16 +69,12 @@ struct TestCrashpadInfo {
 __attribute__((
 #if defined(OS_MACOSX)
     section(SEG_DATA ",crashpad_info"),
-#elif defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_FUCHSIA)
-    section("crashpad_info"),
-#else
-#error Port
 #endif
 #if defined(ADDRESS_SANITIZER)
     aligned(64),
 #endif  // defined(ADDRESS_SANITIZER)
-    used,
-    visibility("hidden")))
+    visibility("hidden"),
+    used))
 #elif defined(OS_WIN)
 #pragma section("CPADinfo", read, write)
 __declspec(allocate("CPADinfo"))
@@ -106,7 +101,6 @@ TestCrashpadInfo g_test_crashpad_info = {'CPad',
 #endif  // CRASHPAD_INFO_SIZE_TEST_MODULE_LARGE
 };
 
-}  // namespace
 }  // namespace crashpad
 
 extern "C" {
@@ -119,6 +113,9 @@ __declspec(dllexport)
 #error Port
 #endif  // OS_POSIX
 crashpad::TestCrashpadInfo* TestModule_GetCrashpadInfo() {
+  // Note that there's no need to do the back-reference here to the note on
+  // POSIX like CrashpadInfo::GetCrashpadInfo() because the note .S file is
+  // directly included into this test binary.
   return &crashpad::g_test_crashpad_info;
 }
 
