c47754d453059ee4ac71db50c878eb76d586bffe
Scott Graham <scottmg@chromium.org>
2018-02-21 16:21:38 -0800

    .
    .
    
    
    rebase
    
    Bug: crashpad:
    Change-Id: I442f2b2edbdf2c93b4dced89e349409df1350633

---
+++ snapshot/fuchsia/thread_snapshot_fuchsia.cc
@@ -0,0 +1,110 @@
+// Copyright 2018 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "snapshot/fuchsia/thread_snapshot_fuchsia.h"
+
+#include "base/logging.h"
+
+namespace crashpad {
+namespace internal {
+
+ThreadSnapshotFuchsia::ThreadSnapshotFuchsia()
+    : ThreadSnapshot(),
+      context_union_(),
+      context_(),
+      stack_(),
+      thread_specific_data_address_(0),
+      thread_id_(ZX_KOID_INVALID),
+      initialized_() {}
+
+ThreadSnapshotFuchsia::~ThreadSnapshotFuchsia() {}
+
+bool ThreadSnapshotFuchsia::Initialize(
+    ProcessReader* process_reader,
+    const ProcessReader::Thread& thread) {
+  INITIALIZATION_STATE_SET_INITIALIZING(initialized_);
+
+#if defined(ARCH_CPU_X86_64)
+  context_.architecture = kCPUArchitectureX86_64;
+  context_.x86_64 = &context_union_.x86_64;
+#if 0
+  InitializeCPUContextX86_64(thread.thread_info.thread_context.t64,
+                             thread.thread_info.float_context.f64,
+                             context_.x86_64);
+#endif
+#elif defined(ARCH_CPU_ARM64)
+  context_.architecture = kCPUArchitectureARM64;
+  context_.arm64 = &context_union_.arm64;
+#if 0
+  InitializeCPUContextARM64(thread.thread_info.thread_context.t64,
+                            thread.thread_info.float_context.f64,
+                            context_.arm64);
+#endif
+#else
+#error Port.
+#endif
+
+#if 0
+  stack_.Initialize(process_reader,
+                    thread.stack_region_address,
+                    thread.stack_region_size);
+
+  thread_specific_data_address_ =
+      thread.thread_info.thread_specific_data_address;
+
+  thread_id_ = thread.tid;
+#endif
+
+  INITIALIZATION_STATE_SET_VALID(initialized_);
+  return true;
+}
+
+const CPUContext* ThreadSnapshotFuchsia::Context() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return &context_;
+}
+
+const MemorySnapshot* ThreadSnapshotFuchsia::Stack() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return &stack_;
+}
+
+uint64_t ThreadSnapshotFuchsia::ThreadID() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return thread_id_;
+}
+
+int ThreadSnapshotFuchsia::SuspendCount() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  // There's (currently) no thread suspension count on Fuchsia.
+  return 0;
+}
+
+int ThreadSnapshotFuchsia::Priority() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  // There's (currently) no thread priorities on Fuchsia.
+  return 0;
+}
+
+uint64_t ThreadSnapshotFuchsia::ThreadSpecificDataAddress() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return thread_specific_data_address_;
+}
+
+std::vector<const MemorySnapshot*> ThreadSnapshotFuchsia::ExtraMemory() const {
+  return std::vector<const MemorySnapshot*>();
+}
+
+}  // namespace internal
+}  // namespace crashpad
