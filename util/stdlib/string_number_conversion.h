75154ab1f0862403314fe4b30d7d0e7df8fc195a
Mark Mentovai <mark@chromium.org>
2018-03-02 20:55:24 -0500

    Don’t read beyond a StringPiece’s bounds in StringToNumber()
    The implementations requires NUL-termination for the underlying buffer,
    so just use std::string everywhere, rather than trying to detect whether
    strings are already NUL-terminated.
    
    Bug: chromium:817982, chromium:818376
    Change-Id: I4c8dcb5ed15ebca4c531f9a5d0ee865228dc0959

--- util/stdlib/string_number_conversion.h
+++ util/stdlib/string_number_conversion.h
@@ -15,7 +15,7 @@
 #ifndef CRASHPAD_UTIL_STDLIB_STRING_NUMBER_CONVERSION_H_
 #define CRASHPAD_UTIL_STDLIB_STRING_NUMBER_CONVERSION_H_
 
-#include "base/strings/string_piece.h"
+#include <string>
 
 namespace crashpad {
 
@@ -54,10 +54,10 @@ namespace crashpad {
 //!     allow arbitrary bases based on whether the string begins with a prefix
 //!     indicating its base. The functions here are provided for situations
 //!     where such prefix recognition is desirable.
-bool StringToNumber(const base::StringPiece& string, int* number);
-bool StringToNumber(const base::StringPiece& string, unsigned int* number);
-bool StringToNumber(const base::StringPiece& string, int64_t* number);
-bool StringToNumber(const base::StringPiece& string, uint64_t* number);
+bool StringToNumber(const std::string& string, int* number);
+bool StringToNumber(const std::string& string, unsigned int* number);
+bool StringToNumber(const std::string& string, int64_t* number);
+bool StringToNumber(const std::string& string, uint64_t* number);
 //! \}
 
 }  // namespace crashpad
