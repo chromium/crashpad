55af874ca48b42c70cb544b3f49ad16e9386d268
Joshua Peraza <jperaza@chromium.org>
2018-04-02 21:17:29 -0700

    linux: Enable brokered memory reading
    Bug: crashpad:30
    Change-Id: I8c004016065d981acd1fa74ad1b8e51ce07c7c85

--- test/linux/fake_ptrace_connection.h
+++ test/linux/fake_ptrace_connection.h
@@ -22,6 +22,7 @@
 #include "base/macros.h"
 #include "util/linux/ptrace_connection.h"
 #include "util/misc/initialization_state_dcheck.h"
+#include "util/process/process_memory_linux.h"
 
 namespace crashpad {
 namespace test {
@@ -56,8 +57,13 @@ class FakePtraceConnection : public PtraceConnection {
   bool ReadFileContents(const base::FilePath& path,
                         std::string* contents) override;
 
+  //! \brief Attempts to create a ProcessMemory when called, calling
+  //!     ADD_FAILURE() and returning `nullptr` on failure.
+  ProcessMemory* Memory() override;
+
  private:
   std::set<pid_t> attachments_;
+  std::unique_ptr<ProcessMemoryLinux> memory_;
   pid_t pid_;
   bool is_64_bit_;
   InitializationStateDcheck initialized_;
