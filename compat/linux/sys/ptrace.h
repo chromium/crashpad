2cd50a6d9e8bd600932104a4e11da86befcde222
Tom Anderson <thomasanderson@chromium.org>
2018-04-05 16:55:58 -0700

    Fix build with glibc 2.27
    Glibc now defines PTRACE_GET_THREAD_AREA as an enum value.  Trying to define our
    own will result in an error:
    
    ../../third_party/crashpad/crashpad/compat/linux/sys/ptrace.h:25:35: error: redefinition of 'PTRACE_GET_THREAD_AREA' as different kind of symbol
    static constexpr __ptrace_request PTRACE_GET_THREAD_AREA =
                                      ^
    ../../build/linux/debian_sid_amd64-sysroot/usr/include/x86_64-linux-gnu/sys/ptrace.h:110:3: note: previous definition is here
      PTRACE_GET_THREAD_AREA = 25,
    
    However, glibc also defines a new macro for the corresponding value, so it's
    possible to detect this case:
    
    ----- ptrace.h -----
      /* Get a TLS entry in the GDT.  */
      PTRACE_GET_THREAD_AREA = 25,
    ----- ptrace.h -----
    
    This CL prevents defining our own PTRACE_GET_THREAD_AREA when PT_GET_THREAD_AREA
    is defined.
    
    Bug: None
    Change-Id: Idf931e54dadd57788f04da47f12f0f0588a255cc

--- compat/linux/sys/ptrace.h
+++ compat/linux/sys/ptrace.h
@@ -20,7 +20,8 @@
 #include <sys/cdefs.h>
 
 // https://sourceware.org/bugzilla/show_bug.cgi?id=22433
-#if !defined(PTRACE_GET_THREAD_AREA) && defined(__GLIBC__)
+#if !defined(PTRACE_GET_THREAD_AREA) && !defined(PT_GET_THREAD_AREA) && \
+    defined(__GLIBC__)
 #if defined(__i386__) || defined(__x86_64__)
 static constexpr __ptrace_request PTRACE_GET_THREAD_AREA =
     static_cast<__ptrace_request>(25);
