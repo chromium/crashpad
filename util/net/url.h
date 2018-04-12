eca0ea8427c2897088b61fb266960288363367eb
Scott Graham <scottmg@chromium.org>
2018-04-12 14:42:20 -0700

    Add limited version of URL cracking
    This is a very basic form of URL cracking to break a
    HTTPTransport::SetURL() argument up into component parts. This is split
    out of the (upcoming)
    https://chromium-review.googlesource.com/c/crashpad/crashpad/+/1008407
    for Linux and Fuchsia.
    
    Bug: crashpad:196
    Change-Id: Iba075d9c8720c14550ce53e23d684362da84740c
    Reviewed-on: https://chromium-review.googlesource.com/1010972
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>

--- util/net/url.h
+++ util/net/url.h
@@ -26,6 +26,25 @@ namespace crashpad {
 //! \return The encoded string.
 std::string URLEncode(const std::string& url);
 
+//! \brief Crack a URL into component parts.
+//!
+//! This is not a general function, and works only on the limited style of URLs
+//! that are expected to be used by HTTPTransport::SetURL().
+//!
+//! \param[in] url The URL to crack.
+//! \param[out] scheme The request scheme, either http or https.
+//! \param[out] host The hostname.
+//! \param[out] port The port.
+//! \param[out] rest The remainder of the URL (both resource and URL params).
+//! \return `true` on success in which case all output parameters will be filled
+//!     out, or `false` on failure, in which case the output parameters will be
+//!     unmodified and an error will be logged.
+bool CrackURL(const std::string& url,
+              std::string* scheme,
+              std::string* host,
+              std::string* port,
+              std::string* rest);
+
 }  // namespace crashpad
 
 #endif  // CRASHPAD_UTIL_NET_URL_H_
