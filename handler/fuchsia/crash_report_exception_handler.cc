56b2678797853efc585d7346b1da09d09b7690c2
Scott Graham <scottmg@chromium.org>
2018-05-01 11:32:21 -0700

    fuchsia: Flesh out CrashReportExceptionHandler
    Now initializes the exception in the process snapshot and writes out a
    minidump on exception.
    
    Bug: crashpad:196
    Change-Id: I06ba604331f067981bf47584a52e8a2528acbac5

--- handler/fuchsia/crash_report_exception_handler.cc
+++ handler/fuchsia/crash_report_exception_handler.cc
@@ -17,6 +17,12 @@
 #include <zircon/syscalls/exception.h>
 
 #include "base/logging.h"
+#include "client/settings.h"
+#include "minidump/minidump_file_writer.h"
+#include "minidump/minidump_user_extension_stream_data_source.h"
+#include "snapshot/fuchsia/process_snapshot_fuchsia.h"
+#include "util/fuchsia/scoped_task_suspend.h"
+#include "util/misc/metrics.h"
 
 namespace crashpad {
 
@@ -53,17 +59,104 @@ CrashReportExceptionHandler::CrashReportExceptionHandler(
     CrashReportDatabase* database,
     CrashReportUploadThread* upload_thread,
     const std::map<std::string, std::string>* process_annotations,
-    const UserStreamDataSources* user_stream_data_sources) {}
+    const UserStreamDataSources* user_stream_data_sources)
+    : database_(database),
+      upload_thread_(upload_thread),
+      process_annotations_(process_annotations),
+      user_stream_data_sources_(user_stream_data_sources) {}
 
 CrashReportExceptionHandler::~CrashReportExceptionHandler() = default;
 
 bool CrashReportExceptionHandler::HandleException(uint32_t type,
                                                   uint64_t pid,
                                                   uint64_t tid) {
+  Metrics::ExceptionEncountered();
+
   LOG(ERROR) << "got type=" << ExceptionTypeName(type) << " (" << type
              << "), pid=" << pid << ", tid=" << tid;
-  LOG(ERROR) << "TODO: WRITE AN EXCEPTION REPORT";
 
+  // TODO(scottmg): This is what crashlogger does, but I think it's being bad,
+  // and this use of parent will probably go away. Replace this with something
+  // else when it does.
+  zx_handle_t process_raw;
+  zx_status_t status = zx_object_get_child(
+      ZX_HANDLE_INVALID, pid, ZX_RIGHT_SAME_RIGHTS, &process_raw);
+  if (status != ZX_OK) {
+    Metrics::ExceptionCaptureResult(Metrics::CaptureResult::kSnapshotFailed);
+    ZX_LOG(ERROR, status) << "zx_object_get_child process";
+    return false;
+  }
+  base::ScopedZxHandle process(process_raw);
+
+  ScopedTaskSuspend suspend(process.get());
+
+  ProcessSnapshotFuchsia process_snapshot;
+  if (!process_snapshot.Initialize(process.get())) {
+    Metrics::ExceptionCaptureResult(Metrics::CaptureResult::kSnapshotFailed);
+    return false;
+  }
+
+  CrashpadInfoClientOptions client_options;
+  process_snapshot.GetCrashpadOptions(&client_options);
+
+  if (client_options.crashpad_handler_behavior != TriState::kDisabled) {
+    if (!process_snapshot.InitializeException(type, process.get(), tid)) {
+      Metrics::ExceptionCaptureResult(
+          Metrics::CaptureResult::kExceptionInitializationFailed);
+      return false;
+    }
+
+    UUID client_id;
+    Settings* const settings = database_->GetSettings();
+    if (settings) {
+      // If GetSettings() or GetClientID() fails, something else will log a
+      // message and client_id will be left at its default value, all zeroes,
+      // which is appropriate.
+      settings->GetClientID(&client_id);
+    }
+
+    process_snapshot.SetClientID(client_id);
+    process_snapshot.SetAnnotationsSimpleMap(*process_annotations_);
+
+    std::unique_ptr<CrashReportDatabase::NewReport> new_report;
+    CrashReportDatabase::OperationStatus database_status =
+        database_->PrepareNewCrashReport(&new_report);
+    if (database_status != CrashReportDatabase::kNoError) {
+      Metrics::ExceptionCaptureResult(
+          Metrics::CaptureResult::kPrepareNewCrashReportFailed);
+      return false;
+    }
+
+    process_snapshot.SetReportID(new_report->ReportID());
+
+    MinidumpFileWriter minidump;
+    minidump.InitializeFromSnapshot(&process_snapshot);
+    AddUserExtensionStreams(
+        user_stream_data_sources_, &process_snapshot, &minidump);
+
+    if (!minidump.WriteEverything(new_report->Writer())) {
+      Metrics::ExceptionCaptureResult(
+          Metrics::CaptureResult::kMinidumpWriteFailed);
+      return false;
+    }
+
+    UUID uuid;
+    database_status =
+        database_->FinishedWritingCrashReport(std::move(new_report), &uuid);
+    if (database_status != CrashReportDatabase::kNoError) {
+      Metrics::ExceptionCaptureResult(
+          Metrics::CaptureResult::kFinishedWritingCrashReportFailed);
+      return false;
+    }
+
+    if (upload_thread_) {
+      upload_thread_->ReportPending(uuid);
+    }
+  }
+
+  // TODO(scottmg): If it was disabled, pass it on?
+
+  Metrics::ExceptionCaptureResult(Metrics::CaptureResult::kSuccess);
   return true;
 }
 
