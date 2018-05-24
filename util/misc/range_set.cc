b9c45976093e28abc6f38646dba97fa8f5872c49
Joshua Peraza <jperaza@chromium.org>
2018-05-24 09:28:41 -0700

    Add ProcessSnapshotSanitized
    A ProcessSnapshotSanitized enables filtering possibly sensitive
    information from a snapshot.
    
    WebView has different privacy constraints than Chrome and needs to
    avoid collecting data in annotations or from stack memory that may
    contain PII. This CL enables:
    
    1. Filtering annotations by name using a whitelist.
    2. Filtering for crashes which reference a particular module.
    3. Redacting non-essential information from stack memory.
    
    Bug: crashpad:30
    Change-Id: I8944c70fdcca6d6d4b7955d983320909bf871254

---
+++ util/misc/range_set.cc
@@ -0,0 +1,60 @@
+// Copyright 2018 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/misc/range_set.h"
+
+#include "base/logging.h"
+
+namespace crashpad {
+
+RangeSet::RangeSet() : ranges_() {
+  memset(bloom_filter_, 0, sizeof(bloom_filter_));
+}
+
+RangeSet::~RangeSet() = default;
+
+#define FILTER_INDEX(address) \
+  ((address & 0xffffffff) >> (32 - kFilterAddressBits))
+
+#define SET_FILTER_BIT(index)                      \
+  do {                                             \
+    bloom_filter_[index >> 3] |= 1 << (index & 7); \
+  } while (0)
+
+#define GET_FILTER_BIT(index) (bloom_filter_[index >> 3] & (1 << (index & 7)))
+
+void RangeSet::Insert(VMAddress base, VMSize size) {
+  ranges_.push_back(std::make_pair(base, size));
+
+  for (size_t bit_index = FILTER_INDEX(base);
+       bit_index < FILTER_INDEX(base + size);
+       ++bit_index) {
+    SET_FILTER_BIT(bit_index);
+  }
+}
+
+bool RangeSet::Contains(VMAddress address) {
+  if (!GET_FILTER_BIT(FILTER_INDEX(address))) {
+    return false;
+  }
+
+  for (const auto& range : ranges_) {
+    if (address >= range.first && address < range.first + range.second) {
+      return true;
+    }
+  }
+  return false;
+}
+
+}  // namespace crashpad
