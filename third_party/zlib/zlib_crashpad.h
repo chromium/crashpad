3072b4059fbef261975bd5fc7363226b109b21fd
Charly Delay <delay@google.com>
2018-07-06 13:10:46 -0400

    zlib.gyp: support the "external" dependency mode
    This CL implements support for the external model for Crashpad's zlib
    dependency, in which the dependencies and Crashpad are checked out as
    sibling in the same directory.
    
    Bug: crashpad:
    Change-Id: I0ca640e0be9b6a4fd8379026dfc8eb061b40badf
    Reviewed-on: https://chromium-review.googlesource.com/1124778
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>

--- third_party/zlib/zlib_crashpad.h
+++ third_party/zlib/zlib_crashpad.h
@@ -19,9 +19,8 @@
 // available at any other location in the source tree. It will #include the
 // proper <zlib.h> depending on how the build has been configured.
 
-#if defined(CRASHPAD_ZLIB_SOURCE_EXTERNAL)
-#include "third_party/zlib/zlib.h"
-#elif defined(CRASHPAD_ZLIB_SOURCE_SYSTEM)
+#if defined(CRASHPAD_ZLIB_SOURCE_SYSTEM) || \
+    defined(CRASHPAD_ZLIB_SOURCE_EXTERNAL)
 #include <zlib.h>
 #elif defined(CRASHPAD_ZLIB_SOURCE_EMBEDDED)
 #include "third_party/zlib/zlib/zlib.h"
