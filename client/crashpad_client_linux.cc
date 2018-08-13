ef036c2a4ba72d5d21635dc1ba9507a3f985b731
Joshua Peraza <jperaza@chromium.org>
2018-08-13 15:10:45 -0700

    linux: Use correct handler argument for client FDs
    This worked before because getopt also accepts prefixes of known
    options.
    
    Change-Id: I0a479ad17954c541e84dc77230abcff19e8fae72

--- client/crashpad_client_linux.cc
+++ client/crashpad_client_linux.cc
@@ -199,7 +199,7 @@ bool CrashpadClient::StartHandlerForClient(
   std::vector<std::string> argv = BuildHandlerArgvStrings(
       handler, database, metrics_dir, url, annotations, arguments);
 
-  argv.push_back(FormatArgumentInt("initial-client", socket));
+  argv.push_back(FormatArgumentInt("initial-client-fd", socket));
 
   return DoubleForkAndExec(argv, socket, true, nullptr);
 }
