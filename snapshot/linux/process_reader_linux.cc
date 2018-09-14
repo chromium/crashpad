0e58f1db820638f76d94ef3544613cf62726289a
Joshua Peraza <jperaza@chromium.org>
2018-09-14 08:52:47 -0700

    linux: allow collecting scheduler priorities to fail
    SELinux blocks the handler from collecting these values on Android M.
    They should eventually be collected via the broker.
    
    Change-Id: Iad47759b2ebf23148cb5b2c401241ee87f8ffd27

--- snapshot/linux/process_reader_linux.cc
+++ snapshot/linux/process_reader_linux.cc
@@ -60,20 +60,24 @@ bool ProcessReaderLinux::Thread::InitializePtrace(
     return false;
   }
 
+  // TODO(jperaza): Collect scheduling priorities via the broker when they can't
+  // be collected directly.
+  have_priorities = false;
+
   // TODO(jperaza): Starting with Linux 3.14, scheduling policy, static
   // priority, and nice value can be collected all in one call with
   // sched_getattr().
   int res = sched_getscheduler(tid);
   if (res < 0) {
     PLOG(ERROR) << "sched_getscheduler";
-    return false;
+    return true;
   }
   sched_policy = res;
 
   sched_param param;
   if (sched_getparam(tid, &param) != 0) {
     PLOG(ERROR) << "sched_getparam";
-    return false;
+    return true;
   }
   static_priority = param.sched_priority;
 
@@ -81,10 +85,11 @@ bool ProcessReaderLinux::Thread::InitializePtrace(
   res = getpriority(PRIO_PROCESS, tid);
   if (res == -1 && errno) {
     PLOG(ERROR) << "getpriority";
-    return false;
+    return true;
   }
   nice_value = res;
 
+  have_priorities = true;
   return true;
 }
 
