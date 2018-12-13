f4aed4177328c1e4ba3f81440e5363672bcd2765
Scott Graham <scottmg@chromium.org>
2018-12-13 13:19:46 -0800

    fuchsia: Use process version of task suspension
    Bug: crashpad:269
    Change-Id: I1bac4afe030528bc7358464a95bd160905dfa448

--- snapshot/fuchsia/process_snapshot_fuchsia.cc
+++ snapshot/fuchsia/process_snapshot_fuchsia.cc
@@ -41,13 +41,6 @@ bool ProcessSnapshotFuchsia::Initialize(const zx::process& process) {
   InitializeThreads();
   InitializeModules();
 
-  for (const auto& entry : process_reader_.MemoryMap()->Entries()) {
-    if (entry.type == ZX_INFO_MAPS_TYPE_MAPPING) {
-      memory_map_.push_back(
-          std::make_unique<internal::MemoryMapRegionSnapshotFuchsia>(entry));
-    }
-  }
-
   INITIALIZATION_STATE_SET_VALID(initialized_);
   return true;
 }
@@ -182,11 +175,7 @@ const ExceptionSnapshot* ProcessSnapshotFuchsia::Exception() const {
 std::vector<const MemoryMapRegionSnapshot*> ProcessSnapshotFuchsia::MemoryMap()
     const {
   INITIALIZATION_STATE_DCHECK_VALID(initialized_);
-  std::vector<const MemoryMapRegionSnapshot*> memory_map;
-  for (const auto& item : memory_map_) {
-    memory_map.push_back(item.get());
-  }
-  return memory_map;
+  return std::vector<const MemoryMapRegionSnapshot*>();
 }
 
 std::vector<HandleSnapshot> ProcessSnapshotFuchsia::Handles() const {
