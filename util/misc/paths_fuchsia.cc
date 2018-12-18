ff86cf165e307d822a233b37f53af6fce1f92397
Nathan Rogers <nathanrogers@google.com>
2018-12-17 22:33:08 -0800

    fuchsia: Update Paths::Executable documentation
    Update the "...namespaces.md..." link to its current URL.
    
    Change-Id: Ic30c6be5dba4f531e5b8a55af37555626398df5a

--- util/misc/paths_fuchsia.cc
+++ util/misc/paths_fuchsia.cc
@@ -25,7 +25,7 @@ namespace crashpad {
 // static
 bool Paths::Executable(base::FilePath* path) {
   // Assume the environment has been set up following
-  // https://fuchsia.googlesource.com/docs/+/master/namespaces.md#typical-directory-structure
+  // https://fuchsia.googlesource.com/docs/+/master/the-book/namespaces.md#typical-directory-structure
   // .
   *path = base::FilePath("/pkg/bin/app");
   return true;
