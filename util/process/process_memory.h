de1e61519f11827f23b9a00e72becb8b7d87505b
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2018-12-21 13:03:43 -0800

    Change ProcessMemory to accept VMSizes
    As Mark noted in [1] ProcessMemory should accept VMSize instead of
    size_t, the two types can differ on platforms where a cross-bitness
    handler could cause a 32-bit handler to inspect a 64-bit process. By
    centralizing the checks in ProcessMemory, we can leave the individual
    platform-specific implementations (in ProcessMemory*::ReadUpTo) to
    accept size_ts.
    
    [1] crrev.com/c/1388017/2/snapshot/crashpad_types/crashpad_info_reader.cc#70
    
    Bug: crashpad:270
    Change-Id: I3aab483221de36f3b1478cb9503101b142dae681

--- util/process/process_memory.h
+++ util/process/process_memory.h
@@ -46,7 +46,7 @@ class ProcessMemory {
   //!
   //! \return `true` on success, with \a buffer filled appropriately. `false` on
   //!     failure, with a message logged.
-  bool Read(VMAddress address, size_t size, void* buffer) const;
+  bool Read(VMAddress address, VMSize size, void* buffer) const;
 
   //! \brief Reads a `NUL`-terminated C string from the target process into a
   //!     string in the current process.
@@ -79,7 +79,7 @@ class ProcessMemory {
   //!     a `NUL` terminator is not found within \a size bytes, or when
   //!     encountering unmapped or unreadable pages.
   bool ReadCStringSizeLimited(VMAddress address,
-                              size_t size,
+                              VMSize size,
                               std::string* string) const {
     return ReadCStringInternal(address, true, size, string);
   }
@@ -124,7 +124,7 @@ class ProcessMemory {
   //!     encountering unmapped or unreadable pages.
   virtual bool ReadCStringInternal(VMAddress address,
                                    bool has_size,
-                                   size_t size,
+                                   VMSize size,
                                    std::string* string) const;
 };
 
