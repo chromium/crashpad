8612fe586f516107ad0896e0080019646600bd87
Petr Hosek <phosek@google.com>
2019-02-13 19:22:23 -0800

    Fix issues revealed by -ftrivial-auto-var-init=pattern
    -ftrivial-auto-var-init=pattern automatically initializes all variables
    with a pattern. This revealed two issues:
    
    1. Unitialized read of field from CrashpadInfoClientOptions.
    2. The PC distance check in TestCaptureContext (due to additional
    instrumentation, the distance is now 76 on x86-64 and 92 on aarch64).
    
    Change-Id: I528e5f21c70d2849c9300776da783fde59411e9e

--- snapshot/crashpad_info_client_options.cc
+++ snapshot/crashpad_info_client_options.cc
@@ -39,7 +39,8 @@ TriState CrashpadInfoClientOptions::TriStateFromCrashpadInfo(
 CrashpadInfoClientOptions::CrashpadInfoClientOptions()
     : crashpad_handler_behavior(TriState::kUnset),
       system_crash_reporter_forwarding(TriState::kUnset),
-      gather_indirectly_referenced_memory(TriState::kUnset) {
+      gather_indirectly_referenced_memory(TriState::kUnset),
+      indirectly_referenced_memory_cap(0) {
 }
 
 }  // namespace crashpad
