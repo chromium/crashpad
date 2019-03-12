1bb8ca4059d8bcc933478e02d7869dda7daa0263
Joshua Peraza <jperaza@chromium.org>
2019-03-12 14:33:01 -0700

    Silence some warnings
    These warnings create a lot of noise in the Android logcat
    
    Change-Id: I747a7f4cd61f4dcbb16c6dfcb3a1b4caeeaed06a
    Reviewed-on: https://chromium-review.googlesource.com/c/crashpad/crashpad/+/1518320
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>

--- snapshot/minidump/thread_snapshot_minidump.cc
+++ snapshot/minidump/thread_snapshot_minidump.cc
@@ -86,7 +86,6 @@ bool ThreadSnapshotMinidump::InitializeContext(
   }
 
   if (context_.architecture == CPUArchitecture::kCPUArchitectureX86) {
-    LOG(WARNING) << "Snapshot X86 context support has no unit tests.";
     context_memory_.resize(sizeof(CPUContextX86));
     context_.x86 = reinterpret_cast<CPUContextX86*>(context_memory_.data());
     const MinidumpContextX86* src =
@@ -180,7 +179,6 @@ bool ThreadSnapshotMinidump::InitializeContext(
     context_.x86_64->dr4 = src->dr6;
     context_.x86_64->dr5 = src->dr7;
   } else if (context_.architecture == CPUArchitecture::kCPUArchitectureARM) {
-    LOG(WARNING) << "Snapshot ARM32 context support has no unit tests.";
     context_memory_.resize(sizeof(CPUContextARM));
     context_.arm = reinterpret_cast<CPUContextARM*>(context_memory_.data());
     const MinidumpContextARM* src =
@@ -242,7 +240,6 @@ bool ThreadSnapshotMinidump::InitializeContext(
     context_.arm64->fpsr = src->fpsr;
     context_.arm64->spsr = src->cpsr;
   } else if (context_.architecture == CPUArchitecture::kCPUArchitectureMIPSEL) {
-    LOG(WARNING) << "Snapshot MIPS context support has no unit tests.";
     context_memory_.resize(sizeof(CPUContextMIPS));
     context_.mipsel = reinterpret_cast<CPUContextMIPS*>(context_memory_.data());
     const MinidumpContextMIPS* src =
@@ -278,7 +275,6 @@ bool ThreadSnapshotMinidump::InitializeContext(
     memcpy(&context_.mipsel->fpregs, &src->fpregs, sizeof(src->fpregs));
   } else if (context_.architecture ==
              CPUArchitecture::kCPUArchitectureMIPS64EL) {
-    LOG(WARNING) << "Snapshot MIPS64 context support has no unit tests.";
     context_memory_.resize(sizeof(CPUContextMIPS64));
     context_.mips64 =
       reinterpret_cast<CPUContextMIPS64*>(context_memory_.data());
