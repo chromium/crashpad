a47c1d9eddadd6b46f649fd60508319a19bc0311
Joshua Peraza <jperaza@chromium.org>
2019-04-25 09:26:37 -0700

    linux: add wrappers for send/recvmsg
    Change-Id: Id09f87125540255687a3c35d5bed7fa01ec07cff

---
+++ util/linux/socket.h
@@ -0,0 +1,29 @@
+// Copyright 2019 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_UTIL_LINUX_SOCKET_H_
+#define CRASHPAD_UTIL_LINUX_SOCKET_H_
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+namespace crashpad {
+
+int SendMsg(int fd, const void* buf, size_t buf_size);
+
+bool RecvMsg(int fd, void* buf, size_t buf_size, ucred* creds);
+
+}  // namespace crashpad
+
+#endif  // CRASHPAD_UTIL_LINUX_SOCKET_H_
