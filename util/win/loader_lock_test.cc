11f748b78f9200d3e5cdd96b90c5d7704851a6bd
James Forshaw <forshaw@chromium.org>
2019-12-02 14:33:13 -0800

    [Windows] Add checks for DLL loader lock.
    This CL adds code to check if the current thread holds the DLL loader
    lock. This code can be used to enforce the requirement that certain
    parts of crashpad, such as process creation are not done during calls
    to DllMain which can lead to deadlocks and crashes. Only one check is
    current enforced, in client process creation, and only in debug builds.
    
    Bug: crashpad: 316
    Change-Id: I5757a264bbf28ce2ab88a0cd7ac9481e46428c17

---
+++ util/win/loader_lock_test.cc
@@ -0,0 +1,39 @@
+// Copyright 2019 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/win/loader_lock.h"
+
+#include "build/build_config.h"
+#include "gtest/gtest.h"
+#include "util/win/get_function.h"
+
+extern "C" bool LoaderLockDetected();
+
+namespace crashpad {
+namespace test {
+namespace {
+
+#ifdef ARCH_CPU_X86_FAMILY
+TEST(LoaderLock, Detected) {
+  EXPECT_FALSE(IsThreadInLoaderLock());
+  auto* loader_lock_detected =
+      GET_FUNCTION(L"loader_lock_test_dll.dll", LoaderLockDetected);
+  ASSERT_NE(loader_lock_detected, nullptr);
+  EXPECT_TRUE(loader_lock_detected());
+}
+#endif  // ARCH_CPU_X86_FAMILY
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
