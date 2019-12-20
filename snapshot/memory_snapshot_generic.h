696916fd97f1f99fe9fc50d48db4aed54527fe26
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-12-19 16:11:33 -0800

    Update comment to match current state of MemorySnapshotGeneric
    MemorySnapshotGeneric is now used on all platforms as it uses
    ProcessMemory instead of templating to use a platform-specific
    ProcessReader* class.
    
    Change-Id: Ib0b6db0fac184c558f1c76a584db92e4804dfe99

--- snapshot/memory_snapshot_generic.h
+++ snapshot/memory_snapshot_generic.h
@@ -28,9 +28,8 @@
 namespace crashpad {
 namespace internal {
 
-//! \brief A MemorySnapshot of a memory region in a process on the running
-//!     system. Used on Mac, Linux, Android, and Fuchsia, templated on the
-//!     platform-specific ProcessReader type.
+//! \brief An implementation of MemorySnapshot, a snapshot of a memory region in
+//!     a process on the running, used on all platforms.
 class MemorySnapshotGeneric final : public MemorySnapshot {
  public:
   MemorySnapshotGeneric() = default;
