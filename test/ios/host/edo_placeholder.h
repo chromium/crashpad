cc02c945df5e8f662ef4f4321de1e6a5d3d0129c
Justin Cohen <justincohen@google.com>
2020-01-29 21:33:01 -0500

    Add plumbing for xcuitest.
    Also adds basic integration for EDO.
    
    Change-Id: If3d193a4967a566b55d6d446585e2988dacad6e6

---
+++ test/ios/host/edo_placeholder.h
@@ -0,0 +1,15 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRASHPAD_TEST_IOS_HOST_EDO_PLACEHOLDER_H_
+#define CRASHPAD_TEST_IOS_HOST_EDO_PLACEHOLDER_H_
+
+#import <UIKit/UIKit.h>
+
+@interface EDOPlaceholder : NSObject
+-(NSString*)testEDO;
+@end
+
+#endif  // CRASHPAD_TEST_IOS_HOST_EDO_PLACEHOLDER_H_
+
