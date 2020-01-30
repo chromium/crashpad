d8bd1ef0a253389b19139e0db71c63c73812b539
Justin Cohen <justincohen@google.com>
2020-01-29 19:38:25 -0500

    Add plumbing for xcuitest.
    Also adds basic integration for EDO.
    
    Change-Id: If3d193a4967a566b55d6d446585e2988dacad6e6

---
+++ test/ios/host/application_delegate.h
@@ -0,0 +1,13 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CRASHPAD_TEST_IOS_HOST_APPLICATION_DELEGATE_H_
+#define CRASHPAD_TEST_IOS_HOST_APPLICATION_DELEGATE_H_
+
+#import <UIKit/UIKit.h>
+
+@interface ApplicationDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+#endif  // CRASHPAD_TEST_IOS_HOST_APPLICATION_DELEGATE_H_
