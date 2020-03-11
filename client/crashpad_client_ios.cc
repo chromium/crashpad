dd5862358c56f1465c89f11aae21a2132c1b7a03
Justin Cohen <justincohen@google.com>
2020-03-10 23:12:21 -0400

    [ios] Bring up first draft system snapshot and iOS data collector.
    Gather most of the necessary information for the system snapshot.
    
    Note that:
     - The 'capture' portion of this CL will be moved out of the snapshot
       interface and into a separate in-process dump to disk location.
     - All of the pointer dereferences need to be wrapped in vm_read.
     - The read-fast-and-dump logic in thread_snapshot may end up in a
       different file completely, but until we pick a
       serialization/deserialization method, keep it as-is.
    
    
    Change-Id: Iac82491fdb4a823163f02149f52a1e18e26fa9de

--- client/crashpad_client_ios.cc
+++ client/crashpad_client_ios.cc
@@ -19,7 +19,10 @@
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
 #include "client/client_argv_handling.h"
+#include "client/crash_report_database.h"
+#include "minidump/minidump_file_writer.h"
 #include "snapshot/ios/process_snapshot_ios.h"
+#include "util/ios/ios_system_data_collector.h"
 #include "util/posix/signals.h"
 
 namespace crashpad {
@@ -43,7 +46,40 @@ class SignalHandler {
   void HandleCrash(int signo, siginfo_t* siginfo, void* context) {
     // TODO(justincohen): This is incomplete.
     ProcessSnapshotIOS process_snapshot;
-    process_snapshot.Initialize();
+    process_snapshot.Initialize(&system_data);
+
+    // TODO(justincohen): For testing only!
+    // To be removed before landing CL, but super useful in testing.
+    {
+      char* tmpdir = getenv("TMPDIR");
+      std::string dir;
+      dir.assign(tmpdir);
+      dir.append("org.chromium.crashpad.test.XXXXXX");
+      mkdtemp(&dir[0]);
+      std::unique_ptr<CrashReportDatabase> database(
+          CrashReportDatabase::Initialize(base::FilePath(dir)));
+      std::unique_ptr<CrashReportDatabase::NewReport> new_report;
+      CrashReportDatabase::OperationStatus database_status =
+          database->PrepareNewCrashReport(&new_report);
+      if (database_status != CrashReportDatabase::kNoError) {
+        Metrics::ExceptionCaptureResult(
+            Metrics::CaptureResult::kPrepareNewCrashReportFailed);
+      }
+      process_snapshot.SetReportID(new_report->ReportID());
+      MinidumpFileWriter minidump;
+      minidump.InitializeFromSnapshot(&process_snapshot);
+      if (!minidump.WriteEverything(new_report->Writer())) {
+        Metrics::ExceptionCaptureResult(
+            Metrics::CaptureResult::kMinidumpWriteFailed);
+      }
+      UUID uuid;
+      database_status =
+          database->FinishedWritingCrashReport(std::move(new_report), &uuid);
+      if (database_status != CrashReportDatabase::kNoError) {
+        Metrics::ExceptionCaptureResult(
+            Metrics::CaptureResult::kFinishedWritingCrashReportFailed);
+      }
+    }
   }
 
  private:
@@ -67,6 +103,9 @@ class SignalHandler {
   }
 
   Signals::OldActions old_actions_ = {};
+
+  // Collect some system data before the signal handler is triggered.
+  IOSSystemDataCollector system_data;
 
   DISALLOW_COPY_AND_ASSIGN(SignalHandler);
 };
