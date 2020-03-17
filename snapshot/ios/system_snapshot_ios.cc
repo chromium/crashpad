3db21d71a2bcf7a13c28ed6d5eaf626e51abf060
Justin Cohen <justincohen@google.com>
2020-03-17 16:21:01 -0400

    [ios] Bring up first draft system snapshot and iOS data collector.
    Gather most of the necessary information for the system snapshot.
    
    Note that:
     - The 'capture' portion of this CL will be moved out of the snapshot
       interface and into a separate in-process dump to disk location.
     - All of the pointer dereferences need to be wrapped in vm_read.
     - The read-fast-and-dump logic in thread_snapshot may end up in a
       different file completely, but until we pick a
       serialization/deserialization method, keep it as-is.
    
    
    Change-Id: Iac82491fdb4a823163f02149f52a1e18e26fa9de

---
+++ snapshot/ios/system_snapshot_ios.cc
@@ -0,0 +1,197 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "snapshot/ios/system_snapshot_ios.h"
+
+#include <mach/mach.h>
+#include <stddef.h>
+#include <sys/sysctl.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+
+#include <algorithm>
+
+#include "base/logging.h"
+#include "base/strings/stringprintf.h"
+#include "build/build_config.h"
+#include "snapshot/cpu_context.h"
+#include "snapshot/posix/timezone.h"
+#include "util/mac/mac_util.h"
+#include "util/numeric/in_range_cast.h"
+
+namespace crashpad {
+
+namespace internal {
+
+SystemSnapshotIOS::SystemSnapshotIOS()
+    : SystemSnapshot(),
+      os_version_build_(),
+      os_version_major_(0),
+      os_version_minor_(0),
+      os_version_bugfix_(0),
+      initialized_() {}
+
+SystemSnapshotIOS::~SystemSnapshotIOS() {}
+
+void SystemSnapshotIOS::Initialize(const IOSSystemDataCollector* system_data) {
+  INITIALIZATION_STATE_SET_INITIALIZING(initialized_);
+
+  os_version_major_ = system_data->MajorVersion();
+  os_version_minor_ = system_data->MinorVersion();
+  os_version_bugfix_ = system_data->PatchVersion();
+  os_version_build_ = system_data->Build();
+  machine_ = system_data->Machine();
+  cpu_count_ = system_data->ProcessorCount();
+  dst_status_ = system_data->DSTStatus();
+  standard_offset_seconds_ = system_data->StandardOffsetSeconds();
+  daylight_offset_seconds_ = system_data->DaylightOffsetSeconds();
+  standard_name_ = system_data->StandardName();
+  daylight_name_ = system_data->DaylightName();
+
+  // Currently unused by minidump.
+  mach_msg_type_number_t hostSize;
+  vm_size_t pageSize;
+  vm_statistics_data_t vmStat;
+  hostSize = sizeof(vm_statistics_data_t) / sizeof(integer_t);
+  host_page_size(mach_host_self(), &pageSize);
+  if (host_statistics(
+          mach_host_self(), HOST_VM_INFO, (host_info_t)&vmStat, &hostSize) !=
+      KERN_SUCCESS) {
+  }
+  active_ = vmStat.active_count * pageSize;
+  inactive_ = vmStat.inactive_count * pageSize;
+  wired_ = vmStat.wire_count * pageSize;
+  free_ = vmStat.free_count * pageSize;
+
+  INITIALIZATION_STATE_SET_VALID(initialized_);
+}
+
+CPUArchitecture SystemSnapshotIOS::GetCPUArchitecture() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+#if defined(ARCH_CPU_X86_FAMILY)
+  return kCPUArchitectureX86_64;
+#else
+  return kCPUArchitectureARM64;
+#endif
+}
+
+uint32_t SystemSnapshotIOS::CPURevision() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return 0l;
+}
+
+uint8_t SystemSnapshotIOS::CPUCount() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return cpu_count_;
+}
+
+std::string SystemSnapshotIOS::CPUVendor() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return std::string();
+}
+
+void SystemSnapshotIOS::CPUFrequency(uint64_t* current_hz,
+                                     uint64_t* max_hz) const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  *current_hz = 0;
+  *max_hz = 0;
+}
+
+uint32_t SystemSnapshotIOS::CPUX86Signature() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return 0;
+}
+
+uint64_t SystemSnapshotIOS::CPUX86Features() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return 0;
+}
+
+uint64_t SystemSnapshotIOS::CPUX86ExtendedFeatures() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return 0;
+}
+
+uint32_t SystemSnapshotIOS::CPUX86Leaf7Features() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return 0;
+}
+
+bool SystemSnapshotIOS::CPUX86SupportsDAZ() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return false;
+}
+
+SystemSnapshot::OperatingSystem SystemSnapshotIOS::GetOperatingSystem() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return kOperatingSystemIOS;
+}
+
+bool SystemSnapshotIOS::OSServer() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return false;
+}
+
+void SystemSnapshotIOS::OSVersion(int* major,
+                                  int* minor,
+                                  int* bugfix,
+                                  std::string* build) const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  *major = os_version_major_;
+  *minor = os_version_minor_;
+  *bugfix = os_version_bugfix_;
+  build->assign(os_version_build_);
+}
+
+std::string SystemSnapshotIOS::OSVersionFull() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  if (os_version_bugfix_) {
+    return base::StringPrintf("%d.%d.%d %s",
+                              os_version_major_,
+                              os_version_minor_,
+                              os_version_bugfix_,
+                              os_version_build_.c_str());
+  } else {
+    return base::StringPrintf("%d.%d %s",
+                              os_version_major_,
+                              os_version_minor_,
+                              os_version_build_.c_str());
+  }
+}
+
+std::string SystemSnapshotIOS::MachineDescription() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return machine_;
+}
+
+bool SystemSnapshotIOS::NXEnabled() const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  return false;
+}
+
+void SystemSnapshotIOS::TimeZone(DaylightSavingTimeStatus* dst_status,
+                                 int* standard_offset_seconds,
+                                 int* daylight_offset_seconds,
+                                 std::string* standard_name,
+                                 std::string* daylight_name) const {
+  INITIALIZATION_STATE_DCHECK_VALID(initialized_);
+  *dst_status = dst_status_;
+  *standard_offset_seconds = standard_offset_seconds_;
+  *daylight_offset_seconds = daylight_offset_seconds_;
+  standard_name->assign(standard_name_);
+  daylight_name->assign(daylight_name_);
+}
+
+}  // namespace internal
+}  // namespace crashpad
---
+++ util/ios/ios_system_data_collector.mm
@@ -0,0 +1,185 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <sys/sysctl.h>
+#include <sys/utsname.h>
+
+#include <string>
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#include "base/mac/mach_logging.h"
+#include "util/ios/ios_system_data_collector.h"
+
+namespace {
+
+std::string ReadStringSysctlByName(const char* name) {
+  size_t buf_len;
+  if (sysctlbyname(name, nullptr, &buf_len, nullptr, 0) != 0) {
+    PLOG(WARNING) << "sysctlbyname (size) " << name;
+    return std::string();
+  }
+
+  if (buf_len == 0) {
+    return std::string();
+  }
+
+  std::string value(buf_len - 1, '\0');
+  if (sysctlbyname(name, &value[0], &buf_len, nullptr, 0) != 0) {
+    PLOG(WARNING) << "sysctlbyname " << name;
+    return std::string();
+  }
+
+  return value;
+}
+
+}  // namespace
+
+namespace crashpad {
+
+IOSSystemDataCollector::IOSSystemDataCollector() {
+  build_ = ReadStringSysctlByName("kern.osversion");
+
+  NSOperatingSystemVersion version =
+      [[NSProcessInfo processInfo] operatingSystemVersion];
+  major_version_ = version.majorVersion;
+  minor_version_ = version.minorVersion;
+  patch_version_ = version.patchVersion;
+  processor_count_ = [[NSProcessInfo processInfo] processorCount];
+
+#if !TARGET_IPHONE_SIMULATOR
+  utsname uts;
+  if (uname(&uts) == 0) {
+    machine_ = uts.machine;
+  }
+#else
+  switch (UI_USER_INTERFACE_IDIOM()) {
+    case UIUserInterfaceIdiomPhone:
+      machine_ = "iOS Simulator (iPhone)";
+      break;
+    case UIUserInterfaceIdiomPad:
+      machine_ = "iOS Simulator (iPad)";
+      break;
+    default:
+      machine_ = "iOS Simulator (Unknown)";
+      break;
+  }
+#endif
+
+  // Timezone.
+  SystemTimeZoneDidChangeNotification();
+  CFNotificationCenterAddObserver(
+      CFNotificationCenterGetLocalCenter(),
+      this,
+      IOSSystemDataCollector::SystemTimeZoneDidChangeNotificationHandler,
+      (CFStringRef)NSSystemTimeZoneDidChangeNotification,
+      NULL,
+      CFNotificationSuspensionBehaviorDeliverImmediately);
+
+  // Orientation.
+  orientation_ = [[UIDevice currentDevice] orientation];
+  CFNotificationCenterAddObserver(
+      CFNotificationCenterGetLocalCenter(),
+      this,
+      IOSSystemDataCollector::OrientationDidChangeNotificationHandler,
+      (CFStringRef)UIDeviceOrientationDidChangeNotification,
+      NULL,
+      CFNotificationSuspensionBehaviorDeliverImmediately);
+
+  // Application state.
+  application_state_ = [[UIApplication sharedApplication] applicationState];
+  CFNotificationCenterAddObserver(
+      CFNotificationCenterGetLocalCenter(),
+      this,
+      IOSSystemDataCollector::ApplicationStateChangeNotificationHandler,
+      (CFStringRef)UIApplicationDidEnterBackgroundNotification,
+      NULL,
+      CFNotificationSuspensionBehaviorDeliverImmediately);
+  CFNotificationCenterAddObserver(
+      CFNotificationCenterGetLocalCenter(),
+      this,
+      IOSSystemDataCollector::ApplicationStateChangeNotificationHandler,
+      (CFStringRef)UIApplicationDidBecomeActiveNotification,
+      NULL,
+      CFNotificationSuspensionBehaviorDeliverImmediately);
+}
+
+void IOSSystemDataCollector::SystemTimeZoneDidChangeNotificationHandler(
+    CFNotificationCenterRef center,
+    void* observer,
+    CFStringRef name,
+    const void* object,
+    CFDictionaryRef userInfo) {
+  (static_cast<IOSSystemDataCollector*>(observer))
+      ->SystemTimeZoneDidChangeNotification();
+}
+
+void IOSSystemDataCollector::SystemTimeZoneDidChangeNotification() {
+  NSTimeZone* time_zone = NSTimeZone.localTimeZone;
+  NSDate* transition =
+      [time_zone nextDaylightSavingTimeTransitionAfterDate:[NSDate date]];
+  if (transition == nil) {
+    dst_status_ = SystemSnapshot::kDoesNotObserveDaylightSavingTime;
+    daylight_name_ = [[time_zone abbreviation] UTF8String];
+    standard_name_ = daylight_name_;
+  } else if (time_zone.isDaylightSavingTime) {
+    dst_status_ = SystemSnapshot::kObservingDaylightSavingTime;
+    daylight_offset_seconds_ = [time_zone secondsFromGMT];
+    standard_offset_seconds_ = [time_zone secondsFromGMTForDate:transition];
+    daylight_name_ = [[time_zone abbreviation] UTF8String];
+    standard_name_ = [[time_zone abbreviationForDate:transition] UTF8String];
+  } else {
+    dst_status_ = SystemSnapshot::kObservingStandardTime;
+    standard_name_ = [[time_zone abbreviation] UTF8String];
+    daylight_name_ = [[time_zone abbreviationForDate:transition] UTF8String];
+    standard_offset_seconds_ = [time_zone secondsFromGMT];
+    daylight_offset_seconds_ = [time_zone secondsFromGMTForDate:transition];
+  }
+}
+
+void IOSSystemDataCollector::ApplicationStateChangeNotificationHandler(
+    CFNotificationCenterRef center,
+    void* observer,
+    CFStringRef name,
+    const void* object,
+    CFDictionaryRef userInfo) {
+  (static_cast<IOSSystemDataCollector*>(observer))
+      ->ApplicationStateDidChangeNotification();
+}
+
+void IOSSystemDataCollector::ApplicationStateDidChangeNotification() {
+  application_state_ = [[UIApplication sharedApplication] applicationState];
+}
+
+void IOSSystemDataCollector::OrientationDidChangeNotificationHandler(
+    CFNotificationCenterRef center,
+    void* observer,
+    CFStringRef name,
+    const void* object,
+    CFDictionaryRef userInfo) {
+  (static_cast<IOSSystemDataCollector*>(observer))
+      ->OrientationDidChangeNotification();
+}
+
+void IOSSystemDataCollector::OrientationDidChangeNotification() {
+  orientation_ = [[UIDevice currentDevice] orientation];
+}
+
+IOSSystemDataCollector::~IOSSystemDataCollector() {
+  CFNotificationCenterRemoveEveryObserver(CFNotificationCenterGetLocalCenter(),
+                                          this);
+}
+
+}  // namespace crashpad
