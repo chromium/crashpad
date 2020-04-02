bb93495aa8af0f93d9de11a3a4448939dfeec9e1
Joshua Peraza <jperaza@chromium.org>
2020-04-02 13:50:02 -0700

    android: initialize signal dispositions
    Bionic installs signal handlers which request crash
    dumps from Android's debuggerd, but there are errors
    in how signals which aren't automatically re-raised
    are handled on Marshmallow (API 23).
    
    Before requesting a dump, Bionic acquires a lock to
    communicate with debuggerd and expecting imminent
    death, never releases it.
    
    While handling the dump request, debuggerd allows
    the dying process to continue before
    ptrace-detaching it. So, when Bionic manually
    re-raises a signal, it is intercepted by debuggerd
    and the dying process is allowed to live.
    
    Bionic restores SIG_DFL for the signal it's just
    handled, but if a different crash signal is later
    recieved, Bionic attempts to reacquire the lock to
    communicate with debuggerd and blocks forever.
    
    Disable Bionic's signal handlers for these signals
    on Marshmallow.
    
    Bug: chromium:1050178
    Change-Id: Ia1fc5a24161a95931684d092ba8fee2f0dfbbdbb

---
+++ util/linux/initial_signal_dispositions.cc
@@ -0,0 +1,70 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/linux/initial_signal_dispositions.h"
+
+#include <signal.h>
+
+#include "base/logging.h"
+#include "build/build_config.h"
+
+#if defined(OS_ANDROID)
+#include <android/api-level.h>
+#endif
+
+namespace crashpad {
+namespace {
+bool LoggingSignal(int signum, sighandler_t handler) {
+  sighandler_t previous = signal(signum, handler);
+  PLOG_IF(ERROR, previous == SIG_ERR) << "signal";
+  return previous != SIG_ERR;
+}
+}  // namespace
+
+bool InitializeSignalDispositions() {
+  bool success = LoggingSignal(SIGPIPE, SIG_IGN);
+
+#if defined(OS_ANDROID)
+  // Bionic installs signal handlers which request crash dumps from Android's
+  // debuggerd, but there are errors in how signals which aren't automatically
+  // re-raised are handled on Marshmallow (API 23).
+  //
+  // Before requesting a dump, Bionic acquires a lock to communicate with
+  // debuggerd and expecting imminent death, never releases it.
+  //
+  // While handling the dump request, debuggerd allows the dying process to
+  // continue before ptrace-detaching it. So, when Bionic manually re-raises a
+  // signal, it is intercepted by debuggerd and the dying process is allowed to
+  // live.
+  //
+  // Bionic restores SIG_DFL for the signal it's just handled, but if a
+  // different crash signal is later recieved, Bionic attempts to reacquire the
+  // lock to communicate with debuggerd and blocks forever.
+  //
+  // Disable Bionic's signal handlers for these signals on Marshmallow.
+  const int api_level = android_get_device_api_level();
+  if (api_level < 0 || api_level == 23) {
+    success = success && LoggingSignal(SIGABRT, SIG_DFL);
+    success = success && LoggingSignal(SIGFPE, SIG_DFL);
+#if defined(SIGSTKFLT)
+    success = success && LoggingSignal(SIGSTKFLT, SIG_DFL);
+#endif
+    success = success && LoggingSignal(SIGTRAP, SIG_DFL);
+  }
+#endif  // OS_ANDROID
+
+  return success;
+}
+
+}  // namespace crashpad
---
+++ util/linux/initial_signal_dispositions.h
@@ -0,0 +1,27 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_UTIL_LINUX_INITIAL_SIGNAL_DISPOSITIONS_H
+#define CRASHPAD_UTIL_LINUX_INITIAL_SIGNAL_DISPOSITIONS_H
+
+namespace crashpad {
+
+//! \brief Establishes signal dispositions for a process based on the platform.
+//!
+//! \return `true` on success. Otherwise `false` with a message logged.
+bool InitializeSignalDispositions();
+
+}  // namespace crashpad
+
+#endif  // CRASHPAD_UTIL_LINUX_INITIAL_SIGNAL_DISPOSITIONS_H
