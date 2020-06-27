5876acecf20c2a908025fa5bc4c26702727f1ccb
Mark Mentovai <mark@chromium.org>
2020-06-27 00:40:56 -0400

    Use OS_WIN, not OS_WINDOWS; provide crashpad_types traits for Fuchsia
    The incorrect macro was used in f4b906c79c02 (at my request when I was
    reviewing from a phone).
    
    The Windows and Fuchsia builds are broken, so…
    
    TBR: epastor@google.com
    Change-Id: I8aebbbc24db261f3c3377210bb1c477132964828

--- snapshot/crashpad_types/crashpad_info_reader.cc
+++ snapshot/crashpad_types/crashpad_info_reader.cc
@@ -20,7 +20,7 @@
 #include "client/crashpad_info.h"
 #include "util/misc/as_underlying_type.h"
 
-#if defined(OS_WINDOWS)
+#if defined(OS_WIN)
 #include "util/win/traits.h"
 #elif defined(OS_LINUX) || defined(OS_ANDROID)
 #include "util/linux/traits.h"
