34419d9acaba67f9a64b5a1c8fa3cb466bda323d
Adam Kallai <kadam@inf.u-szeged.hu>
2020-07-02 10:34:02 +0200

    fix: include missing header file
    Include the header from base/ where NOTREACHED is defined.
    This fixes a build failure on Windows on Arm.
    
    The patch was originally created by Richard Townsend <richard.townsend@arm.com>.
    
    Bug: 1100363
    Change-Id: Ic97f7c217f22e51f1d4c9bd87ae434e263975c6d

--- snapshot/win/system_snapshot_win.cc
+++ snapshot/win/system_snapshot_win.cc
@@ -24,6 +24,7 @@
 #include <vector>
 
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
