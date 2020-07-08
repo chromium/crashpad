70d0e209c31823578f8c521e99de98cb902b20a4
Mark Mentovai <mark@chromium.org>
2020-07-07 23:43:46 -0400

    mac: Crashpad for macOS on arm64, phase 2: build tests
    This gets all tests building. They don’t all pass, and there aren’t any
    guarantees that anything else works yet, either.
    
    This is mostly a lot of CPU context shuffling.
    
    Bug: crashpad:345
    Change-Id: I684017a5816f44917392964d7fb6d08083770b38

--- util/mach/exc_server_variants_test.cc
+++ util/mach/exc_server_variants_test.cc
@@ -1143,43 +1143,47 @@ TEST(ExcServerVariants, ThreadStates) {
     mach_msg_type_number_t count;
   } test_data[] = {
 #if defined(ARCH_CPU_X86_FAMILY)
-      // For the x86 family, exception handlers can only properly receive the
-      // thread, float, and exception state flavors. There’s a bug in the kernel
-      // that causes it to call thread_getstatus() (a wrapper for the more
-      // familiar thread_get_state()) with an incorrect state buffer size
-      // parameter when delivering an exception. 10.9.4
-      // xnu-2422.110.17/osfmk/kern/exception.c exception_deliver() uses the
-      // _MachineStateCount[] array indexed by the flavor number to obtain the
-      // buffer size. 10.9.4 xnu-2422.110.17/osfmk/i386/pcb.c contains the
-      // definition of this array for the x86 family. The slots corresponding to
-      // thread, float, and exception state flavors in both native-width (32-
-      // and 64-bit) and universal are correct, but the remaining elements in
-      // the array are not. This includes elements that would correspond to
-      // debug and AVX state flavors, so these cannot be tested here.
-      //
-      // When machine_thread_get_state() (the machine-specific implementation of
-      // thread_get_state()) encounters an undersized buffer as reported by the
-      // buffer size parameter, it returns KERN_INVALID_ARGUMENT, which causes
-      // exception_deliver() to not actually deliver the exception and instead
-      // return that error code to exception_triage() as well.
-      //
-      // This bug is filed as radar 18312067.
-      //
-      // Additionaly, the AVX state flavors are also not tested because they’re
-      // not available on all CPUs and OS versions.
+    // For the x86 family, exception handlers can only properly receive the
+    // thread, float, and exception state flavors. There’s a bug in the kernel
+    // that causes it to call thread_getstatus() (a wrapper for the more
+    // familiar thread_get_state()) with an incorrect state buffer size
+    // parameter when delivering an exception. 10.9.4
+    // xnu-2422.110.17/osfmk/kern/exception.c exception_deliver() uses the
+    // _MachineStateCount[] array indexed by the flavor number to obtain the
+    // buffer size. 10.9.4 xnu-2422.110.17/osfmk/i386/pcb.c contains the
+    // definition of this array for the x86 family. The slots corresponding to
+    // thread, float, and exception state flavors in both native-width (32- and
+    // 64-bit) and universal are correct, but the remaining elements in the
+    // array are not. This includes elements that would correspond to debug and
+    // AVX state flavors, so these cannot be tested here.
+    //
+    // When machine_thread_get_state() (the machine-specific implementation of
+    // thread_get_state()) encounters an undersized buffer as reported by the
+    // buffer size parameter, it returns KERN_INVALID_ARGUMENT, which causes
+    // exception_deliver() to not actually deliver the exception and instead
+    // return that error code to exception_triage() as well.
+    //
+    // This bug is filed as radar 18312067.
+    //
+    // Additionaly, the AVX state flavors are also not tested because they’re
+    // not available on all CPUs and OS versions.
+    {x86_THREAD_STATE, x86_THREAD_STATE_COUNT},
+    {x86_FLOAT_STATE, x86_FLOAT_STATE_COUNT},
+    {x86_EXCEPTION_STATE, x86_EXCEPTION_STATE_COUNT},
 #if defined(ARCH_CPU_X86)
-      {x86_THREAD_STATE32, x86_THREAD_STATE32_COUNT},
-      {x86_FLOAT_STATE32, x86_FLOAT_STATE32_COUNT},
-      {x86_EXCEPTION_STATE32, x86_EXCEPTION_STATE32_COUNT},
+    {x86_THREAD_STATE32, x86_THREAD_STATE32_COUNT},
+    {x86_FLOAT_STATE32, x86_FLOAT_STATE32_COUNT},
+    {x86_EXCEPTION_STATE32, x86_EXCEPTION_STATE32_COUNT},
+#elif defined(ARCH_CPU_X86_64)
+    {x86_THREAD_STATE64, x86_THREAD_STATE64_COUNT},
+    {x86_FLOAT_STATE64, x86_FLOAT_STATE64_COUNT},
+    {x86_EXCEPTION_STATE64, x86_EXCEPTION_STATE64_COUNT},
 #endif
-#if defined(ARCH_CPU_X86_64)
-      {x86_THREAD_STATE64, x86_THREAD_STATE64_COUNT},
-      {x86_FLOAT_STATE64, x86_FLOAT_STATE64_COUNT},
-      {x86_EXCEPTION_STATE64, x86_EXCEPTION_STATE64_COUNT},
-#endif
-      {x86_THREAD_STATE, x86_THREAD_STATE_COUNT},
-      {x86_FLOAT_STATE, x86_FLOAT_STATE_COUNT},
-      {x86_EXCEPTION_STATE, x86_EXCEPTION_STATE_COUNT},
+#elif defined(ARCH_CPU_ARM64)
+    {ARM_UNIFIED_THREAD_STATE, ARM_UNIFIED_THREAD_STATE_COUNT},
+    {ARM_THREAD_STATE64, ARM_THREAD_STATE64_COUNT},
+    {ARM_NEON_STATE64, ARM_NEON_STATE64_COUNT},
+    {ARM_EXCEPTION_STATE64, ARM_EXCEPTION_STATE64_COUNT},
 #else
 #error Port this test to your CPU architecture.
 #endif
