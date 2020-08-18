7d640440ba91e6ddde6686c967cdfdcceaaeb88b
Mark Mentovai <mark@chromium.org>
2020-08-18 16:44:04 -0400

    NOT FOR CHECKIN: Test build with -Wvla
    Change-Id: I721f1dccc7e1188631a57f435c5c097ff83853b8

--- snapshot/sanitized/process_snapshot_sanitized_test.cc
+++ snapshot/sanitized/process_snapshot_sanitized_test.cc
@@ -16,6 +16,7 @@
 
 #include "base/macros.h"
 #include "base/notreached.h"
+#include "base/stl_util.h"
 #include "build/build_config.h"
 #include "gtest/gtest.h"
 #include "test/multiprocess_exec.h"
@@ -98,7 +99,7 @@ void ChildTestFunction() {
   static StringAnnotation<32> non_allowed_annotation(kNonAllowedAnnotationName);
   non_allowed_annotation.Set(kNonAllowedAnnotationValue);
 
-  char string_data[strlen(kSensitiveStackData) + 1];
+  char string_data[base::size(kSensitiveStackData)];
   strcpy(string_data, kSensitiveStackData);
 
   void (*code_pointer)(void) = ChildTestFunction;
