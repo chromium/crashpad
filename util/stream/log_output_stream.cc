e0a1889887975e4e0f0cc4f6ce29a24da6325c04
Joshua Peraza <jperaza@chromium.org>
2020-09-30 10:59:19 -0700

    fix flake in LogOutputStreamTest
    LogOutputStreamTest.{WriteAbort,FlushAbort} are flaky because the logcat
    is sometimes overloaded earlier than expected causing FlushAbort to fail
    during Write() or either test to fail to write the abort message.
    
    This change updates LogOutputStream to detect logcat overloads (EAGAIN)
    and make one attempt at writing the abort message, even if the output
    cap hasn't been reached.
    
    I've removed FlushAbort because we'd need to mock the logcat failure to
    reliably trigger a failure in Flush(). The abort message writing code is
    still tested by WriteAbort.
    
    I've removed VerifyGuards because LogOutputStream no longer writes
    guards if Write() has never been called and the guards are tested in
    other tests.
    
    Change-Id: Icad83524aaf573c3e082469f1de095b6ca2c4839

--- util/stream/log_output_stream.cc
+++ util/stream/log_output_stream.cc
@@ -19,6 +19,7 @@
 #include <algorithm>
 
 #include "base/check.h"
+#include "base/check_op.h"
 #include "base/logging.h"
 
 #if defined(OS_ANDROID)
@@ -50,17 +51,19 @@ LogOutputStream::~LogOutputStream() {
 
 bool LogOutputStream::Write(const uint8_t* data, size_t size) {
   DCHECK(!flushed_);
+
+  static constexpr char kBeginMessage[] = "-----BEGIN CRASHPAD MINIDUMP-----";
+  if (output_count_ == 0 && WriteToLog(kBeginMessage) < 0) {
+    return false;
+  }
+
   flush_needed_ = true;
   while (size > 0) {
     size_t m = std::min(kLineBufferSize - buffer_.size(), size);
     buffer_.append(reinterpret_cast<const char*>(data), m);
     data += m;
     size -= m;
-    if (buffer_.size() == kLineBufferSize && !WriteBuffer()) {
-      flush_needed_ = false;
-      LOG(ERROR) << "Write: exceeds cap.";
-      if (output_stream_for_testing_)
-        output_stream_for_testing_->Flush();
+    if (!WriteBuffer()) {
       return false;
     }
   }
@@ -68,53 +71,58 @@ bool LogOutputStream::Write(const uint8_t* data, size_t size) {
 }
 
 bool LogOutputStream::WriteBuffer() {
-  if (output_count_ == 0) {
-    if (!WriteToLog("-----BEGIN CRASHPAD MINIDUMP-----"))
-      return false;
-  }
-
   if (buffer_.empty())
     return true;
 
+  static constexpr char kAbortMessage[] = "-----ABORT CRASHPAD MINIDUMP-----";
+
   output_count_ += buffer_.size();
   if (output_count_ > kOutputCap) {
-    WriteToLog("-----ABORT CRASHPAD MINIDUMP-----");
+    WriteToLog(kAbortMessage);
     return false;
   }
 
-  bool result = WriteToLog(buffer_.c_str());
+  int result = WriteToLog(buffer_.c_str());
+  if (result < 0) {
+    if (result == -EAGAIN) {
+      WriteToLog(kAbortMessage);
+    }
+    flush_needed_ = false;
+    return false;
+  }
+
   buffer_.clear();
-  return result;
+  return true;
 }
 
-bool LogOutputStream::WriteToLog(const char* buf) {
+int LogOutputStream::WriteToLog(const char* buf) {
+  int ret = 0;
 #if defined(OS_ANDROID)
-  int ret =
+  ret =
       __android_log_buf_write(LOG_ID_CRASH, ANDROID_LOG_FATAL, "crashpad", buf);
   if (ret < 0) {
     errno = -ret;
     PLOG(ERROR) << "__android_log_buf_write";
-    return false;
   }
 #endif
   // For testing.
   if (output_stream_for_testing_) {
-    return output_stream_for_testing_->Write(
-        reinterpret_cast<const uint8_t*>(buf), strlen(buf));
+    output_stream_for_testing_->Write(reinterpret_cast<const uint8_t*>(buf),
+                                      strlen(buf));
   }
-  return true;
+  return ret;
 }
 
 bool LogOutputStream::Flush() {
-  flush_needed_ = false;
-  flushed_ = true;
-
   bool result = true;
-  if (WriteBuffer()) {
-    result = WriteToLog("-----END CRASHPAD MINIDUMP-----");
-  } else {
-    LOG(ERROR) << "Flush: exceeds cap.";
-    result = false;
+  if (flush_needed_) {
+    flush_needed_ = false;
+    flushed_ = true;
+
+    static constexpr char kEndMessage[] = "-----END CRASHPAD MINIDUMP-----";
+    if (!WriteBuffer() || WriteToLog(kEndMessage) < 0) {
+      result = false;
+    }
   }
 
   // Since output_stream_for_testing_'s Write() method has been called, its
