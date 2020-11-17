4441efd80faa11d959528294475f362f010c178d
Justin Cohen <justincohen@google.com>
2020-11-17 14:56:01 -0500

    ios: Serialize to disk and de-serialize intermediate minidump format.
    DO NOT LAND.  NOT FOR REVIEW.  YOUR EYES ONLY.
    
    Change-Id: I09a6b692ab643eb047561191e352a5be708a3bb1

--- snapshot/ios/exception_snapshot_ios.h
+++ snapshot/ios/exception_snapshot_ios.h
@@ -24,6 +24,7 @@
 #include "build/build_config.h"
 #include "snapshot/cpu_context.h"
 #include "snapshot/exception_snapshot.h"
+#include "util/ios/pack_ios_state.h"
 #include "util/mach/mach_extensions.h"
 #include "util/misc/initialization_state_dcheck.h"
 
@@ -42,21 +43,14 @@ class ExceptionSnapshotIOS final : public ExceptionSnapshot {
   //!
   //! \return `true` if the snapshot could be created, `false` otherwise with
   //!     an appropriate message logged.
-  void InitializeFromSignal(const siginfo_t* siginfo,
-                            const ucontext_t* context);
+  bool InitializeFromSignal(const PackedMap& exception_data);
 
   //! \brief Initialize the object from a Mach exception for the current task.
   //!
   //! \return `true` if the snapshot could be created, `false` otherwise with
   //!     an appropriate message logged.
-  void InitializeFromMachException(exception_behavior_t behavior,
-                                   thread_t exception_thread,
-                                   exception_type_t exception,
-                                   const mach_exception_data_type_t* code,
-                                   mach_msg_type_number_t code_count,
-                                   thread_state_flavor_t flavor,
-                                   ConstThreadState state,
-                                   mach_msg_type_number_t state_count);
+  bool InitializeFromMachException(const PackedMap& exception_data,
+                                   const PackedList& thread_list);
   // ExceptionSnapshot:
 
   const CPUContext* Context() const override;
