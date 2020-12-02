a0435e06b05581a7e49f19381b4d5a35d467ce79
Joshua Peraza <jperaza@chromium.org>
2020-12-02 10:41:23 -0800

    refactor test modules
    Change-Id: Ie4f7932d65710fc3e20b6e2488e497c5aab27cdd

---
+++ snapshot/linux/test_modules.h
@@ -0,0 +1,31 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_SNAPSHOT_LINUX_TEST_MODULES_H_
+#define CRASHPAD_SNAPSHOT_LINUX_TEST_MODULES_H_
+
+#include <string>
+
+#include "test/scoped_module_handle.h"
+
+namespace crashpad {
+namespace test {
+
+ScopedModuleHandle LoadTestModule(const std::string& module_name,
+                                  const std::string& module_soname);
+
+}
+}  // namespace crashpad
+
+#endif  // CRASHPAD_SNAPSHOT_LINUX_DEBUG_RENDEZVOUS_H_
