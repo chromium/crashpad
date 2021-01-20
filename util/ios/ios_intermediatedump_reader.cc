e922a765925b0127bf5c1259104d877d9c02d04d
Justin Cohen <justincohen@google.com>
2021-01-19 22:23:35 -0500

    ios: Serialize to disk and de-serialize intermediate minidump format.
    DO NOT LAND.  NOT FOR REVIEW.  YOUR EYES ONLY.
    
    Change-Id: I09a6b692ab643eb047561191e352a5be708a3bb1

---
+++ util/ios/ios_intermediatedump_reader.cc
@@ -0,0 +1,50 @@
+// Copyright 2020 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediatedump_reader.h"
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "base/logging.h"
+
+namespace crashpad {
+namespace internal {
+
+bool IOSIntermediatedumpReader::Initialize(const base::FilePath& dump_path) {
+  reader_ = std::make_unique<crashpad::FileReader>();
+  if (!reader_->Open(dump_path)) {
+    return false;
+  }
+
+  // Don't initialize empty files.
+  if (reader_->Seek(0, SEEK_END) == 0)
+    return false;
+
+  return true;
+}
+
+bool IOSIntermediatedumpReader::Parse() {
+  if (!IOSIntermediatedumpMap::Parse(reader_.get(), minidump_)) {
+    DLOG(ERROR) << "Intermediate dump parsing failed";
+    return false;
+  }
+#ifndef NDEBUG
+  minidump_.DebugDump();
+#endif
+  return true;
+}
+
+}  // namespace internal
+}  // namespace crashpad
