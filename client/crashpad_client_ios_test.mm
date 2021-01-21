468dc34d6ebe7fb0116569e95a08dc3fe474a869
Justin Cohen <justincohen@google.com>
2021-01-20 23:20:10 -0500

    ios: Serialize to disk and de-serialize intermediate minidump format.
    DO NOT LAND.  NOT FOR REVIEW.  YOUR EYES ONLY.
    
    Change-Id: I09a6b692ab643eb047561191e352a5be708a3bb1

--- client/crashpad_client_ios_test.mm
+++ client/crashpad_client_ios_test.mm
@@ -18,7 +18,9 @@
 
 #include <vector>
 
+#include "client/annotation.h"
 #include "gtest/gtest.h"
+#include "test/scoped_temp_dir.h"
 #include "testing/platform_test.h"
 
 namespace crashpad {
@@ -29,8 +31,10 @@ using CrashpadIOSClient = PlatformTest;
 
 TEST_F(CrashpadIOSClient, DumpWithoutCrash) {
   CrashpadClient client;
-  client.StartCrashpadInProcessHandler();
-
+  ScopedTempDir database_dir;
+  base::FilePath metrics_dir;
+  client.StartCrashpadInProcessHandler(
+      base::FilePath(database_dir.path()), metrics_dir, "", {});
   NativeCPUContext context;
   CaptureContext(&context);
   client.DumpWithoutCrash(&context);
@@ -42,7 +46,10 @@ TEST_F(CrashpadIOSClient, DumpWithoutCrash) {
 // during development only.
 TEST_F(CrashpadIOSClient, DISABLED_ThrowNSException) {
   CrashpadClient client;
-  client.StartCrashpadInProcessHandler();
+  ScopedTempDir database_dir;
+  base::FilePath metrics_dir;
+  client.StartCrashpadInProcessHandler(
+      base::FilePath(database_dir.path()), metrics_dir, "", {});
   [NSException raise:@"GoogleTestNSException" format:@"ThrowException"];
 }
 
@@ -52,7 +59,10 @@ TEST_F(CrashpadIOSClient, DISABLED_ThrowNSException) {
 // during development only.
 TEST_F(CrashpadIOSClient, DISABLED_ThrowException) {
   CrashpadClient client;
-  client.StartCrashpadInProcessHandler();
+  ScopedTempDir database_dir;
+  base::FilePath metrics_dir;
+  client.StartCrashpadInProcessHandler(
+      base::FilePath(database_dir.path()), metrics_dir, "", {});
   std::vector<int> empty_vector;
   empty_vector.at(42);
 }
--- test/ios/host/cptest_shared_object.h
+++ test/ios/host/cptest_shared_object.h
@@ -21,6 +21,13 @@
 // Returns the string "crashpad" for testing EDO.
 - (NSString*)testEDO;
 
+// Returns last exception as int.
+- (int)lastException;
+
+- (int)pendingReportCount;
+
+- (void)clearReports;
+
 // Triggers an EXC_BAD_ACCESS exception and crash.
 - (void)crashBadAccess;
 
