29b2339ac02dce9a883f7554d80e3a1743f0b35a
Joshua Peraza <jperaza@chromium.org>
2021-02-03 11:20:22 -0800

    Resurrect http_transport_libcurl
    Bug: chromium:1077284
    Change-Id: I19eed6072dd7854b81f7fa8c25470b18b3bc1ffd

--- build/install_linux_sysroot.py
+++ build/install_linux_sysroot.py
@@ -29,8 +29,8 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 # https://cs.chromium.org/chromium/src/build/linux/sysroot_scripts/sysroots.json
 SERVER = 'https://commondatastorage.googleapis.com'
 PATH = 'chrome-linux-sysroot/toolchain'
-REVISION = '3c248ba4290a5ad07085b7af07e6785bf1ae5b66'
-FILENAME = 'debian_stretch_amd64_sysroot.tar.xz'
+REVISION = '22f2db7711f7426a364617bb6d78686cce09a8f9'
+FILENAME = 'debian_sid_amd64_sysroot.tar.xz'
 
 
 def main():
