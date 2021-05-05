d3a53c8ede30fc508f5a1e8b59ff878345aef923
Justin Cohen <justincohen@google.com>
2021-05-04 23:59:55 -0400

    ios: Add support for intermediate dump reader and writer.
    Bug: crashpad: 31
    Change-Id: I43a19204935303afd753c8c7090c54099634ccd6

---
+++ util/ios/ios_intermediatedump_data.cc
@@ -0,0 +1,47 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediatedump_data.h"
+
+namespace crashpad {
+namespace internal {
+
+IOSIntermediatedumpObjectType IOSIntermediatedumpData::type() const {
+  return DATA;
+}
+
+const IOSIntermediatedumpData& IOSIntermediatedumpData::AsData() const {
+  return *this;
+}
+void IOSIntermediatedumpData::GetString(std::string* string) const {
+  *string = std::string(reinterpret_cast<const char*>(data_.get()), length_);
+}
+
+void IOSIntermediatedumpData::GetInt(int* val) const {
+  *val = static_cast<int>(*data_.get());
+}
+void IOSIntermediatedumpData::GetBool(bool* val) const {
+  *val = static_cast<bool>(data_.get());
+}
+
+const uint8_t* IOSIntermediatedumpData::data() const {
+  return data_.get();
+}
+
+uint64_t IOSIntermediatedumpData::length() const {
+  return length_;
+}
+
+}  // namespace internal
+}  // namespace crashpad
