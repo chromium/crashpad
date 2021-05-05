1ad98a940e81908b952d4fef90e67ca26afe74c7
Justin Cohen <justincohen@google.com>
2021-05-04 22:08:12 -0400

    ios: Add support for intermediate dump reader and writer.
    Bug: crashpad: 31
    Change-Id: I43a19204935303afd753c8c7090c54099634ccd6

---
+++ util/ios/ios_intermediatedump_writer_test.cc
@@ -0,0 +1,80 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediatedump_writer.h"
+
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "base/files/scoped_file.h"
+#include "base/posix/eintr_wrapper.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "test/errors.h"
+#include "test/scoped_temp_dir.h"
+#include "util/ios/ios_intermediatedump_data.h"
+
+namespace crashpad {
+namespace test {
+namespace {
+
+using internal::IntermediateDumpKey;
+
+class IOSIntermediatedumpWriterTest : public testing::Test {
+ protected:
+  // testing::Test:
+
+  void SetUp() override {
+    path_ = temp_dir_.path().Append("dump_file");
+
+    fd_ = base::ScopedFD(HANDLE_EINTR(
+        ::open(path_.value().c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644)));
+    EXPECT_GE(fd_.get(), 0) << ErrnoMessage("open");
+
+    writer_ = std::make_unique<internal::IOSIntermediatedumpWriter>();
+    EXPECT_TRUE(writer_->Open(path_));
+  }
+
+  void TearDown() override {
+    fd_.reset();
+    writer_.reset();
+    EXPECT_EQ(unlink(path_.value().c_str()), 0) << ErrnoMessage("unlink");
+  }
+
+  int fd() { return fd_.get(); }
+
+  void closeFD() {}
+
+  const base::FilePath& path() const { return path_; }
+
+  std::unique_ptr<internal::IOSIntermediatedumpWriter> writer_;
+
+ private:
+  base::ScopedFD fd_;
+  ScopedTempDir temp_dir_;
+  base::FilePath path_;
+};
+
+TEST_F(IOSIntermediatedumpWriterTest, WriteMap) {
+  {
+    internal::IOSIntermediatedumpWriter::ScopedMap map(
+        writer_.get(), IntermediateDumpKey::kName);
+    writer_->AddProperty(IntermediateDumpKey::kName, "a_name", 6);
+  }
+  EXPECT_TRUE(writer_->Close());
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
