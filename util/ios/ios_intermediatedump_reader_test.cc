2a6582337c549acd8b2577010eb77680227d6ae3
Justin Cohen <justincohen@google.com>
2021-05-06 23:39:03 -0400

    ios: Add support for intermediate dump reader and writer.
    Bug: crashpad: 31
    Change-Id: I43a19204935303afd753c8c7090c54099634ccd6

---
+++ util/ios/ios_intermediatedump_reader_test.cc
@@ -0,0 +1,233 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediatedump_reader.h"
+
+#include <fcntl.h>
+
+#include "base/posix/eintr_wrapper.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "test/errors.h"
+#include "test/scoped_temp_dir.h"
+#include "util/ios/ios_intermediatedump_data.h"
+#include "util/ios/ios_intermediatedump_list.h"
+#include "util/ios/ios_intermediatedump_writer.h"
+
+namespace crashpad {
+namespace test {
+namespace {
+
+using internal::IntermediateDumpKey;
+using internal::IOSIntermediatedumpWriter;
+
+class IOSIntermediatedumpReaderTest : public testing::Test {
+ protected:
+  // testing::Test:
+
+  void SetUp() override {
+    path_ = temp_dir_.path().Append("dump_file");
+    fd_ = base::ScopedFD(HANDLE_EINTR(
+        ::open(path_.value().c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644)));
+    ASSERT_GE(fd_.get(), 0) << ErrnoMessage("open");
+
+    writer_ = std::make_unique<IOSIntermediatedumpWriter>();
+    ASSERT_TRUE(writer_->Open(path_));
+  }
+
+  void TearDown() override {
+    fd_.reset();
+    writer_.reset();
+    ASSERT_EQ(unlink(path_.value().c_str()), 0) << ErrnoMessage("unlink");
+  }
+
+  int fd() { return fd_.get(); }
+
+  const base::FilePath& path() const { return path_; }
+
+  std::unique_ptr<IOSIntermediatedumpWriter> writer_;
+
+ private:
+  base::ScopedFD fd_;
+  ScopedTempDir temp_dir_;
+  base::FilePath path_;
+};
+
+TEST_F(IOSIntermediatedumpReaderTest, ReadNoFile) {
+  internal::IOSIntermediatedumpReader reader;
+  EXPECT_FALSE(reader.Initialize(base::FilePath()));
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, ReadEmptyFile) {
+  internal::IOSIntermediatedumpReader reader;
+  EXPECT_FALSE(reader.Initialize(path()));
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, ReadHelloWorld) {
+  std::string hello_world("hello world.");
+  EXPECT_TRUE(
+      LoggingWriteFile(fd(), hello_world.c_str(), hello_world.length()));
+  internal::IOSIntermediatedumpReader reader;
+  EXPECT_TRUE(reader.Initialize(path()));
+  EXPECT_FALSE(reader.Parse());
+
+  uint8_t version = 0;
+  const auto& root_map = reader.RootMap();
+  const auto& version_data = root_map[IntermediateDumpKey::kVersion].AsData();
+  EXPECT_FALSE(version_data.GetValue<uint8_t>(&version));
+  EXPECT_EQ(version, 0);
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, WriteBadPropertyDataLength) {
+  internal::IOSIntermediatedumpReader reader;
+  uint8_t t = internal::PROPERTY;
+  EXPECT_TRUE(LoggingWriteFile(fd(), &t, sizeof(t)));
+  IntermediateDumpKey key = IntermediateDumpKey::kVersion;
+  EXPECT_TRUE(LoggingWriteFile(fd(), &key, sizeof(key)));
+  uint8_t value = 1;
+  size_t value_length = 999999;
+  EXPECT_TRUE(LoggingWriteFile(fd(), &value_length, sizeof(size_t)));
+  EXPECT_TRUE(LoggingWriteFile(fd(), &value, sizeof(value)));
+  EXPECT_TRUE(reader.Initialize(path()));
+  EXPECT_FALSE(reader.Parse());
+
+  uint8_t version = 0;
+  const auto& root_map = reader.RootMap();
+  const auto& version_data = root_map[IntermediateDumpKey::kVersion].AsData();
+  EXPECT_FALSE(version_data.GetValue<uint8_t>(&version));
+  EXPECT_EQ(version, 0);
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, InvalidArrayInArray) {
+  internal::IOSIntermediatedumpReader reader;
+
+  IOSIntermediatedumpWriter::ScopedArray threadArray(
+      writer_.get(), IntermediateDumpKey::kThreads);
+  IOSIntermediatedumpWriter::ScopedArray innerThreadArray(
+      writer_.get(), IntermediateDumpKey::kModules);
+
+  // Write version last, so it's not parsed.
+  uint8_t version = 1;
+  writer_->AddProperty(IntermediateDumpKey::kVersion, &version);
+  writer_->Close();
+  EXPECT_TRUE(reader.Initialize(path()));
+  EXPECT_FALSE(reader.Parse());
+
+  version = 0;
+  const auto& root_map = reader.RootMap();
+  const auto& version_data = root_map[IntermediateDumpKey::kVersion].AsData();
+  EXPECT_FALSE(version_data.GetValue<uint8_t>(&version));
+  EXPECT_EQ(version, 0);
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, InvalidPropertyInArray) {
+  internal::IOSIntermediatedumpReader reader;
+
+  IOSIntermediatedumpWriter::ScopedArray threadArray(
+      writer_.get(), IntermediateDumpKey::kThreads);
+
+  // Write version last, so it's not parsed.
+  uint8_t version = 1;
+  writer_->AddProperty(IntermediateDumpKey::kVersion, &version);
+  EXPECT_TRUE(reader.Initialize(path()));
+  EXPECT_FALSE(reader.Parse());
+
+  version = 0;
+  const auto& root_map = reader.RootMap();
+  const auto& version_data = root_map[IntermediateDumpKey::kVersion].AsData();
+  EXPECT_FALSE(version_data.GetValue<uint8_t>(&version));
+  EXPECT_EQ(version, 0);
+}
+
+TEST_F(IOSIntermediatedumpReaderTest, ReadValidData) {
+  internal::IOSIntermediatedumpReader reader;
+  uint8_t version = 1;
+  EXPECT_TRUE(writer_->AddProperty(IntermediateDumpKey::kVersion, &version));
+
+  {
+    IOSIntermediatedumpWriter::ScopedArray threadArray(
+        writer_.get(), IntermediateDumpKey::kThreadContextMemoryRegions);
+    IOSIntermediatedumpWriter::ScopedMap threadMap(writer_.get());
+
+    std::string random_data("random_data");
+    EXPECT_TRUE(writer_->AddProperty(
+        IntermediateDumpKey::kThreadContextMemoryRegionAddress, &version));
+    EXPECT_TRUE(writer_->AddProperty(
+        IntermediateDumpKey::kThreadContextMemoryRegionData,
+        random_data.c_str(),
+        random_data.length()));
+  }
+
+  {
+    IOSIntermediatedumpWriter::ScopedMap map(writer_.get(),
+                                             IntermediateDumpKey::kProcessInfo);
+    pid_t p_pid = getpid();
+    EXPECT_TRUE(writer_->AddProperty(IntermediateDumpKey::kPID, &p_pid));
+  }
+
+  EXPECT_TRUE(writer_->Close());
+  EXPECT_TRUE(reader.Initialize(path()));
+  EXPECT_TRUE(reader.Parse());
+
+  auto& root_map = reader.RootMap();
+  version = 0;
+  const auto& version_data = root_map[IntermediateDumpKey::kVersion].AsData();
+  EXPECT_TRUE(version_data.GetValue<uint8_t>(&version));
+  EXPECT_EQ(version, 1);
+
+  const auto& process_info =
+      root_map[IntermediateDumpKey::kProcessInfo].AsMap();
+  const auto& pid_data = process_info[IntermediateDumpKey::kPID].AsData();
+  pid_t p_pid = 0;
+  EXPECT_TRUE(pid_data.GetValue<pid_t>(&p_pid));
+  ASSERT_EQ(p_pid, getpid());
+
+  const auto& thread_context_memory_regions =
+      root_map[IntermediateDumpKey::kThreadContextMemoryRegions].AsList();
+  for (auto& region : thread_context_memory_regions) {
+    const auto& region_map = (*region).AsMap();
+    vm_size_t data_size =
+        region_map[IntermediateDumpKey::kThreadContextMemoryRegionData]
+            .AsData()
+            .length();
+    EXPECT_EQ(data_size, 11UL);
+
+    // Load as string.
+    std::string name;
+    region_map[IntermediateDumpKey::kThreadContextMemoryRegionData].GetString(
+        &name);
+    EXPECT_EQ(name, "random_data");
+
+    // Load as data.
+    vm_address_t data =
+        (vm_address_t)
+            region_map[IntermediateDumpKey::kThreadContextMemoryRegionData]
+                .AsData()
+                .bytes();
+    EXPECT_EQ(std::string(reinterpret_cast<const char*>(data), data_size),
+              "random_data");
+  }
+
+  // It's valid to check for data that doesn't exist.
+  const auto& system_info = root_map[IntermediateDumpKey::kSystemInfo].AsMap();
+  pid_t parent_pid = 0;
+  EXPECT_FALSE(
+      system_info[IntermediateDumpKey::kParentPID].AsData().GetValue<pid_t>(
+          &parent_pid));
+  ASSERT_EQ(parent_pid, 0);
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace crashpad
---
+++ util/ios/ios_intermediatedump_writer.h
@@ -0,0 +1,219 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CRASHPAD_UTIL_IOS_IOS_INTERMEDIATEDUMP_WRITER_H_
+#define CRASHPAD_UTIL_IOS_IOS_INTERMEDIATEDUMP_WRITER_H_
+
+#include <unistd.h>
+
+#include "base/files/file_path.h"
+#include "base/logging.h"
+#include "util/mach/mach_extensions.h"
+
+namespace crashpad {
+namespace internal {
+
+//! \brief FILLMEOUT.
+enum CommandType : uint8_t {
+  //! \brief FILLMEOUT.
+  MAP_START = 0x01,
+
+  //! \brief FILLMEOUT.
+  MAP_END = 0x02,
+
+  //! \brief FILLMEOUT.
+  ARRAY_START = 0x03,
+
+  //! \brief FILLMEOUT.
+  ARRAY_END = 0x04,
+
+  //! \brief FILLMEOUT.
+  PROPERTY = 0x05,
+
+  //! \brief FILLMEOUT.
+  DOCUMENT_END = 0x06,
+};
+
+// Declare tokens for intermediate format as enum DumpKey. Use
+// |INTERMEDIATE_DUMP_KEYS|'s so it's easier to log keys in debug for testing.
+// These values are persisted to the intermediate crash dump file. Entries
+// should not be renumbered and numeric values should never be reused.
+// clang-format off
+#define INTERMEDIATE_DUMP_KEYS(TD) \
+  TD(kInvalid, 0) \
+  TD(kVersion, 1) \
+  TD(kMachException, 1000) \
+    TD(kCode, 1001) \
+    TD(kCodeCount, 1002) \
+    TD(kException, 1003) \
+    TD(kFlavor, 1004) \
+    TD(kState, 1005) \
+    TD(kStateCount, 1006) \
+  TD(kSignalException, 2000) \
+    TD(kSignalNumber, 2001) \
+    TD(kSignalCode, 2002) \
+    TD(kSignalAddress, 2003) \
+  TD(kNSException, 2500) \
+  TD(kModules, 3000) \
+    TD(kAddress, 3001) \
+    TD(kFileType, 3002) \
+    TD(kName, 3003) \
+    TD(kSize, 3004) \
+    TD(kDylibCurrentVersion, 3005) \
+    TD(kSourceVersion, 3006) \
+    TD(kTimestamp, 3007) \
+    TD(kUUID, 3008) \
+    TD(kAnnotationObjects, 3009) \
+    TD(kAnnotationsSimpleMap, 3010) \
+    TD(kAnnotationsVector, 3011) \
+    TD(kAnnotationType, 3012) \
+    TD(kAnnotationName, 3013) \
+    TD(kAnnotationValue, 3014) \
+    TD(kAnnotationsCrashInfo, 3015) \
+    TD(kAnnotationsCrashInfoMessage1, 3016) \
+    TD(kAnnotationsCrashInfoMessage2, 3017) \
+    TD(kAnnotationsDyldErrorString, 3018) \
+  TD(kProcessInfo, 4000) \
+    TD(kParentPID, 4001) \
+    TD(kPID, 4002) \
+    TD(kStartTime, 4003) \
+    TD(kSnapshotTime, 4004) \
+    TD(kTaskBasicInfo, 4005) \
+    TD(kTaskThreadTimes, 4006) \
+    TD(kSystemTime, 4007) \
+    TD(kUserTime, 4008) \
+  TD(kSystemInfo, 5000) \
+    TD(kCpuCount, 5001) \
+    TD(kCpuVendor, 5002) \
+    TD(kDaylightName, 5003) \
+    TD(kDaylightOffsetSeconds, 5004) \
+    TD(kHasDaylightSavingTime, 5005) \
+    TD(kIsDaylightSavingTime, 5006) \
+    TD(kMachineDescription, 5007) \
+    TD(kOSVersionBugfix, 5008) \
+    TD(kOSVersionBuild, 5009) \
+    TD(kOSVersionMajor, 5010) \
+    TD(kOSVersionMinor, 5011) \
+    TD(kPageSize, 5012) \
+    TD(kStandardName, 5013) \
+    TD(kStandardOffsetSeconds, 5014) \
+    TD(kVMStat, 5015) \
+    TD(kActive, 5016) \
+    TD(kFree, 5017) \
+    TD(kInactive, 5018) \
+    TD(kWired, 5019) \
+  TD(kThreads, 6000) \
+    TD(kDebugState, 6001) \
+    TD(kFloatState, 6002) \
+    TD(kThreadState, 6003) \
+    TD(kPriority, 6004) \
+    TD(kStackRegionAddress, 6005) \
+    TD(kStackRegionData, 6006) \
+    TD(kSuspendCount, 6007) \
+    TD(kThreadID, 6008) \
+    TD(kThreadDataAddress, 6009) \
+    TD(kThreadUncaughtNSExceptionFrames, 6010) \
+    TD(kThreadContextMemoryRegions, 6011) \
+    TD(kThreadContextMemoryRegionAddress, 6012) \
+    TD(kThreadContextMemoryRegionData, 6013) \
+
+
+//! \brief FILLMEOUT.
+enum class IntermediateDumpKey : uint64_t {
+  #define X(NAME, VALUE) NAME = VALUE,
+  INTERMEDIATE_DUMP_KEYS(X)
+  #undef X
+};
+// clang-format on
+
+//! \brief FILLMEOUT.
+//!
+//! Note: All methods except Open are `RUNS-DURING-CRASH`.
+class IOSIntermediatedumpWriter final {
+ public:
+  IOSIntermediatedumpWriter() = default;
+
+  //! \brief FILLMEOUT.
+  bool Open(const base::FilePath& path);
+
+  //! \brief FILLMEOUT.
+  bool Close();
+
+  //! \brief FILLMEOUT.
+  class ScopedMap {
+   public:
+    explicit ScopedMap(IOSIntermediatedumpWriter* writer,
+                       IntermediateDumpKey key)
+        : writer_(writer) {
+      writer->MapStart(key);
+    }
+    explicit ScopedMap(IOSIntermediatedumpWriter* writer) : writer_(writer) {
+      writer->ArrayMapStart();
+    }
+    ~ScopedMap() { writer_->MapEnd(); }
+
+   private:
+    IOSIntermediatedumpWriter* writer_;
+    DISALLOW_COPY_AND_ASSIGN(ScopedMap);
+  };
+
+  //! \brief FILLMEOUT.
+  class ScopedArray {
+   public:
+    explicit ScopedArray(IOSIntermediatedumpWriter* writer,
+                         IntermediateDumpKey key)
+        : writer_(writer) {
+      writer->ArrayStart(key);
+    }
+    ~ScopedArray() { writer_->ArrayEnd(); }
+
+   private:
+    IOSIntermediatedumpWriter* writer_;
+    DISALLOW_COPY_AND_ASSIGN(ScopedArray);
+  };
+
+  //! \brief FILLMEOUT.
+  template <typename T>
+  bool AddProperty(IntermediateDumpKey key, const T* value, size_t count = 1) {
+    return AddPropertyInternal(
+        key, reinterpret_cast<const char*>(value), count * sizeof(T));
+  }
+
+  //! \brief FILLMEOUT.
+  bool AddPropertyBytes(IntermediateDumpKey key,
+                        const void* value,
+                        size_t value_length) {
+    return AddPropertyInternal(
+        key, reinterpret_cast<const char*>(value), value_length);
+  }
+
+ private:
+  bool AddPropertyInternal(IntermediateDumpKey key,
+                           const char* value,
+                           size_t value_length);
+
+  bool ArrayStart(IntermediateDumpKey key);
+  bool MapStart(IntermediateDumpKey key);
+  bool ArrayMapStart();
+  bool ArrayEnd();
+  bool MapEnd();
+
+  int fd_;
+  DISALLOW_COPY_AND_ASSIGN(IOSIntermediatedumpWriter);
+};
+
+}  // namespace internal
+}  // namespace crashpad
+
+#endif  // CRASHPAD_UTIL_IOS_IOS_INTERMEDIATEDUMP_WRITER_H_
