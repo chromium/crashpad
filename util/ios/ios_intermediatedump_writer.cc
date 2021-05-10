2af030db771e0583aad87ce36be9d2c2fe5a9470
Justin Cohen <justincohen@google.com>
2021-05-10 11:45:12 -0400

    ios: Add support for intermediate dump reader and writer.
    Due to the limitations of in-process handling, an intermediate dump file
    is written during exceptions. The data is streamed to a file using only
    in-process safe methods. The file format is similar to binary JSON,
    supporting keyed properties, maps and arrays.
     - Property [key:int, length:int, value:intarray]
     - StartMap [key:int], followed by repeating Properties until EndMap
     - StartArray [key:int], followed by repeating Maps until EndArray
     - EndMap, EndArray, EndDocument
    
    Similar to JSON, maps can contain other maps, arrays and properties.
    
    Once loaded, the binary file is read into a set of data structures that
    expose the data, maps and arrays.
    
    Bug: crashpad: 31
    Change-Id: I43a19204935303afd753c8c7090c54099634ccd6

---
+++ util/ios/ios_intermediatedump_writer.cc
@@ -0,0 +1,129 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediatedump_writer.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "base/posix/eintr_wrapper.h"
+#include "build/build_config.h"
+#include "util/ios/raw_logging.h"
+#include "util/ios/scoped_vm_read.h"
+
+namespace crashpad {
+namespace internal {
+
+// Similar to LoggingWriteFile but with RAW_LOG.
+bool RawLoggingWriteFile(int fd, const void* buffer, size_t size) {
+  uintptr_t buffer_int = reinterpret_cast<uintptr_t>(buffer);
+  while (size > 0) {
+    ssize_t bytes_written = HANDLE_EINTR(
+        write(fd, reinterpret_cast<const char*>(buffer_int), size));
+    if (bytes_written < 0 || bytes_written == 0) {
+      RAW_LOG_ERROR(bytes_written, "RawLoggingWriteFile");
+      return false;
+    }
+    buffer_int += bytes_written;
+    size -= bytes_written;
+  }
+  return true;
+}
+
+// Similar to LoggingCloseFile but with RAW_LOG.
+bool RawLoggingCloseFile(int fd) {
+  int rv = IGNORE_EINTR(close(fd));
+  if (rv != 0) {
+    RAW_LOG_ERROR(rv, "RawLoggingCloseFile");
+  }
+  return rv == 0;
+}
+
+// Similar to LoggingLockFile but with RAW_LOG.
+bool RawLoggingLockFileExclusiveNonBlocking(int fd) {
+  int rv = HANDLE_EINTR(flock(fd, LOCK_EX | LOCK_NB));
+  if (rv != 0) {
+    RAW_LOG_ERROR(rv, "RawLoggingLockFileExclusiveNonBlocking");
+  }
+  return rv == 0;
+}
+
+bool IOSIntermediatedumpWriter::Open(const base::FilePath& path) {
+  // Set data protection class D (No protection). A file with this type of
+  // protection can be read from or written to at any time.
+  // See:
+  // https://support.apple.com/guide/security/data-protection-classes-secb010e978a/web
+  constexpr int PROTECTION_CLASS_D = 4;
+  fd_ = HANDLE_EINTR(open_dprotected_np(path.value().c_str(),
+                                        O_WRONLY | O_CREAT | O_TRUNC,
+                                        PROTECTION_CLASS_D,
+                                        0 /* dpflags */,
+                                        0644 /* mode */));
+  if (fd_ < 0) {
+    RAW_LOG_ERROR(fd_, "open intermediate dump");
+    RAW_LOG(path.value().c_str());
+    return false;
+  }
+
+  return RawLoggingLockFileExclusiveNonBlocking(fd_);
+}
+
+bool IOSIntermediatedumpWriter::Close() {
+  static uint8_t t = DOCUMENT_END;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t)) &&
+         RawLoggingCloseFile(fd_);
+}
+
+bool IOSIntermediatedumpWriter::ArrayMapStart() {
+  static uint8_t t = MAP_START;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t));
+}
+
+bool IOSIntermediatedumpWriter::MapStart(IntermediateDumpKey key) {
+  static uint8_t t = MAP_START;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t)) &&
+         RawLoggingWriteFile(fd_, &key, sizeof(key));
+}
+
+bool IOSIntermediatedumpWriter::ArrayStart(IntermediateDumpKey key) {
+  static uint8_t t = ARRAY_START;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t)) &&
+         RawLoggingWriteFile(fd_, &key, sizeof(key));
+}
+
+bool IOSIntermediatedumpWriter::MapEnd() {
+  static uint8_t t = MAP_END;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t));
+}
+
+bool IOSIntermediatedumpWriter::ArrayEnd() {
+  static uint8_t t = ARRAY_END;
+  return RawLoggingWriteFile(fd_, &t, sizeof(uint8_t));
+}
+
+bool IOSIntermediatedumpWriter::AddPropertyInternal(IntermediateDumpKey key,
+                                                    const char* value,
+                                                    size_t value_length) {
+  ScopedVMRead<char> vmread;
+  if (!vmread.Read(value, value_length))
+    return false;
+  uint8_t t = PROPERTY;
+  return RawLoggingWriteFile(fd_, &t, sizeof(t)) &&
+         RawLoggingWriteFile(fd_, &key, sizeof(key)) &&
+         RawLoggingWriteFile(fd_, &value_length, sizeof(size_t)) &&
+         RawLoggingWriteFile(fd_, vmread.get(), value_length);
+}
+
+}  // namespace internal
+}  // namespace crashpad
