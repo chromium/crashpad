607607d461c6bbaa88a589eb647503645a044f07
Justin Cohen <justincohen@google.com>
2021-05-14 11:47:07 -0400

    ios: Add support for intermediate dump reader and writer.
    Due to the limitations of in-process handling, an intermediate dump file
    is written during exceptions. The data is streamed to a file using only
    in-process safe methods. The file format is similar to binary JSON,
    supporting keyed properties, maps and arrays.
     - Property [key:int, length:int, value:intarray]
     - StartMap [key:int], followed by repeating Properties until EndMap
     - StartArray [key:int], followed by repeating Maps until EndArray
     - EndMap, EndArray, EndDocument
    
    Similar to JSON, maps can contain other maps, arrays and properties.
    
    Once loaded, the binary file is read into a set of data structures that
    expose the data, maps and arrays.
    
    Bug: crashpad: 31
    Change-Id: I43a19204935303afd753c8c7090c54099634ccd6

---
+++ util/ios/ios_intermediate_dump_reader.cc
@@ -0,0 +1,160 @@
+// Copyright 2021 The Crashpad Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/ios/ios_intermediate_dump_reader.h"
+
+#include <memory>
+#include <stack>
+#include <vector>
+
+#include "base/logging.h"
+#include "util/ios/ios_intermediate_dump_data.h"
+#include "util/ios/ios_intermediate_dump_format.h"
+#include "util/ios/ios_intermediate_dump_list.h"
+#include "util/ios/ios_intermediate_dump_object.h"
+#include "util/ios/ios_intermediate_dump_writer.h"
+
+namespace crashpad {
+namespace internal {
+
+bool IOSIntermediateDumpReader::Initialize(const base::FilePath& path) {
+  auto reader = std::make_unique<crashpad::FileReader>();
+  if (!reader->Open(path)) {
+    return false;
+  }
+
+  if (!Parse(reader.get(), minidump_)) {
+    LOG(ERROR) << "Intermediate dump parsing failed";
+  }
+
+  return true;
+}
+
+bool IOSIntermediateDumpReader::Parse(FileReaderInterface* reader,
+                                      IOSIntermediateDumpMap& root_map) {
+  if (!reader->SeekSet(0)) {
+    return false;
+  }
+  std::stack<IOSIntermediateDumpObject*> stack;
+  stack.push(&root_map);
+  using Command = IOSIntermediateDumpWriter::CommandType;
+  using Type = IOSIntermediateDumpObject::Type;
+
+  Command command;
+  while (reader->ReadExactly(&command, sizeof(Command))) {
+    IOSIntermediateDumpObject* parent = stack.top();
+    switch (command) {
+      case Command::kMapStart: {
+        std::unique_ptr<IOSIntermediateDumpMap> new_map(
+            new IOSIntermediateDumpMap());
+        if (parent->GetType() == Type::kMap) {
+          const auto parent_map = static_cast<IOSIntermediateDumpMap*>(parent);
+          stack.push(new_map.get());
+          IntermediateDumpKey key;
+          if (!reader->ReadExactly(&key, sizeof(key)))
+            return false;
+          if (key == IntermediateDumpKey::kInvalid)
+            return false;
+          parent_map->map_[key] = std::move(new_map);
+        } else if (parent->GetType() == Type::kList) {
+          const auto parent_list =
+              static_cast<IOSIntermediateDumpList*>(parent);
+          stack.push(new_map.get());
+          parent_list->push_back(std::move(new_map));
+        } else {
+          LOG(ERROR) << "Unexpected parent (not a map or list).";
+          return false;
+        }
+        break;
+      }
+      case Command::kArrayStart: {
+        auto new_list = std::make_unique<IOSIntermediateDumpList>();
+        if (parent->GetType() != Type::kMap) {
+          LOG(ERROR) << "Attempting to push an array not in a map.";
+          return false;
+        }
+
+        IntermediateDumpKey key;
+        if (!reader->ReadExactly(&key, sizeof(key)))
+          return false;
+        if (key == IntermediateDumpKey::kInvalid)
+          return false;
+
+        auto parent_map = static_cast<IOSIntermediateDumpMap*>(parent);
+        stack.push(new_list.get());
+        parent_map->map_[key] = std::move(new_list);
+        break;
+      }
+      case Command::kMapEnd:
+      case Command::kArrayEnd:
+        if (stack.size() < 2) {
+          LOG(ERROR) << "Attempting to pop off main map.";
+          return false;
+        }
+        stack.pop();
+        break;
+      case Command::kProperty: {
+        if (parent->GetType() != Type::kMap) {
+          LOG(ERROR) << "Attempting to add a property not in a map.";
+          return false;
+        }
+        IntermediateDumpKey key;
+        if (!reader->ReadExactly(&key, sizeof(key)))
+          return false;
+        if (key == IntermediateDumpKey::kInvalid)
+          return false;
+
+        off_t value_length;
+        if (!reader->ReadExactly(&value_length, sizeof(value_length))) {
+          return false;
+        }
+
+        constexpr int kMaximumPropertyLength = 64 * 1024 * 1024;  // 64MB.
+        if (value_length > kMaximumPropertyLength) {
+          LOG(ERROR) << "Attempting to read a property that's too big: "
+                     << value_length;
+          return false;
+        }
+
+        std::vector<uint8_t> data(value_length);
+        if (!reader->ReadExactly(data.data(), value_length)) {
+          return false;
+        }
+        auto parent_map = static_cast<IOSIntermediateDumpMap*>(parent);
+        if (parent_map->map_.find(key) != parent_map->map_.end()) {
+          LOG(ERROR) << "Inserting duplicate key";
+        }
+        parent_map->map_[key] =
+            std::make_unique<IOSIntermediateDumpData>(std::move(data));
+        break;
+      }
+      case Command::kRootMapEnd: {
+        if (stack.size() != 1) {
+          LOG(ERROR) << "Unexpected end of root map.";
+          return false;
+        }
+        return true;
+      }
+      default:
+        LOG(ERROR) << "Failed to parse serialized intermediate minidump.";
+        return false;
+    }
+  }
+
+  LOG(ERROR) << "Unexpected end of root map.";
+  return false;
+}
+
+}  // namespace internal
+}  // namespace crashpad
