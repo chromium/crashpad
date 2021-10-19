e4719db8283fa1fe17047fe921422c2ab5319c1c
Arpad Borsos <arpad.borsos@sentry.io>
2021-10-19 14:23:53 +0200

    feat: Do client-side stackwalking on Mac (NATIVE-151) (#47)

--- snapshot/mac/process_reader_mac.h
+++ snapshot/mac/process_reader_mac.h
@@ -181,6 +181,8 @@ class ProcessReaderMac {
   //!     On failure, returns `0` with a message logged.
   mach_vm_address_t DyldAllImageInfo(mach_vm_size_t* all_image_info_size);
 
+  task_t task_;  // weak
+
  private:
   //! Performs lazy initialization of the \a threads_ vector on behalf of
   //! Threads().
@@ -250,7 +252,6 @@ class ProcessReaderMac {
   std::vector<Module> modules_;
   std::vector<std::unique_ptr<MachOImageReader>> module_readers_;
   ProcessMemoryMac process_memory_;
-  task_t task_;  // weak
   InitializationStateDcheck initialized_;
 
 #if defined(CRASHPAD_MAC_32_BIT_SUPPORT)
