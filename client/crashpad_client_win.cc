e9937cb36cd12f24d73a07c4b91168cf1885b5db
Ben Hamilton <benhamilton@google.com>
2022-02-09 13:30:54 -0700

    [Cleanup] Fix circular dependency on Windows.
    Crashpad currently has a circular dependency: client->snapshot->client.
    
    The dependency from snapshot -> client only exists to pull in a single
    constant for Windows (CrashpadClient::kTriggeredExceptionCode), so this
    change breaks the dependency by splitting the constant out into a new
    file util/win/exception_codes.h.
    
    Change-Id: I6b74b367df716e097758e63a44c53cb92ea5e04d
    Reviewed-on: https://chromium-review.googlesource.com/c/crashpad/crashpad/+/3450763
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>

--- client/crashpad_client_win.cc
+++ client/crashpad_client_win.cc
@@ -36,6 +36,7 @@
 #include "util/win/command_line.h"
 #include "util/win/context_wrappers.h"
 #include "util/win/critical_section_with_debug_info.h"
+#include "util/win/exception_codes.h"
 #include "util/win/get_function.h"
 #include "util/win/handle.h"
 #include "util/win/initial_client_data.h"
@@ -926,7 +927,7 @@ bool CrashpadClient::DumpAndCrashTargetProcess(HANDLE process,
 
     // ecx = kTriggeredExceptionCode for dwExceptionCode.
     data_to_write.push_back(0xb9);
-    AddUint32(&data_to_write, kTriggeredExceptionCode);
+    AddUint32(&data_to_write, ExceptionCodes::kTriggeredExceptionCode);
 
     // jmp to RaiseException() via rax.
     data_to_write.push_back(0x48);  // mov rax, imm.
